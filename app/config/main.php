9d599b83957ade818f57a2c97c2b7b517f02d57d
ipti009 <ipti009@IPTI009>
2013-11-26 10:29:23 -0300

    OK

--- app/config/main.php
+++ app/config/main.php
@@ -4,7 +4,7 @@
 // Yii::setPathOfAlias('local','path/to/local-folder');
 
 // This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
+// CWebApplication propeties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'TAG',
