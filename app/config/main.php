402c4c45367dcfc5815659f8f4e341df544406ca
ipti009 <ipti009@IPTI009>
2013-12-18 10:45:04 -0300

    Linguagem padrão: pt-br

--- app/config/main.php
+++ app/config/main.php
@@ -9,6 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'TAG',
         'theme'=>'default',
+        'language' => 'pt_br',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
