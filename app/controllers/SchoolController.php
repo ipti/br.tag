83dc7643cbe97682e6b3a4296094fb18927f1cb7
ipti004 <ipti004@ipti004>
2014-01-30 11:07:36 -0300

    limitar escola para o admin

--- app/controllers/SchoolController.php
+++ app/controllers/SchoolController.php
@@ -27,13 +27,13 @@ class SchoolController extends Controller {
     public function accessRules() {
         return array(
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('index', 'view', 'create', 'update', 
-                    'edcenso_import', 'configacl',
-                    'getcities','getdistricts', 'getorgans', 'updateufdependencies'),
-                'users' => array('admin'),
+                'actions' => array('edcenso_import'),
+                'users' => array('@'),
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
-                'actions' => array('admin', 'delete'),
+                'actions' => array('admin', 'delete','index', 'view', 'create', 'update', 
+                    'edcenso_import', 'configacl',
+                    'getcities','getdistricts', 'getorgans', 'updateufdependencies'),
                 'users' => array('admin'),
             ),
             array('deny', // deny all users
