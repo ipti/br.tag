fe9b4b2701be076ccaac9a4a823ed1827d02f080
Francisco Cabral Fh <franciscocabralfilho@gmail.com>
2014-05-07 10:31:53 -0300

    Alterações nas estruturas das pastas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/app/config/main.php';
 
 // remove the following lines when in production mode
