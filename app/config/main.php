58ab1e4a712f233fcf63f0c983554813d0cfbbac
iptipc002 <iptipc002@IPTIPC002.IPTIPC002>
2015-04-09 09:07:10 -0300

    Merge origin/master
    Conflicts:
    	app/config/main.php
    Signed-off-by:iptipc002 <iptipc002@IPTIPC002.IPTIPC002>

--- app/config/main.php
+++ app/config/main.php
@@ -38,15 +38,15 @@ return array(
             'allowAutoLogin' => true,
         ),
         // uncomment the following to enable URLs in path-format
+            'urlManager'=>array(
+                'urlFormat'=>'path',
+                'rules'=>array(
+                    '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                    '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                    '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                ),
+            ),
         /*
-          'urlManager'=>array(
-          'urlFormat'=>'path',
-          'rules'=>array(
-          '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-          '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-          '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-          ),
-          ),
           'db'=>array(
           'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
           ),
