3823e144b5abe6ac8011d2fcb312190dc7446f80
ruanc_000 <ruanc_000@olimpo>
2015-08-09 15:36:32 -0300

    Ativação do modo de depuração como padrão.

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+date_default_timezone_set('America/Los_Angeles');
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/app/config/main.php';
 
 // remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',false);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
