55cab88990fc1491ab686acc57dde95c9e5810d4
franciscocabral <franciscocabralfilho@gmail.com>
2015-09-11 07:23:59 -0300

    Informações Municipáis. Incompleto.

--- app/modules/resultsmanagement/messages/pt_br/layout.php
+++ app/modules/resultsmanagement/messages/pt_br/layout.php
@@ -6,7 +6,14 @@ return [
 
     //Right Column
     "City Informations" => "Informações do Município",
-    "Services" => "Serviços",
     "Enrollments" => "Matrículas",
+    "TOTAL SCHOOLS" => "TOTAL DE ESCOLAS",
+    "Working : {active} | Not working: {inactive} | Extinct: {extinct}" => "Em atividade : {active} | Paralisada: {inactive} | Extinta: {extinct}",
+    "URBAN AREA" => "ÁREA URBANA",
+    "RURAL AREA" => "ÁREA RURAL",
+    "REGULAR EDUCATION"=>"DE ENSINO REGULAR",
+    "ADITIONAL ACTV."=>"DE ATV. COMPLEMENTAR",
+    "Services" => "Serviços",
+
 
 ];
--- app/modules/resultsmanagement/views/layouts/resultsmanagement.php
+++ app/modules/resultsmanagement/views/layouts/resultsmanagement.php
@@ -5,90 +5,63 @@ $themeUrl = Yii::app()->theme->baseUrl;
 $homeUrl = Yii::app()->homeUrl;
 $year = Yii::app()->user->year;
 $city = "Boquim";
-$headerDescription = CHtml::tag("span", [],
-    yii::t("resultsmanagementModule.index", 'In this area it is possible to know some of the educational indicators of the schools in the city of {city}', ['{city}' => $city])
-);
+$headerDescription = CHtml::tag("span", [], yii::t("resultsmanagementModule.index", 'In this area it is possible to know some of the educational indicators of the schools in the city of {city}', ['{city}' => $city]));
+
+/* @var $schools SchoolIdentification[] */
+$schools = SchoolIdentification::model()->findAll();
+$schoolsCount = count($schools);
+$schoolsActive = 0;
+$schoolsInactive = 0;
+$schoolsExtinct = 0;
+foreach($schools as $school){
+    if($school->situation == 1) $schoolsActive++;
+    else if($school->situation == 2) $schoolsInactive++;
+    else if($school->situation == 3) $schoolsExtinct++;
+}
+
+$enrollments = StudentEnrollment::model()->findAllBySql("select student_enrollment.* from student_enrollment JOIN classroom ON classroom.id = student_enrollment.classroom_fk
+where classroom.school_year = :year",["year"=>$year]);
+$enrollmentsCount = count($enrollments);
+$enrollmentsUrban = 0;
+$enrollmentsRural = 0;
+$enrollmentsRegular = 0;
+$enrollmentsAditional = 0;
+foreach($schools as $school){
+    if($school->situation == 1) $schoolsActive++;
+    else if($school->situation == 2) $schoolsInactive++;
+    else if($school->situation == 3) $schoolsExtinct++;
+}
 
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>
-<html class="ie lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>
-<html class="ie lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>
-<html class="ie lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>
-<html class="ie gt-ie8"> <![endif]-->
-<!--[if !IE]><!-->
-<html><!-- <![endif]-->
+<!--[if lt IE 7]><html class="ie lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="ie lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="ie lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><html class="ie gt-ie8"> <![endif]-->
+<!--[if !IE]><!--><html><!-- <![endif]-->
 <head>
     <title><?= CHtml::encode($this->pageTitle); ?></title>
 
     <meta charset="UTF-8"/>
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE"/>
 
-    <!-- Bootstrap -->
     <link href="themes/default/lib/css/bootstrap.css" rel="stylesheet"/>
-
-    <!-- Glyphicons Font Icons -->
     <link href="themes/default/lib/fonts/glyphicons/css/glyphicons_social.css" rel="stylesheet"/>
     <link href="themes/default/lib/fonts/glyphicons/css/glyphicons_filetypes.css" rel="stylesheet"/>
     <link href="themes/default/lib/fonts/glyphicons/css/glyphicons_regular.css" rel="stylesheet"/>
-
-    <!-- Font Awsome Icons -->
     <link href="themes/default/lib/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
-
-    <!-- Uniform Pretty Checkboxes -->
-    <link href="themes/default/lib/js/plugins/forms/pixelmatrix-uniform/css/uniform.default.css" rel="stylesheet"/>
-
-    <!-- PrettyPhoto -->
     <link href="themes/default/lib/js/plugins/gallery/prettyphoto/css/prettyPhoto.css" rel="stylesheet"/>
-
-    <!--[if IE]><!-->
-    <script src="themes/default/lib/js/plugins/other/excanvas/excanvas.js"></script>
-    <!--<![endif]-->
-    <!--[if lt IE 8]>
-    <script src="themes/default/lib/js/plugins/other/json2.js"></script>
-    <![endif]-->
-
-    <!-- Bootstrap Extended -->
     <link href="/themes/default/lib/extend/jasny-bootstrap/css/jasny-bootstrap.min.css" rel="stylesheet">
     <link href="/themes/default/lib/extend/jasny-bootstrap/css/jasny-bootstrap-responsive.min.css" rel="stylesheet">
     <link href="/themes/default/lib/extend/bootstrap-wysihtml5/css/bootstrap-wysihtml5-0.0.2.css" rel="stylesheet">
     <link href="/themes/default/lib/extend/bootstrap-select/bootstrap-select.css" rel="stylesheet"/>
     <link href="/themes/default/lib/extend/bootstrap-switch/static/stylesheets/bootstrap-switch.css" rel="stylesheet"/>
-
-    <!-- Select2 Plugin -->
     <link href="themes/default/lib/js/plugins/forms/select2/select2.css" rel="stylesheet"/>
+    <link href="themes/default/lib/js/plugins/system/jquery-ui/css/smoothness/jquery-ui-1.9.2.custom.min.css" rel="stylesheet"/>
 
-    <!-- DateTimePicker Plugin -->
-    <link href="themes/default/lib/js/plugins/forms/bootstrap-datetimepicker/css/datetimepicker.css" rel="stylesheet"/>
-
-    <!-- JQueryUI -->
-    <link href="themes/default/lib/js/plugins/system/jquery-ui/css/smoothness/jquery-ui-1.9.2.custom.min.css"
-          rel="stylesheet"/>
-
-    <!-- MiniColors ColorPicker Plugin -->
-    <link href="themes/default/lib/js/plugins/color/jquery-miniColors/jquery.miniColors.css" rel="stylesheet"/>
-
-    <!-- Notyfy Notifications Plugin -->
-    <link href="themes/default/lib/js/plugins/notifications/notyfy/jquery.notyfy.css" rel="stylesheet"/>
-    <link href="themes/default/lib/js/plugins/notifications/notyfy/themes/default.css" rel="stylesheet"/>
-
-    <!-- Gritter Notifications Plugin -->
-    <link href="themes/default/lib/js/plugins/notifications/Gritter/css/jquery.gritter.css" rel="stylesheet"/>
-
-    <!-- Easy-pie Plugin -->
-    <link href="themes/default/lib/js/plugins/charts/easy-pie/jquery.easy-pie-chart.css" rel="stylesheet"/>
-
-    <!-- Google Code Prettify Plugin -->
-    <link href="themes/default/lib/js/plugins/other/google-code-prettify/prettify.css" rel="stylesheet"/>
-
-    <!-- Main Theme Stylesheet :: CSS -->
     <link href="/themes/default/common/css/style.css" rel="stylesheet"/>
 
 </head>
@@ -96,16 +69,14 @@ $headerDescription = CHtml::tag("span", [],
 <body>
 
 <div class="container fluid menu-left">
-
     <div class="navbar main hidden-print">
         <a href="/" class="appbrand pull-left">
             <img src="themes/default/img/tag_logo.png"/>
-            <!--<span><span>Ano atual: --><? //=$year?><!--</span></span>-->
+<!--            <span><span>Ano atual: --><?//=$year?><!--</span></span>-->
         </a>
     </div>
 
     <div id="wrapper">
-        <!-- Content -->
         <div id="content">
             <div class="header-block">
                 <h2><?= yii::t('resultsmanagementModule.layout', "Results Management") ?>
@@ -138,10 +109,26 @@ $headerDescription = CHtml::tag("span", [],
                                             <?= yii::t('resultsmanagementModule.layout', 'Enrollments') ?>
                                         </a>
                                     </h4>
-                                    <div id="collapseEnrollment" class="collapse" style="height: auto;">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-                                        dolor brunch.
+                                    <div id="collapseEnrollment"  class="collapse panel-content">
+                                        <hr/>
+                                        <h5><span><?= yii::t('resultsmanagementModule.layout', 'TOTAL SCHOOLS')?>: </span><?=$schoolsCount?></h5>
+                                        <span><?= yii::t('resultsmanagementModule.layout', 'Working : {active} | Not working: {inactive} | Extinct: {extinct}',
+                                                ['{active}' => $schoolsActive, "{inactive}"=>$schoolsInactive, "{extinct}"=>$schoolsExtinct])?></span>
+                                        <h5><span><?= yii::t('resultsmanagementModule.layout', 'TOTAL ENROLLMENTS')?>: </span><?=$enrollmentsCount?></h5>
+                                        <div class="panel-box">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <span><?=yii::t('resultsmanagementModule.layout',"URBAN AREA")?>: <?=$enrollmentsUrban?></span>
+                                                    <br>
+                                                    <span><?=yii::t('resultsmanagementModule.layout',"RURAL AREA")?>: <?=$enrollmentsRural?></span>
+                                                </div>
+                                                <div class="col-md-7">
+                                                    <span><?=yii::t('resultsmanagementModule.layout',"REGULAR EDUCATION")?>: <?=$enrollmentsRegular?></span>
+                                                    <br>
+                                                    <span><?=yii::t('resultsmanagementModule.layout',"ADITIONAL ACTV.")?>:  <?=$enrollmentsAditional?></span>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="separator bottom"></div>
                                     <h4 class="panel-title">
@@ -151,7 +138,8 @@ $headerDescription = CHtml::tag("span", [],
                                         </a>
                                     </h4>
 
-                                    <div id="collapseService" class="collapse" style="height: auto;">
+                                    <div id="collapseService" class="collapse panel-content">
+                                        <hr/>
                                         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
                                         richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
                                         brunch.
@@ -160,48 +148,25 @@ $headerDescription = CHtml::tag("span", [],
                             </div>
                         </div>
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-        <!-- // Content END -->
-
     </div>
     <div class="clearfix"></div>
-    <!-- // Sidebar menu & content wrapper END -->
-
 </div>
-<!-- // Main Container Fluid END -->
 
-
-<!-- JQuery -->
 <script src="themes/default/lib/js/plugins/system/jquery.min.js"></script>
 <script src="themes/default/lib/js/plugins/system/jquery-ui/js/jquery-ui-1.9.2.custom.min.js"></script>
 <script src="themes/default/lib/js/plugins/system/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
 <script src="themes/default/lib/js/plugins/system/modernizr.js"></script>
 <script src="/themes/default/lib/js/bootstrap.min.js"></script>
-
-<!-- SlimScroll Plugin -->
 <script src="themes/default/lib/js/plugins/other/jquery-slimScroll/jquery.slimscroll.min.js"></script>
-
-
-<!-- Holder Plugin -->
 <script src="themes/default/lib/js/plugins/other/holder/holder.js"></script>
-
-<!-- Uniform Forms Plugin -->
 <script src="themes/default/lib/js/plugins/forms/pixelmatrix-uniform/jquery.uniform.min.js"></script>
-
-<!-- PrettyPhoto -->
 <script src="themes/default/lib/js/plugins/gallery/prettyphoto/js/jquery.prettyPhoto.js"></script>
 
-<!-- Global -->
-<script>
-    var basePath = '/themes/default/common/';
-</script>
+<script>    var basePath = '/themes/default/common/';</script>
 
-<!-- Bootstrap Extended -->
 <script src="/themes/default/lib/extend/bootstrap-select/bootstrap-select.js"></script>
 <script src="/themes/default/lib/extend/bootstrap-switch/static/js/bootstrap-switch.js"></script>
 <script src="/themes/default/lib/extend/bootstrap-hover-dropdown/twitter-bootstrap-hover-dropdown.min.js"></script>
@@ -211,57 +176,7 @@ $headerDescription = CHtml::tag("span", [],
 <script src="/themes/default/lib/extend/bootstrap-wysihtml5/js/wysihtml5-0.3.0_rc2.min.js"></script>
 <script src="/themes/default/lib/extend/bootstrap-wysihtml5/js/bootstrap-wysihtml5-0.0.2.js"></script>
 
-<!-- Google Code Prettify -->
-<script src="themes/default/lib/js/plugins/other/google-code-prettify/prettify.js"></script>
-
-<!-- Gritter Notifications Plugin -->
-<script src="themes/default/lib/js/plugins/notifications/Gritter/js/jquery.gritter.min.js"></script>
-
-<!-- Notyfy Notifications Plugin -->
-<script src="themes/default/lib/js/plugins/notifications/notyfy/jquery.notyfy.js"></script>
-
-<!-- MiniColors Plugin -->
-<script src="themes/default/lib/js/plugins/color/jquery-miniColors/jquery.miniColors.js"></script>
-
-<!-- DateTimePicker Plugin -->
-<script src="themes/default/lib/js/plugins/forms/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
-
-<!-- Cookie Plugin -->
-<script src="themes/default/lib/js/plugins/system/jquery.cookie.js"></script>
-
-<!-- Colors -->
-<script>
-    var primaryColor = '#e25f39',
-        dangerColor = '#bd362f',
-        successColor = '#609450',
-        warningColor = '#ab7a4b',
-        inverseColor = '#45484d';
-</script>
-
-
-<!-- Easy-pie Plugin -->
-<script src="themes/default/lib/js/plugins/charts/easy-pie/jquery.easypiechart.js"></script>
-
-<!-- Sparkline Charts Plugin -->
-<script src="themes/default/lib/js/plugins/charts/sparkline/jquery.sparkline.min.js"></script>
-
-<!-- Ba-Resize Plugin -->
-<script src="themes/default/lib/js/plugins/other/jquery.ba-resize.js"></script>
-
-
-<!-- Google JSAPI -->
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-
-<!--  Flot Charts Plugin -->
-<script src="themes/default/lib/js/plugins/charts/flot/jquery.flot.js"></script>
-<script src="themes/default/lib/js/plugins/charts/flot/jquery.flot.pie.js"></script>
-<script src="themes/default/lib/js/plugins/charts/flot/jquery.flot.tooltip.js"></script>
-<script src="themes/default/lib/js/plugins/charts/flot/jquery.flot.selection.js"></script>
-<script src="themes/default/lib/js/plugins/charts/flot/jquery.flot.resize.js"></script>
-<script src="themes/default/lib/js/plugins/charts/flot/jquery.flot.orderBars.js"></script>
-
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjWiKq1o5qX_LEokFDPUkIin3ckXpmWY0&callback=initMap">
-</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjWiKq1o5qX_LEokFDPUkIin3ckXpmWY0&callback=initMap"></script>
 
 <script src="themes/default/lib/js/jquery.select2.js"></script>
 <script src="themes/default/lib/js/jquery.select2.pt-br.js"></script>
