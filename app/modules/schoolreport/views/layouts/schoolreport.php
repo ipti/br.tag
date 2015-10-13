acc2b5f94cb4e1a5cbefd506f25760843c553b80
franciscocabral <franciscocabralfilho@gmail.com>
2015-10-13 16:51:15 -0300

    Adicionado novo layout na tela de Boletim Escolar

--- app/modules/schoolreport/views/layouts/schoolreport.php
+++ app/modules/schoolreport/views/layouts/schoolreport.php
@@ -1,12 +1,17 @@
 <?php
-/* @var $content String Conteúdo da página. */
-
+/* @var $content String Conteúdo da página.
+ * @var $enrollment StudentEnrollment
+ */
 $themeUrl = Yii::app()->theme->baseUrl;
 $homeUrl = Yii::app()->controller->module->baseUrl;
 $baseScriptUrl = Yii::app()->controller->module->baseScriptUrl;
 
 $year = Yii::app()->user->year;
-
+$eid = 6396;
+$enrollment = StudentEnrollment::model()->findByPk($eid                                       );
+$student = $enrollment->studentFk;
+$classroom = $enrollment->classroomFk;
+$school = $classroom->schoolInepFk;
 
 ?>
 <!DOCTYPE html>
@@ -16,65 +21,191 @@ $year = Yii::app()->user->year;
 <!--[if gt IE 8]><html class="ie gt-ie8"> <![endif]-->
 <!--[if !IE]><!--><html><!-- <![endif]-->
 <head>
+    <meta charset="UTF-8"/>
     <title><?= CHtml::encode($this->pageTitle); ?></title>
 
-    <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE"/>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link href="<?=$baseScriptUrl?>/lib/fonts/glyphicons/css/glyphicons_social.css" rel="stylesheet"/>
-    <link href="<?=$baseScriptUrl?>/lib/fonts/glyphicons/css/glyphicons_filetypes.css" rel="stylesheet"/>
-    <link href="<?=$baseScriptUrl?>/lib/fonts/glyphicons/css/glyphicons_regular.css" rel="stylesheet"/>
-
-    <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
-
-    <link href="<?=$baseScriptUrl?>/common/css/style.css" rel="stylesheet"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.4/semantic.min.css" rel="stylesheet"/>
     <link href="<?=$baseScriptUrl?>/common/css/layout.css" rel="stylesheet"/>
-    <script type="text/javascript">var $baseScriptUrl = "<?=$baseScriptUrl?>";</script>
+    <script type="text/javascript">var $baseScriptUrl = "<?=$baseScriptUrl?>"</script>
 </head>
 
-<body>
-
-    <div class="container fluid menu-left">
-        <div class="navbar main hidden-print">
-            <a href="<?=$homeUrl?>" class="appbrand pull-left">
-                <img src="/themes/default/img/tag_logo.png"/>
-            </a>
-        </div>
-
-        <div id="wrapper">
-            <div id="content">
-                <div class="header-block">
-                    <h2><?= yii::t('schoolreportModule.layout', "School Report") ?>
-                        <div class="separator bottom"></div>
-                        <div class="border-header header-description">
-                            <?= yii::t("schoolreportModule.layout","In this section it's possible to see the student's performance") ?>
-                        </div>
-                    </h2>
+    <header>
+        <div class="ui container">
+            <div class="ui grid">
+                <div class="four wide column">
+                    <a href="<?=$homeUrl?>">
+                        <img src="<?=$baseScriptUrl?>/common/img/logo.png" height="30">
+                    </a>
+                    <h1><?= yii::t('schoolreportModule.layout', "School Report") ?></h1>
                 </div>
-                <div class="separator bottom"></div>
-                <div class="breadcrumb-bar"></div>
-                <div class="innerLR content-block">
-                    <div class="clearfix"></div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <?= $content ?>
-                        </div>
-                    </div>
+                <div class="ten wide column"></div>
+                <div class="two wide column right aligned">
+                    <button class="ui inverted basic button">Sair</button>
                 </div>
             </div>
         </div>
-        <div class="clearfix"></div>
-    </div>
+    </header>
+
+    <section id="info">
+        <div class="ui container">
+            <div class="ui grid">
+                <div class="fourteen wide column">
+                    <h2 class="ui header">
+                        <img src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/unknown2-128.png" class="ui circular image">
+                        <div class="content">
+                             <?=strtolower($student->name)?>
+                            <div class="sub header"><?=strtolower($school->name)?></div>
+                        </div>
+                    </h2>
+                </div>
+                <div class="two wide column right aligned">
+                    <button class="ui small button">
+                        <i class="print icon"></i>
+                        Imprimir
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="reports">
+        <div class="ui container">
+            <table class="ui striped very basic table">
+                <thead>
+                <tr>
+                    <th class="left aligned">Disciplina</th>
+                    <th>1ª</th>
+                    <th>2ª</th>
+                    <th>RS</th>
+                    <th>3ª</th>
+                    <th>4ª</th>
+                    <th>RS</th>
+                    <th>RF</th>
+                    <th>Média Final</th>
+                </tr></thead>
+                <tbody>
+                <tr>
+                    <td class="left aligned">Português</td>
+                    <td><a class="ui blue label">10</a></td>
+                    <td><a class="ui red label">4.4</a></td>
+                    <td><a class="ui blue label">6.8</a></td>
+                    <td><a class="ui blue label">6.8</a></td>
+                    <td><a class="ui blue label">6.8</a></td>
+                    <td><a class="ui blue label">6.8</a></td>
+                    <td><a class="ui blue label">6.8</a></td>
+                    <td><a class="ui blue image label">6.8<div class="detail">Aprovado</div></td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Matemática</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Geografia</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">História</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Biologia</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Física</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Química</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Inglês</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Redação</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td class="left aligned">Filosofia</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+            <?= $content ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
     <script src="<?=$baseScriptUrl?>/common/js/layout.js"></script>
-    <script src="<?=$baseScriptUrl?>/lib/js/jquery.select2.pt-br.js"></script>
 </body>
 </html>
