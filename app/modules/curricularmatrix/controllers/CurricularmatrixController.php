5b39cdb6b6f6d4980cd30c9b05ccd4145154d1a2
Francisco Cabral <franciscocabralfilho@gmail.com>
2016-04-29 10:17:39 -0300

    Inicio da criação da tela

--- app/modules/curricularmatrix/controllers/CurricularmatrixController.php
+++ app/modules/curricularmatrix/controllers/CurricularmatrixController.php
@@ -1,46 +1,53 @@
 <?php
 
-class CurricularmatrixController extends Controller
-{
-    /**
-     * @return array action filters
-     */
-    public function filters()
-    {
-        return [
-            'accessControl', // perform access control for CRUD operations
-            'postOnly + delete', // we only allow deletion via POST request
-        ];
-    }
+	class CurricularmatrixController extends Controller {
+		/**
+		 * @return array action filters
+		 */
+		public function filters() {
+			return [
+				'accessControl', // perform access control for CRUD operations
+				'postOnly + delete', // we only allow deletion via POST request
+			];
+		}
 
-    /**
-     * Specifies the access control rules.
-     * This method is used by the 'accessControl' filter.
-     * @return array access control rules
-     */
-    public function accessRules()
-    {
-        return [
-            [
-                'allow',  // allow all users to perform 'index' and 'view' actions
-                'actions' => [], 'users' => ['*'],
-            ], [
-                'allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => ['index'],
-                'users' => ['@'],
-            ], [
-                'allow', // allow admin user to perform 'admin' and 'delete' actions
-                'actions' => [], 'users' => ['admin'],
-            ], [
-                'deny',  // deny all users
-                'users' => ['*'],
-            ],
-        ];
-    }
+		/**
+		 * Specifies the access control rules.
+		 * This method is used by the 'accessControl' filter.
+		 * @return array access control rules
+		 */
+		public function accessRules() {
+			return [
+				[
+					'allow',  // allow all users to perform 'index' and 'view' actions
+					'actions' => [], 'users' => ['*'],
+				], [
+					'allow', // allow authenticated user to perform 'create' and 'update' actions
+					'actions' => ['index','addMatrix'], 'users' => ['@'],
+				], [
+					'allow', // allow admin user to perform 'admin' and 'delete' actions
+					'actions' => [], 'users' => ['admin'],
+				], [
+					'deny',  // deny all users
+					'users' => ['*'],
+				],
+			];
+		}
 
 
-    public function actionIndex()
-    {
-        $this->render('index');
-    }
-}
+		public function actionIndex() {
+			$this->render('index');
+		}
+
+		public function actionAddMatrix(){
+			$stages = $_POST['stages'];
+			$disciplines = $_POST['disciplines'];
+			$workload  = $_POST['workload'];
+			$credits = $_POST['credits'];
+
+			if(isset($stages,$disciplines,$workload,$credits)){
+				foreach($stages as $stage){
+				}
+			}
+		}
+	}
--- app/modules/curricularmatrix/views/curricularmatrix/index.php
+++ app/modules/curricularmatrix/views/curricularmatrix/index.php
@@ -1,41 +1,77 @@
 <?php
-/* @var $this TimesheetController
- * @var $cs CClientScript
- */
+	/* @var $this TimesheetController
+	 * @var $cs CClientScript
+	 */
 
-$baseScriptUrl = Yii::app()->controller->module->baseScriptUrl;
+	$baseScriptUrl = Yii::app()->controller->module->baseScriptUrl;
 
-$cs = Yii::app()->getClientScript();
-$cs->registerCssFile($baseScriptUrl . '/common/css/layout.css');
-$cs->registerScriptFile($baseScriptUrl . '/common/js/curricularmatrix.js', CClientScript::POS_END);
-//$cs->registerScript("vars", "var generateTimesheet = '" . $this->createUrl("generateTimesheet") . "';", CClientScript::POS_HEAD);
-$this->setPageTitle('TAG - ' . Yii::t('curricularMatrixModule.index', 'Curricular Matrix'));
+	$cs = Yii::app()->getClientScript();
+	$cs->registerCssFile($baseScriptUrl . '/common/css/layout.css');
+	$cs->registerScriptFile($baseScriptUrl . '/common/js/curricularmatrix.js', CClientScript::POS_END);
+	$cs->registerScript("vars", "var addMatrix = '" . $this->createUrl("addMatrix") . "';", CClientScript::POS_HEAD);
+	$this->setPageTitle('TAG - ' . Yii::t('curricularMatrixModule.index', 'Curricular Matrix'));
 ?>
 <?php if (Yii::app()->user->hasFlash('success')): ?>
-    <div class="row-fluid">
-        <div class="span12">
-            <div class="alert alert-success">
-                <?php echo Yii::app()->user->getFlash('success') ?>
-            </div>
-        </div>
-    </div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-success">
+				<?php echo Yii::app()->user->getFlash('success') ?>
+			</div>
+		</div>
+	</div>
 <?php endif ?>
 <?php if (Yii::app()->user->hasFlash('error')): ?>
-    <div class="row-fluid">
-        <div class="span12">
-            <div class="alert alert-error">
-                <?php echo Yii::app()->user->getFlash('error') ?>
-            </div>
-        </div>
-    </div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="alert alert-error">
+				<?php echo Yii::app()->user->getFlash('error') ?>
+			</div>
+		</div>
+	</div>
 <?php endif ?>
 
 <div class="row-fluid">
-    <div class="span12">
-        <h3 class="heading-mosaic"><?= yii::t('curricularMatrixModule.index', 'Curricular Matrix') ?></h3>
-    </div>
+	<div class="span12">
+		<h3 class="heading-mosaic"><?= yii::t('curricularMatrixModule.index', 'Curricular Matrix') ?></h3>
+	</div>
 </div>
 
 <div class="innerLR home">
-
+	<div class="row-fluid">
+		<div class="span5">
+			<?= CHtml::label(Yii::t('curricularMatrixModule.index', 'Stage'), 'stages', ['class' => "control-label"]) ?>
+			<div class="form-group ">
+				<?= CHtml::dropDownList("stages", [], CHtml::listData(EdcensoStageVsModality::model()->findAll(), "id", "name"), [
+					"multiple" => "multiple", "class" => "select-search-on span12"
+				]) ?>
+			</div>
+		</div>
+		<div class="span3">
+			<?= CHtml::label(Yii::t('curricularMatrixModule.index', 'Disciplines'), 'disciplines', ['class' => "control-label"]) ?>
+			<div class="form-group ">
+				<?= CHtml::dropDownList("disciplines", [], CHtml::listData(EdcensoDiscipline::model()->findAll(), "id", "name"), [
+					"multiple" => "multiple", "class" => "select-search-on span12"
+				]) ?>
+			</div>
+		</div>
+		<div class="span1">
+			<?= CHtml::label(Yii::t('curricularMatrixModule.index', 'Workload'), 'workload', ['class' => "control-label"]) ?>
+			<div class="form-group ">
+				<?= CHtml::numberField("workload", "0", ["min" => "0", "max" => "9999", "class" => "span12"]) ?>
+			</div>
+		</div>
+		<div class="span1">
+			<?= CHtml::label(Yii::t('curricularMatrixModule.index', 'Credits'), 'credits', ['class' => "control-label"]) ?>
+			<div class="form-group ">
+				<?= CHtml::numberField("credits", "0", ["min" => "0", "max" => "99", "class" => "span12"]) ?>
+			</div>
+		</div>
+		<div class="span2">
+			<?= CHtml::label("&nbsp;", 'credits', ['class' => "control-label"]) ?>
+			<div class="form-group ">
+				<?= CHtml::button(Yii::t('curricularMatrixModule.index', 'Add'),["id"=>"add-matrix", "class" => "btn btn-primary"]) ?>
+			</div>
+		</div>
+	</div>
+	<hr>
 </div>
