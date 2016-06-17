0ff0e26a5b98b6513eb8e25f30d46ceeb583c421
paulones <paulones89@gmail.com>
2016-06-17 16:21:11 -0300

    inserido school_fk no log

--- app/migrations/m160617_144948_log.php
+++ app/migrations/m160617_144948_log.php
@@ -9,9 +9,12 @@ class m160617_144948_log extends CDbMigration
 			'table' => 'varchar(50) NOT NULL',
 			'table_pk' => 'varchar(20) NOT NULL',
 			'crud' => 'char(1) NOT NULL',
-			'purged_info' => 'varchar(100)',
 			'date' => 'datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()',
+			'purged_info' => 'varchar(100)',
+			'school_fk' => 'varchar(8) NOT NULL',
 		],  'ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci');
+
+		$this->addForeignKey('log_school_fkey','log','school_fk','school_identification','inep_id','CASCADE','CASCADE');
 	}
 
 	public function down()
