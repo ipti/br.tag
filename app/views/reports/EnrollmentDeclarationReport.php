4d671a5b8a2d3f2ee89602c09806e08420c757e7
RuanNascimentoSantos <ruan@celi.se>
2016-07-25 05:02:18 -0300

    Fix logo de boquim na declaração

--- app/views/reports/EnrollmentDeclarationReport.php
+++ app/views/reports/EnrollmentDeclarationReport.php
@@ -61,8 +61,9 @@ $school = SchoolIdentification::model()->findByPk(Yii::app()->user->school);
                     </p>
                     <span style="display: block; clear: both"></span>
                 <?php }else{?>
-                    <img src="data:<?=$school->logo_file_type?>;base64,<?=base64_encode($school->logo_file_content)?>" width="40px" style="float: left; margin-right: 5px;">
+                    <img src="/images/boquim.png" width="40px" style="float: left; margin-right: 5px;">
                     <span style="text-align: center; float: left; margin-top: 5px;">PREFEITURA MUNICIPAL DE <?=strtoupper($school->edcensoCityFk->name)?><br>
+                        <?php echo $school->name ?>
                         <?php //strtoupper($school->report_header)?></span>
                     <span style="clear:both;display:block"></span>
                 <?php }?>
