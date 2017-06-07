2dc96303e02eeb44981f61dde50c3f3f19565db0
RuanNascimentoSantos <ruan@celi.se>
2017-06-06 21:03:15 -0300

    2.10.5 - Centralização do cabeçalho

--- themes/default/views/reports/head.php
+++ themes/default/views/reports/head.php
@@ -1,22 +1,33 @@
 <?php
 /* @var $this ReportsController
-    @var $school SchoolIdentification
+@var $school SchoolIdentification
  */
 $school = SchoolIdentification::model()->findByPk(Yii::app()->user->school);
 //$school = new SchoolIdentification;
 ?>
+<style>
+    #info li {text-align:center;}
+    #addinfo li{text-align: center}
+</style>
 <h3 class="heading visible-print"><?php echo @$title ?></h3>
 <div id="header-report">
-    <img src="<?php echo Yii::app()->controller->createUrl('school/displayLogo', array('id' => $school->inep_id));?>" id="logo"/>
+    <?php
+    if(isset($school->logo_file_name)){
+        echo '<img id="logo" src="data:'.$school->logo_file_type.';base64,'.base64_encode($school->logo_file_content).'">';
+    };
+    ?>
     <ul id="info">
-        <li>ESTADO DE <?php echo strtoupper($school->edcensoUfFk->name) ?></li>
-        <li>PREFEITURA MUNICIPAL DE <?php echo $school->edcensoCityFk->name ?></li>
-        <li><?php echo $school->name ?></li>
+        <?php if(isset($school->act_of_acknowledgement)&&(!empty($school->act_of_acknowledgement))){?>
+            <li><?php echo $school->name ?></li>
+        <?php }else{?>
+            <li>PREFEITURA MUNICIPAL DE <?php echo $school->edcensoCityFk->name ?></li>
+            <li><?php echo $school->name ?></li>
+        <?php }?>
     </ul>
     <ul id="addinfo">
-        <li><?php echo $school->address.', '.(!empty($school->address_number) ? $school->address_number.', ':'' ).$school->address_neighborhood; ?></li>
-        <li><?php echo $school->cep.' - '.$school->edcensoCityFk->name . " - " . $school->edcensoUfFk->acronym ?></li>
-        <?php echo 'Email: '.(!empty($school->email) ? $school->email.' - ': (!empty($school->manager_email) ? $school->manager_email.' - ':'' ) ).'Tel: '.(!empty($school->phone_number) ? $school->phone_number:'' )?>
+        <li><?php echo $school->address.', '.(!empty($school->address_number) ? $school->address_number.', ':'' ).$school->address_neighborhood; ?>, <?php echo $school->edcensoCityFk->name . " - " . $school->edcensoUfFk->acronym ?> </li>
+        <li><?php echo $school->act_of_acknowledgement ?></li>
+        <!--<?php echo 'Email: '.(!empty($school->email) ? $school->email.' - ': (!empty($school->manager_email) ? $school->manager_email.' - ':'' ) ).'Tel: '.(!empty($school->phone_number) ? $school->phone_number:'' )?>-->
     </ul>
     <span class="clear"></span>
 
