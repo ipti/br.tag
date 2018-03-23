021704e42b7081ba320d106b2a59108e5df17fce
Jario Neto <jarioneto@gmail.com>
2018-03-23 16:43:50 -0300

    Atualizando branch

--- app/modules/quiz/widgets/QuizWidget.php
+++ app/modules/quiz/widgets/QuizWidget.php
@@ -4,30 +4,31 @@
 class QuizWidget extends CWidget{
 
     public $type;
-    public $model;
-    protected $view;
+    public $questions;
 
     public function init(){
-        switch ($this->type) {
-            case '1':
-                $this->view = 'subjective';
-                break;
-            case '2':
-                $this->view = 'ojective';
-                break;
-            case '3':
-                $this->view = 'ojectiveMultiple';
-                break;
-            case '4':
-                $this->view = 'selection';
-                break;
+
+        $quiz = Quiz::model()->findByPk($this->quiz);
+        $questions = $quiz->questions;
+        $groups = $quiz->questionsGroups;
+        $groupedQuestions = [];
+        $query = Yii::app()->db->createCommand()
+                    ->select()
+                    ->from('question q')
+                    ->join('quiz_question qq', 'q.id=qq.question_id')
+                    ->join('quiz qz', 'qz.id=qq.quiz_id')
+                    ->leftJoin('question_group qg', 'qz.id=qg.quiz_id')
+                    ->leftJoin('question_group_question qgq', 'qg.id=qgq.question_group_id AND q.id=qgq.question_id');
+
+        foreach ($questions as $question) {
+            foreach ($question->questionGroups as $group) {
+                $groupedQuestions[$group->id][] = $question;
+            }
         }
     }
 
     public function run(){
-        if(isset($this->view)){
-            $this->render($this->view, ['model' => $this->model]);
-        }
+        $this->render('form', ['questions' => $this->questions]);
     }
 }
 ?>
