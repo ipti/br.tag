e60ea48bbf5a7ce672470e985a47e0520dfa363f
ipti <ti@ipti.org.br>
2018-05-09 14:26:23 -0300

    Update config.php
    Atualizações do Censo 2018

--- config.php
+++ config.php
@@ -1,14 +1,21 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG',false);
-define("TAG_VERSION",'2.10.7f');
+define("TAG_VERSION",'2.10.7E');
 define("BOARD_MSG",'<div class="alert alert-success">
 		<button type="button" class="close" data-dismiss="alert">×</button>
-		<strong>TAG VERSÃO 2.10.7f:</strong>
+		<strong>TAG VERSÃO 2.10.7E:</strong>
 		<br>
 		<ul>
-		    <li>Novo Formulário -  Quadro de Notas.</li>
-		     <li>Novo Formulário - Atas de Notas.</li>
-        </ul>
+		    <li>Educacenso 2018</li>
+				<ul>
+					<li>Todas as escolas podem exportar os dados do TAG para o CENSO 2018</li>.
+					<li>No menu lateral acessar a opção Educacenso. Essa função validará todos os dados que serão enviados para o Educacenso</li>
+					<li>Caso algum registro não esteja correto, clicar na opção corrigir</li>
+					<li>Obs: Informar dados dos Alunos, Professores e Turmas por completo</li>
+					<li>Obs2: Verificar no relatório de transporte escolar a quantidade de alunos marcados com essa opção</li>
+					<li>Obs3: Lembrar de informar o professor que leciona na turma</li>
+				</ul>
+        	</ul>
 	</div>');
 if(YII_DEBUG){
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
