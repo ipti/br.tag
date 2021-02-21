bffa5e658f33efd814f802bf221e2dcc7cc2465b
RUANCELI SANTOS <ruan@celi.se>
2021-02-21 15:20:03 -0300

    Update config.php

--- config.php
+++ config.php
@@ -25,6 +25,6 @@ if(YII_DEBUG){
     define("YII_ENBLE_ERROR_HANDLER",false);
     define("YII_ENBLE_EXCEPTION_HANDLER",false);
 }
-date_default_timezone_set('America/Maceio');
+date_default_timezone_set('America/Sao_Paulo');
 ini_set('always_populate_raw_post_data','-1');
 setlocale(LC_ALL, 'portuguese','pt_BR.UTF-8','pt_BR.UTF8', 'pt_br.UTF8', 'ptb_BRA.UTF8',"ptb", 'ptb.UTF8');
