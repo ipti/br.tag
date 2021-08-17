c9188856c2acc93313ddbfafb1531e3ba0ffb07e
Jario Neto <jarioneto@gmail.com>
2021-08-17 14:07:39 -0300

    Ajustes na importaçao censo 2021

--- app/libraries/Educacenso/Register40.php
+++ app/libraries/Educacenso/Register40.php
@@ -5,7 +5,7 @@ class Register40
     {
         $registers = [];
 
-        array_push($registers, '40|'.Yii::app()->user->school.'|909999||1|2||1');
+        array_push($registers, '40|'.Yii::app()->user->school.'|909999||1|2|1');
 
         return $registers;
     }
