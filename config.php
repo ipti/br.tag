db90d6c4761c90e61ec0c8f145a11aedde183079
Ruanceli N Santos <ruanceli@gmail.com>
2022-06-18 14:52:59 -0300

    version update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', false);
-define("TAG_VERSION", '2.12.2');
+define("TAG_VERSION", '2.12.3');
 define("BOARD_MSG", '
 		
 		<div class="alert alert-success">
