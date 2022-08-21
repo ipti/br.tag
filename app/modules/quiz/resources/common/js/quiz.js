e5182171324279709d06a82b48aea6864ce682d8
Paulo <paulones89@gmail.com>
2022-08-21 10:32:44 -0300

    snyk update

--- app/modules/quiz/resources/common/js/quiz.js
+++ app/modules/quiz/resources/common/js/quiz.js
@@ -89,7 +89,7 @@ var Option = function(){
                                 .append($('<button></button>').attr({'class': 'btn btn-primary space-button font-button'}).text('Editar').click(Option.initUpdate))
                                 .append($('<button></button>').attr({'class': 'btn btn-primary space-button font-button'}).text('Excluir').click(Option.initDelete))
                             );
-                        container.append(element);
+                        container.append(DOMPurify.sanitize(element));
                         Option.clear();
                     }
                 })
@@ -213,7 +213,7 @@ var Option = function(){
                                 .append($('<button></button>').attr({'class': 'btn btn-primary space-button font-button'}).text('Editar').click(Option.initUpdate))
                                 .append($('<button></button>').attr({'class': 'btn btn-primary space-button font-button'}).text('Excluir').click(Option.initDelete))
                             );
-                container.append(element);
+                container.append(DOMPurify.sanitize(element));
             });
         },
         showComplement: function(){
@@ -294,7 +294,7 @@ var QuizQuestion = function(){
                             .append($('<td></td>').attr({'class': 'center-button'})
                                 .append($('<button></button>').attr({'class': 'btn btn-primary space-button font-button'}).text('Excluir').click(QuizQuestion.initDelete))
                             );
-                        container.append(element);
+                        container.append(DOMPurify.sanitize(element));
                         QuizQuestion.clear();
                     }
                     else if(typeof data.errorCode != 'undefined' && parseInt(data.errorCode) > 0){
@@ -370,7 +370,7 @@ var QuizQuestion = function(){
                             .append($('<td></td>').attr({'class': 'center-button'})
                                 .append($('<button></button>').attr({'class': 'btn btn-primary space-button font-button'}).text('Excluir').click(QuizQuestion.initDelete))
                             );
-                container.append(element);
+                container.append(DOMPurify.sanitize(element));
             });
         }
     }
