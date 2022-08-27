f2b025da717bb25cad923aa50b526d1bb367e982
Ruanceli N Santos <ruanceli@gmail.com>
2022-08-27 12:28:19 -0300

    NOVO DOCKERFILE COM COMPOSER CONFIGURADO E REMOÇÃO DA PASTA FRAMEWORK,IMAGES E DOCS

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', false);
-define("TAG_VERSION", '2.12.6');
+define("TAG_VERSION", '2.13.0'.'('.Yii::getVersion().')');
 define("BOARD_MSG", '
 		
 		<div class="alert alert-success">
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yii.php';
+//$yii=dirname(__FILE__).'/framework/yii.php';
+//$yii=dirname(__FILE__).'/../framework/yii.php';
+$yii=dirname(__FILE__).'/app/vendor/yiisoft/yii/framework/yii.php';
+
 $instance=dirname(__FILE__).'/instance.php';
 $configtag=dirname(__FILE__).'/config.php';
 $config=dirname(__FILE__).'/app/config/main.php';
 
+require_once($yii);
 require_once($configtag);
 require_once($instance);
-require_once($yii);
 Yii::createWebApplication($config)->run();
 
 
