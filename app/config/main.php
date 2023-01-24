103112c939a9e41ad3c62ba43bf3a7b206f716d8
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-01-24 10:02:29 -0300

    fix: Merge updates

--- app/config/main.php
+++ app/config/main.php
@@ -131,9 +131,9 @@ return array(
                 ),
                 // uncomment the following to show log messages on web pages
                 
-                  array(
-                  'class'=>'CWebLogRoute',
-                  ),
+                //   array(
+                //   'class'=>'CWebLogRoute',
+                //   ),
                 
             ),
         ),
