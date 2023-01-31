8ac13df2bd3cb2a3890ab4bc62e0ff9c659448b8
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-01-31 11:35:19 -0300

    hotfix: fix attempt for user enable/disable index action

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -9,7 +9,7 @@ class AdminController extends Controller
 				'allow',
 				'actions' => [
 					'CreateUser', 'index', 'conflicts', 'import', 'export', 'clearDB', 
-                    'DisableUser', 'ActiveUser', 'ActiveDisableUser', 'acl', 'backup', 'data', 'exportStudentIdentify', 'syncExport',
+                    'disableUser', 'activeUser', 'activeDisableUser', 'acl', 'backup', 'data', 'exportStudentIdentify', 'syncExport',
 					'syncImport', 'exportToMaster', 'clearMaster', 'importFromMaster'
 				], 'users' => ['@'],
 			],
