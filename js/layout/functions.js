4cfc16ff098f027a9f9b35a0da44368127321449
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-02-10 15:38:35 -0300

    fix: get language ptbr in datatable function is fixed

--- js/layout/functions.js
+++ js/layout/functions.js
@@ -1,9 +1,15 @@
-$.getScript('themes/default/js/datatablesptbr.js');
-
 var div = $("<div></div>").addClass("sidebar-cover");
 $("body").append(div);
 
 $(document).ready(function () {
+    if ($(".js-tag-table").length) {
+        $.getScript('themes/default/js/datatablesptbr.js', function () {
+            $(".js-tag-table").dataTable({
+                language: getLanguagePtbr(),
+                responsive: true
+            });
+        });
+    }
     $(".fullmenu-toggle-button").click(function () {
         $("#menu").toggleClass("hidden-menu");
         div.toggleClass("sidebar-cover")
@@ -16,11 +22,4 @@ $(document).ready(function () {
         $("#menu").toggleClass("hidden-menu");
         div.toggleClass("sidebar-cover")
     });
-
-    if($(".js-tag-table").length) {
-        $(".js-tag-table").dataTable({
-            language: getLanguagePtbr(),
-            responsive: true 
-        });
-    }
 });
