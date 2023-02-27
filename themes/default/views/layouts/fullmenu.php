d85c1d45ef708e64125ce809c56fa46b38768860
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-02-27 10:43:08 -0300

    refactor: clear cache function revenue

--- themes/default/views/layouts/fullmenu.php
+++ themes/default/views/layouts/fullmenu.php
@@ -1,6 +1,7 @@
 <?php
 $baseUrl = Yii::app()->theme->baseUrl;
 header('Cache-Control: no-cache, must-revalidate'); 
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>
