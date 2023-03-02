654acbb81cc084365dd5632b11e6c487b064c699
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-03-02 10:49:50 -0300

    refactor: quality code

--- js/datatables/init.js
+++ js/datatables/init.js
@@ -2,39 +2,28 @@ const action = window.location.search;
 $(document).ready(function () {
     if ($(".js-tag-table").length) {
         $.getScript('themes/default/js/datatablesptbr.js', function () {
+            var indexActionButtons;
             if(action.includes("school") 
             || action.includes("activeDisableUser")) {
-                $(".js-tag-table").dataTable({
-                    language: getLanguagePtbr(),
-                    responsive: true,
-                    columnDefs: [{
-                        orderable: false, targets: [2] 
-                    }],
-                });
+                indexActionButtons = 2;
             }
             if(action.includes("classroom") 
             || action.includes("instructor")
             || action.includes("manageUsers")) {
-                $(".js-tag-table").dataTable({
-                    language: getLanguagePtbr(),
-                    responsive: true,
-                    columnDefs: [{
-                        orderable: false, targets: [3] 
-                    }],
-                });
+                indexActionButtons = 3;
             }
             if(action.includes("student") 
             || action.includes("curricularmatrix") 
             || action.includes("courseplan")) {
-                $(".js-tag-table").dataTable({
-                    language: getLanguagePtbr(),
-                    responsive: true,
-                    columnDefs: [{
-                        orderable: false, targets: [4] 
-                    }],
-                });
-            }         
-
+                indexActionButtons = 4;
+            }
+            $(".js-tag-table").dataTable({
+                language: getLanguagePtbr(),
+                responsive: true,
+                columnDefs: [{
+                    orderable: false, targets: [indexActionButtons] 
+                }],
+            });
 
         });
     }
