06e393a34532697039fbaa8af7461c5a5dc40e2c
Tori Calasans <tori.ti@ipti.org.br>
2023-03-07 11:10:55 -0300

    finalizado cards

--- themes/default/views/wizard/configuration/index.php
+++ themes/default/views/wizard/configuration/index.php
@@ -17,54 +17,55 @@ $this->breadcrumbs = array(
 
 );
 ?>
-
-<div class="row-fluid">
-    <div class="span12">
-        <h1>
-            <?php echo Yii::t('default', 'Configurarion'); ?>
-        </h1>
-    </div>
-</div>
-<div class="innerLR">
-    <?php if (Yii::app()->user->hasFlash('success')) : ?>
-        <div class="alert alert-success">
-            <?php echo Yii::app()->user->getFlash('success') ?>
+<div id="mainPage" class="main">
+    <div class="row-fluid">
+        <div class="span12">
+            <h1>
+                <?php echo Yii::t('default', 'Configurarion'); ?>
+            </h1>
         </div>
-    <?php endif ?>
-    <div class="widget widget-tabs border-bottom-none">
-        <div class="widget-body form-horizontal">
-            <div class="tab-content">
-                <div class="tab-pane row active" id="student">
-                    <div class="column">
-                        <div class="row">
-                            <div class="t-cards">
-                                <a href="<?php echo yii::app()->createUrl('wizard/Configuration/school') ?>" class="t-cards-link">
-                                    <div class="t-cards-title"><img class="t-cards-icon" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/escola.svg" />Escola</div>
-                                    <div class="t-cards-text">Edite as informações da sua escola</div>
-                                </a>
+    </div>
+    <div class="innerLR">
+        <?php if (Yii::app()->user->hasFlash('success')) : ?>
+            <div class="alert alert-success">
+                <?php echo Yii::app()->user->getFlash('success') ?>
+            </div>
+        <?php endif ?>
+        <div class="widget widget-tabs border-bottom-none">
+            <div class="widget-body form-horizontal">
+                <div class="tab-content">
+                    <div class="tab-pane row active" id="student">
+                        <div class="column">
+                            <div class="row">
+                                <div class="t-cards">
+                                    <a href="<?php echo yii::app()->createUrl('wizard/Configuration/school') ?>" class="t-cards-link">
+                                        <div class="t-cards-title"><img class="t-cards-icon" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/escola.svg" />Escola</div>
+                                        <div class="t-cards-text">Edite as informações da sua escola</div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="t-cards">
-                                <a href="<?php echo yii::app()->createUrl('wizard/Configuration/classroom') ?>" class="t-cards-link">
-                                    <div class="t-cards-title"><img class="t-cards-icon" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/turma.svg" />Turmas</div>
-                                    <div class="t-cards-text">Faça o reaproveitamento de turmas</div>
-                                </a>
+                            <div class="row">
+                                <div class="t-cards">
+                                    <a href="<?php echo yii::app()->createUrl('wizard/Configuration/classroom') ?>" class="t-cards-link">
+                                        <div class="t-cards-title"><img class="t-cards-icon" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/turma.svg" />Turmas</div>
+                                        <div class="t-cards-text">Faça o reaproveitamento de turmas</div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="t-cards">
-                                <a href="<?php echo yii::app()->createUrl('wizard/Configuration/student') ?>" class="t-cards-link">
-                                    <div class="t-cards-title"><img class="t-cards-icon" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/aluno.svg" />Aluno</div>
-                                    <div class="t-cards-text">Realize o reaproveitamento de alunos</div>
-                                </a>
+                            <div class="row">
+                                <div class="t-cards">
+                                    <a href="<?php echo yii::app()->createUrl('wizard/Configuration/student') ?>" class="t-cards-link">
+                                        <div class="t-cards-title"><img class="t-cards-icon" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/aluno.svg" />Aluno</div>
+                                        <div class="t-cards-text">Realize o reaproveitamento de alunos</div>
+                                    </a>
+                                </div>
                             </div>
+                            <!-- <div class="column"></div> -->
                         </div>
-                        <!-- <div class="column"></div> -->
                     </div>
                 </div>
+                <?php $this->endWidget(); ?>
             </div>
-            <?php $this->endWidget(); ?>
         </div>
     </div>
 </div>
