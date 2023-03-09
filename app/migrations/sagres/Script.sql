eb2fae8d3d9002ef3404db652bd29e0e9285fdfe
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-03-09 17:31:09 -0300

    fix: query sql for sagres tables is working

--- app/migrations/sagres/Script.sql
+++ app/migrations/sagres/Script.sql
@@ -6,12 +6,8 @@ CREATE TABLE `attendance` (
   `local` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `professional_fk` int(11) NOT NULL,
   PRIMARY KEY (`id_attendance`),
-  KEY `attendance_FK` (`professional_fk`),
-  KEY `attendance_FK_1` (`local`),
-  CONSTRAINT `attendance_FK` FOREIGN KEY (`professional_fk`) REFERENCES `professional` (`id_professional`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `attendance_FK` (`professional_fk`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-INSERT INTO attendance VALUES (1, curdate(), 'Itabaiana', 1);
 
 -- `io.escola.demo`.professional definition
 
@@ -22,22 +18,21 @@ CREATE TABLE `professional` (
   `speciality_fk` int(11) NOT NULL,
   `inep_id_fk` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
   `fundeb` tinyint(1) NOT NULL,
-  `attendance_fk` int(11) NOT NULL,
   PRIMARY KEY (`id_professional`),
   KEY `professional_FK` (`inep_id_fk`),
-  KEY `professional_FK_1` (`speciality_fk`),
-  KEY `professional_FK_2` (`attendance_fk`),
-  CONSTRAINT `professional_FK` FOREIGN KEY (`inep_id_fk`) REFERENCES `school_identification` (`inep_id`),
-  CONSTRAINT `professional_FK_1` FOREIGN KEY (`speciality_fk`) REFERENCES `edcenso_professional_education_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `professional_FK_2` FOREIGN KEY (`attendance_fk`) REFERENCES `attendance` (`id_attendance`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `professional_FK_1` (`speciality_fk`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO professional VALUES (1, 'JOAO DA SILVA', '71685776035', '1001', '28022122', 1);
+INSERT INTO attendance VALUES (1, curdate(), 'Itabaiana', 1);
+
+ALTER TABLE `professional` ADD CONSTRAINT `professional_FK` FOREIGN KEY (`inep_id_fk`) REFERENCES `school_identification` (`inep_id`);
+ALTER TABLE `professional` ADD CONSTRAINT `professional_FK_1` FOREIGN KEY (`speciality_fk`) REFERENCES `edcenso_professional_education_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `attendance` ADD CONSTRAINT `attendance_FK` FOREIGN KEY (`professional_fk`) REFERENCES `professional` (`id_professional`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- ALTER TABLE `student_enrollment` ADD `date_cancellation_enrollment` DATE NULL DEFAULT NULL AFTER `status`;
 -- ALTER TABLE `school_identification` ADD `number_ato` VARCHAR(30) NOT NULL AFTER `final_date`;
 -- ALTER TABLE `lunch_menu` ADD `adjusted` TINYINT NOT NULL AFTER date;
-
-
-INSERT INTO professional VALUES (1, 'JOAO DA SILVA', '71685776035', '1001', '28022122', 1, 1);
 
 -- UPDATE student_identification
 -- SET birthday = STR_TO_DATE(birthday, '%Y-%m-%d');
