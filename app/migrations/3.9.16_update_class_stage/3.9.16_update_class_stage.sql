45f6185f4d4aa6cb7808f2a6b3ad60154e659220
Igor Gonçalves <igor_goncalves@live.com>
2023-03-29 13:13:09 -0300

    Update 3.9.16_update_class_stage.sql

--- app/migrations/3.9.16_update_class_stage/3.9.16_update_class_stage.sql
+++ app/migrations/3.9.16_update_class_stage/3.9.16_update_class_stage.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `br-ong-tag-demo`.classroom MODIFY COLUMN edcenso_stage_vs_modality_fk varchar(6) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
+ALTER TABLE classroom MODIFY COLUMN edcenso_stage_vs_modality_fk varchar(6) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
