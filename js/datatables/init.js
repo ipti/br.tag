5c7d14a4d74d98bcc58f2d495f87e28f9208bd83
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-04-03 11:59:25 -0300

    refactor: remove unused code

--- js/datatables/init.js
+++ js/datatables/init.js
@@ -2,52 +2,50 @@ const action = window.location.search;
 console.log(`${window.location}?r=student/getstudentajax`);
 $(document).ready(function () {
 
-    if($(".js-tag-table").has(".empty").length > 0){
+    if ($(".js-tag-table").has(".empty").length > 0) {
         return;
     }
     if ($(".js-tag-table").length) {
         const isMobile = window.innerWidth <= 768;
         const numColumns = $(".js-tag-table th").length;
-        const columnsIndex = new Array(numColumns-1).fill(1).map( (_, i) => i+1);
+        const columnsIndex = new Array(numColumns - 1).fill(1).map((_, i) => i + 1);
 
 
         $.getScript('themes/default/js/datatablesptbr.js', function () {
             let indexActionButtons;
-            if(action.includes("school") 
-            || action.includes("activeDisableUser")) {
+            if (action.includes("school")
+                || action.includes("activeDisableUser")) {
                 indexActionButtons = 2;
             }
-            if(action.includes("classroom") 
-            || action.includes("instructor")
-            || action.includes("manageUsers")) {
+            if (action.includes("classroom")
+                || action.includes("instructor")
+                || action.includes("manageUsers")) {
                 indexActionButtons = 3;
             }
-            if(action.includes("student") 
-            || action.includes("curricularmatrix") 
-            || action.includes("courseplan")
-            || action.includes("professional")) {
+            if (action.includes("student")
+                || action.includes("curricularmatrix")
+                || action.includes("courseplan")
+                || action.includes("professional")) {
                 indexActionButtons = 4;
             }
-            if(action.includes("student")) {
-                $(document).ready(function () {
-                    $(".js-tag-table").DataTable({
-                        language: getLanguagePtbr(),
-                        serverSide: true,
-                        responsive: true,
-                        ajax: {
-                            url: `${window.location}/getstudentajax`,
-                            type: "POST",
-                            data: function (d) {
-                                d.page = parseInt(d.start / d.length) + 1;
-                                d.perPage = d.length;
-                                d.search = { value: $('input[type="search"]').val() };
-                                return d;
-                            }
-                        },
-                        searching: true,
-                    });
+            if (action.includes("student")) {
+                $(".js-tag-table").DataTable({
+                    language: getLanguagePtbr(),
+                    serverSide: true,
+                    responsive: true,
+                    ajax: {
+                        url: `${window.location}/getstudentajax`,
+                        type: "POST",
+                        data: function (d) {
+                            d.page = parseInt(d.start / d.length) + 1;
+                            d.perPage = d.length;
+                            d.search = { value: $('input[type="search"]').val() };
+                            return d;
+                        }
+                    },
+                    searching: true,
                 });
-            }else {
+            } else {
                 $(".js-tag-table").dataTable({
                     language: getLanguagePtbr(),
                     responsive: true,
