d5bd337ff45e151852d2a24de1645025100dd5ff
Tori Calasans <tori.ti@ipti.org.br>
2023-04-20 12:08:32 -0300

    feito cards de quetionario, falta ajustes

--- themes/default/views/quiz/default/index.php
+++ themes/default/views/quiz/default/index.php
@@ -20,39 +20,71 @@ $this->breadcrumbs = array(
             <h1><?php echo Yii::t('default', 'Quiz'); ?></h1>
         </div>
     </div>
-
-    <div class="innerLR home">
-        <div class="row-fluid">
-            <div class="span12">
-                <div class="span2">
-                    <a href="<?php echo Yii::app()->createUrl('quiz/default/quiz') ?>" class="widget-stats">
-                        <span class="fa-list-ol fa fa-4x block"><i></i></span>
-                        <span class="txt">Questionários</span>
-                        <div class="clearfix"></div>
-                    </a>
+    <div class="widget widget-tabs border-bottom-none">
+        <div class="widget-body form-horizontal">
+            <div class="tab-content">
+                <div class="tab-pane row active">
+                    <div class="column">
+                        <div class="row">
+                            <div class="t-cards">
+                                <a href="<?php echo Yii::app()->createUrl('quiz/default/quiz') ?>" class="t-cards-link">
+                                    <div class="t-cards-title">
+                                        <span class="t-icon-trash"></span>
+                                        Questionários
+                                    </div>
+                                    <div class="t-cards-text">Cadastre uma Unidade gestora.</div>
+                                    <!-- <img class="t-icon-trash" alt="icone escola card" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/escola.svg" /> -->
+                                    <!-- <span class="fa-list-ol fa fa-4x block"><i></i></span>
+                                    <span class="txt">Questionários</span>
+                                    <div class="clearfix"></div> -->
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="t-cards">
+                                <a href="<?php echo Yii::app()->createUrl('quiz/default/question') ?>" class="t-cards-link">
+                                    <div class="t-cards-title">
+                                        <span class="t-icon-trash"></span>
+                                        Perguntas
+                                    </div>
+                                    <div class="t-cards-text">Exporte os dados da Unidade.</div>
+                                    <!-- <span class="fa fa-edit fa-4x block"></span>
+                                    <span class="txt">Perguntas</span>
+                                    <div class="clearfix"></div> -->
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="t-cards">
+                                <a href="<?php echo Yii::app()->createUrl('quiz/default/group') ?>" class="t-cards-link">
+                                    <div class="t-cards-title">
+                                        <span class="t-icon-trash"></span>
+                                        Grupos de Perguntas
+                                    </div>
+                                    <div class="t-cards-text">Atualize os dados da Unidade.</div>
+                                    <!-- <span class="fa fa-archive fa-4x block"><i></i></span>
+                                    <span class="txt">Grupos de Perguntas</span>
+                                    <div class="clearfix"></div> -->
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="t-cards">
+                                <a href="<?php echo Yii::app()->createUrl('quiz/default/questionGroup') ?>" class="t-cards-link">
+                                    <div class="t-cards-title">
+                                        <span class="t-icon-trash"></span>
+                                        Agrupar Perguntas
+                                    </div>
+                                    <div class="t-cards-text">Atualize os dados da Unidade.</div>
+                                    <!-- <span class="fa fa-list fa-4x block"><i></i></span>
+                                    <span class="txt">Agrupar Perguntas</span>
+                                    <div class="clearfix"></div> -->
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="span2">
-                    <a href="<?php echo Yii::app()->createUrl('quiz/default/question') ?>" class="widget-stats">
-                        <span class="fa fa-edit fa-4x block"></span>
-                        <span class="txt">Perguntas</span>
-                        <div class="clearfix"></div>
-                    </a>
-                </div>
-                <div class="span2">
-                    <a href="<?php echo Yii::app()->createUrl('quiz/default/group') ?>" class="widget-stats">
-                        <span class="fa fa-archive fa-4x block"><i></i></span>
-                        <span class="txt">Grupos de Perguntas</span>
-                        <div class="clearfix"></div>
-                    </a>
-                </div>
-                <div class="span2">
-                    <a href="<?php echo Yii::app()->createUrl('quiz/default/questionGroup') ?>" class="widget-stats">
-                        <span class="fa fa-list fa-4x block"><i></i></span>
-                        <span class="txt">Agrupar Perguntas</span>
-                        <div class="clearfix"></div>
-                    </a>
-                </div>
-            </div> <!-- .span12 -->
-        </div> <!-- .row-fluid -->
-    </div> <!-- .innerLr -->
+            </div>
+        </div>
+    </div>
 </div>
