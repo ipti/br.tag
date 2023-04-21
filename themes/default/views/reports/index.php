caf64aa9211dcfc4298e34267f6422d203538503
Carlos <109424074+CarlosDcomp@users.noreply.github.com>
2023-04-20 21:47:08 -0300

    fix: model label

--- themes/default/views/reports/index.php
+++ themes/default/views/reports/index.php
@@ -388,9 +388,9 @@ $this->breadcrumbs = array(
                                 <select name="classroom_student" id="classroom_student" style="width: 100%;" required>
                                     <option value="">Selecione a Turma</option>
                                 </select>
-                                <label for="model_quartely" class="control-label"></label>
+                                <label for="model_quartely" class="control-label" style="width: 100%;">Modelo de Eixos e Campos de Experiência</label>
                                 <select name="model_quartely" id="model_quartely" style="width: 100%;" required>
-                                    <option value="">Selecione o modelo de relatório</option>
+                                    <option value="">Selecione o modelo</option>
                                     <option value="1">1º ANO</option>
                                     <option value="2">2º ANO</option>
                                     <option value="3">3º ANO</option>
