83c2e9f89322721602483e09345c6b722c42a0c2
gabriel-IPTI <115715913+gabriel-IPTI@users.noreply.github.com>
2023-05-05 14:50:21 -0300

    alterando módulo de matrixcurricular para exibir as mensagens de erro (#289)

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 // defined('YII_DEBUG') or define('YII_DEBUG', false);
 
-define("TAG_VERSION", '3.16.38');
+define("TAG_VERSION", '3.16.39');
 
 
 define("YII_VERSION", Yii::getVersion());
