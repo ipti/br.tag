e0c98f38b7134bfe64b01cf4e131a7759cef1416
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-05-26 11:07:59 -0300

    fix: curricular component select in class content and frequency module

--- themes/default/views/classes/classContents.php
+++ themes/default/views/classes/classContents.php
@@ -119,12 +119,12 @@ $school = SchoolIdentification::model()->findByPk(Yii::app()->user->school);
                 ?>
 
             </div>
-            <div class="disciplines-container" style="display: none;">
-                <?php echo CHtml::label(yii::t('default', 'Discipline') . " *", 'disciplines', array('class' => 'control-label required', 'style' => 'width: 85px;')); ?>
+            <div class="disciplines-container" style="display: none;width: 35%;">
+                <?php echo CHtml::label(yii::t('default', 'Discipline') . " *", 'disciplines', array('class' => 'control-label required')); ?>
                 <?php
                 echo CHtml::dropDownList('disciplines', '', array(), array(
                     'key' => 'id',
-                    'class' => 'select-search-on control-input classContents-input',
+                    'class' => 'select-search-on control-input classContents-input-curricular-component',
                 ));
                 ?>
             </div>
