dc448d4e1abb38eeed51f355febab8a80fcf3b8c
Nathan Santos <117388330+nathanipti@users.noreply.github.com>
2023-05-31 16:29:25 -0300

    Fix: removal of the responsible person's cpf masks (#339)
    * Feat: add validation for DiretorTType
    
    * Fix: correction in getSchedules
    
    * Feat: added verification for status student aproved
    
    * Feat: Add actionInconsistencySagres function to controller
    
    * Feat: Add HTML code for Inconsistency Sagres page
    
    * Fix: Update URL for Inconsistency Sagres page
    
    * Fix: removed school duplication in $schoolList array at end of iteration in foreach loop
    
    * Feat: add tbody for ValidationSagresModel
    
    * Feat: Add ValidationSagresModel class
    
    * Feat: add SagresValidations class
    
    * Feat: added validator for sagres
    
    * Fix: remove invalid shift duplicate msg from school
    
    * Feat: add validations for menu
    
    * Feat: add function validatorClass
    
    * Feat: added css class for notification of number of inconsistencies
    
    * Feat: get the number of inconsistencies
    
    * Feat: Added code snippet to display the number of inconsistencies
    
    * Feat: Added a function to retrieve the count of inconsistencies from the "inconsistency_sagres"
    
    * Feat: added validation for Series and Enrollments
    
    * Feat: add validations for validationStudent
    
    * Feat: add validation for validatorSchedules
    
    * Feat: added typecast to guarantee the return type
    
    * Feat: added validations for getFinalTurma
    
    * Feat: added maxLength validation for course name
    
    * Feat: Check the existence of objects
    
    * Feat: Checks for existence of director and menu objects
    
    * Feat: added validation for Attendance
    
    * Feat: add sql for creation table inconsistency_sagres
    
    * Fix: modification for validation sagres objects
    
    * Fix: corrected alias for create_date
    
    * Fix: added validation based on a minimum year
    
    * Fix: Changed format date for better viewing
    
    * Feat: added validation for status aproved student
    
    * Fix: Fix in function getStudentSituation
    
    * Refactor getStudentSituation() function to access the index and return the corresponding situation
    
    * Feat: added validation for year in date
    
    * Fix: correction in ORDER BY c.create_date
    
    * Fix: Modified export function to be based on previous year and month
    
    * Fix: Updated actionExport parameters for month selection
    
    * Fix: queries modified to be selected by the current year and month prior to the current one
    
    * Fix: menu validation removed as optional based on Education.xsd, (minOccurs="0")
    
    * Fix: Remove unnecessary validations for selected ranges
    
    * Feat: added CSS to display a pop-up of the sagres xml file loading
    
    * Feat: Added loading element
    
    * Fix: added validation for optional class description basead in Education.xsd (minOccurs="0")
    
    * Feat: added validation for invalid cpf of professionals
    
    * Fix: query selection modified to select data up to the current day
    
    * Fix: validated menu attributes if they have unexpected values
    
    * Fix: Enhanced msg to report the absence of a teacher or curricular components for the class
    
    * Fix: add validation for maxLength disciplina
    
    * Fix: correction of the approved student status
    
    * Feat: added var for verification of approved status for validation message
    
    * Fix: Removed use of unused functions (getRecentEnrollments and getRecentSchedules)
    
    * Fix: added filtering of students by year
    
    * Fix: removed unused functions
    
    * Fix: Correction of student status
    
    * Fix: Correction on namespace sagres
    
    * Fix: correction in cpf type
    
    * Fix: removal of the responsible person's cpf masks
    
    ---------
    
    Co-authored-by: Nathan <nathanaelsantos15@gmail.com>

--- app/modules/sagres/models/SagresConsultModel.php
+++ app/modules/sagres/models/SagresConsultModel.php
@@ -323,7 +323,7 @@ class SagresConsultModel
                 ->setDuracao(2)
                 ->setHoraInicio($this->getStartTime($schedule['schedule'], $this->convertTurn($schedule['turn'])))
                 ->setDisciplina(substr($schedule['disciplineName'], 0, 50))
-                ->setCpfProfessor([$schedule['cpfInstructor']]);
+                ->setCpfProfessor([str_replace([".", "-"], "", $schedule['cpfInstructor'])]);
 
             $scheduleList[] = $scheduleType;
         }
@@ -586,7 +586,7 @@ class SagresConsultModel
         foreach ($professionals as $professional) {
             $professionalType = new ProfissionalTType();
             $professionalType
-                ->setCpfProfissional($professional['cpfProfissional'])
+                ->setCpfProfissional(str_replace([".", "-"], "", $professional['cpfProfissional']))
                 ->setEspecialidade($professional['especialidade'])
                 ->setIdEscola($professional['idEscola'])
                 ->setFundeb($professional['fundeb'])
