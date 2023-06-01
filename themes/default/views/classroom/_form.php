572eb1837dfea2f79e6757774ffde1548d904b0d
Tori Calasans <tori.ti@ipti.org.br>
2023-06-01 09:41:13 -0300

    adcionado novos ícones

--- themes/default/views/classroom/_form.php
+++ themes/default/views/classroom/_form.php
@@ -143,6 +143,7 @@ $form = $this->beginWidget('CActiveForm', array(
                     </div>
                     <div>
                         <h3>Horário de funcionamento</h3>
+                        <span class="t-hospital-user"></span>
                     </div>
                     <div class="row">
                         <div class="column">
