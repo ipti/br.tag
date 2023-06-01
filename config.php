c247093570655df3bdef4079ead468ddc32df093
Igor Gonçalves <igor_goncalves@live.com>
2023-05-31 22:51:00 -0300

    fix: change version

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-// defined('YII_DEBUG') or define('YII_DEBUG', false);
+// defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', false);
 
-define("TAG_VERSION", '3.24.57');
+define("TAG_VERSION", '3.24.58');
 
 
 define("YII_VERSION", Yii::getVersion());
