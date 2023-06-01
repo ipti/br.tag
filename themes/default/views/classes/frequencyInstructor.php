80c01111139e20639ccb49eebcdf05033d8c01be
Tori Calasans <tori.ti@ipti.org.br>
2023-06-01 09:12:08 -0300

    alterado tela de fraquência

--- themes/default/views/classes/frequencyInstructor.php
+++ themes/default/views/classes/frequencyInstructor.php
@@ -1,8 +1,16 @@
 <?php
 
+/** 
+ * @var ClassesController $this ClassesController
+ * @var CActiveDataProvider $dataProvider CActiveDataProvider
+ */
+
 $baseUrl = Yii::app()->baseUrl;
 $themeUrl = Yii::app()->theme->baseUrl;
 $cs = Yii::app()->getClientScript();
+$cs->registerScriptFile($baseUrl . '/js/classes/frequency/_initialization.js?v=1.0', CClientScript::POS_END);
+$cs->registerCssFile($themeUrl . '/css/template2.css');
+$cs->registerCssFile($baseUrl . 'sass/css/main.css');
 $this->setPageTitle('TAG - ' . Yii::t('default', 'Classes'));
 
 $school = SchoolIdentification::model()->findByPk(Yii::app()->user->school);
@@ -15,11 +23,52 @@ $school = SchoolIdentification::model()->findByPk(Yii::app()->user->school);
         'enableAjaxValidation' => false,
         'action' => CHtml::normalizeUrl(array('classes/saveFrequency')),
     ));
+
     ?>
+    <div class="row-fluid">
+        <div class="span12">
+            <h1><?php echo Yii::t('default', 'Frequency'); ?></h1>
+            <h5> Marcar apenas faltas.</h5>
+        </div>
+    </div>
+
+    <table class="table table-bordered table-striped visible-print">
+        <tr>
+            <th>Escola:</th>
+            <td colspan="7"><?php echo $school->inep_id . " - " . $school->name ?></td>
+        <tr>
+        <tr>
+            <th>Estado:</th>
+            <td colspan="2"><?php echo $school->edcensoUfFk->name . " - " . $school->edcensoUfFk->acronym ?></td>
+            <th>Municipio:</th>
+            <td colspan="2"><?php echo $school->edcensoCityFk->name ?></td>
+            <th>Endereço:</th>
+            <td colspan="2"><?php echo $school->address ?></td>
+        <tr>
+        <tr>
+            <th>Localização:</th>
+            <td colspan="2"><?php echo ($school->location == 1 ? "URBANA" : "RURAL") ?></td>
+            <th>Dependência Administrativa:</th>
+            <td colspan="4"><?php
+                            $ad = $school->administrative_dependence;
+                            echo ($ad == 1 ? "FEDERAL" : ($ad == 2 ? "ESTADUAL" : ($ad == 3 ? "MUNICIPAL" :
+                                "PRIVADA")));
+                            ?></td>
+        <tr>
+    </table>
+    <br>
+
     <div class="tag-inner">
+
+        <?php if (Yii::app()->user->hasFlash('success')) : ?>
+            <div class="alert alert-success">
+                <?php echo Yii::app()->user->getFlash('success') ?>
+            </div>
+        <?php endif ?>
         <div class="alert-required-fields no-show alert alert-error">
             Os Campos com * são obrigatórios.
         </div>
+        <!-- Mês e componente curricular -->
         <div class="mobile-row">
             <!-- Mês -->
             <div class="column helper">
@@ -72,6 +121,48 @@ $school = SchoolIdentification::model()->findByPk(Yii::app()->user->school);
                 <img class="loading-frequency" style="display:none;margin: 10px 20px;" height="30px" width="30px" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/loadingTag.gif" alt="TAG Loading">
             </div>
         </div>
+        <div class="alert-incomplete-data alert alert-warning display-hide"></div>
+        <div id="frequency-container" class="t-tabs table-responsive">
+
+        </div>
     </div>
     <?php $this->endWidget(); ?>
+
+</div>
+
+<div class="modal fade modal-content" id="save-justification-modal" tabindex="-1" role="dialog" aria-labelledby="Save Justification">
+    <div class="modal-dialog" role="document">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="position:static;">
+                <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/Close.svg" alt="" style="vertical-align: -webkit-baseline-middle">
+            </button>
+            <h4 class="modal-title" id="myModalLabel">Justificativa</h4>
+        </div>
+        <div class="centered-loading-gif">
+            <i class="fa fa-spin fa-spinner"></i>
+        </div>
+        <form method="post">
+            <div class="modal-body">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <?= chtml::label("Justificativa", "title", array('class' => 'control-label')); ?>
+                        <div class="form-control">
+                            <input type="hidden" id="justification-classroomid">
+                            <input type="hidden" id="justification-studentid">
+                            <input type="hidden" id="justification-day">
+                            <input type="hidden" id="justification-month">
+                            <input type="hidden" id="justification-schedule">
+                            <input type="hidden" id="justification-fundamentalmaior">
+                            <textarea class="justification-text span12"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="cancel-save-justifiaction btn btn-default" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary btn-save-justification">Adicionar</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
