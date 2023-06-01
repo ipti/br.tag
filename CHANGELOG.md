395ba47cd13236a19a78867a3e555f7413f188d9
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-06-01 10:05:30 -0300

    Fix/generate RA API (#341)
    * fix: change method response
    
    * fix: loading students margin
    
    * refactor: update localhost DB
    
    * refactor: update TAG version
    
    * fix: code smeels
    
    * fix: improve exception
    
    ---------
    
    Co-authored-by: Igor Gonçalves <igor_goncalves@live.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Atualizações
 
+## [Versão 3.24.59]
+- Um bug que fazia gerar RA não funcionar corretamente foi corrigido
+
 ## [Versão 3.24.58]
 - Adicionamento versão para arquivos css, resolvendo problema
 com cache do style
--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 
-define("TAG_VERSION", '3.24.58');
+define("TAG_VERSION", '3.24.59');
 
 
 define("YII_VERSION", Yii::getVersion());
