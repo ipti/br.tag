506d0c24d8a68587037807e170fce308dab684e2
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-06-07 12:33:54 -0300

    hotfix: name discipline function

--- themes/default/views/forms/EnrollmentGradesReport.php
+++ themes/default/views/forms/EnrollmentGradesReport.php
@@ -7,81 +7,53 @@ $cs->registerScriptFile($baseUrl . '/js/reports/EnrollmentGradesReport/_initiali
 
 $this->setPageTitle('TAG - ' . Yii::t('default', 'Reports'));
 
-// Tratamento do cabeçalho das disciplinas
-function classroomDisciplineLabelResumeArray($discipline_id)
-{
-    $label = '';
-    if ($discipline_id == 1)  $label = "Química";
-    if ($discipline_id == 2)  $label = "Física";
-    if ($discipline_id == 3)  $label = "Matemática";
-    if ($discipline_id == 4)  $label = "Biologia";
-    if ($discipline_id == 5)  $label = "Ciências";
-    if ($discipline_id == 6)  $label = "Português";
-    if ($discipline_id == 7)  $label = "Inglês";
-    if ($discipline_id == 8)  $label = "Espanhol";
-    if ($discipline_id == 9)  $label = "Outro Idioma";
-    if ($discipline_id == 10)  $label = "Artes";
-    if ($discipline_id == 11)  $label = "Edicação Física";
-    if ($discipline_id == 12)  $label = "História";
-    if ($discipline_id == 13)  $label = "Geografia";
-    if ($discipline_id == 14)  $label = "Filosofia";
-    if ($discipline_id == 16)  $label = "Informática";
-    if ($discipline_id == 17)  $label = "Disc. Profissionalizante";
-    if ($discipline_id == 20)  $label = "Educação Especial";
-    if ($discipline_id == 21)  $label = "Sociedade&nbspe Cultura";
-    if ($discipline_id == 23)  $label = "Libras";
-    if ($discipline_id == 25)  $label = "Pedogogia";
-    if ($discipline_id == 26)  $label = "Ensino Religioso";
-    if ($discipline_id == 27)  $label = "Língua Nativa";
-    if ($discipline_id == 28)  $label = "Estudo Social";
-    if ($discipline_id == 29)  $label = "Sociologia";
-    if ($discipline_id == 30)  $label = "Francês";
-    if ($discipline_id == 99)  $label = "Outras";
-    if ($discipline_id == 10001)  $label = "Redação";
-    if ($discipline_id == 10002)  $label = "Linguagem oral e escrita";
-    if ($discipline_id == 10003)  $label = "Natureza e sociedade";
-    if ($discipline_id == 10004)  $label = "Movimento";
-    if ($discipline_id == 10005)  $label = "Música";
-    if ($discipline_id == 10006)  $label = "Artes visuais";
-    if ($discipline_id == 10007)  $label = "Escuta, Fala, Pensamento e Imaginação";
-    if ($discipline_id == 10008)  $label = "Espaço, Tempo, Quantidade, Relações e Transformações";
-    if ($discipline_id == 10009)  $label = "Corpo, Gesto e Movimento";
-    if ($discipline_id == 10010)  $label = "Traços, Sons, Cores e Formas";
-    if ($discipline_id == 10011)  $label = "O Eu, O Outro e o Nós";
-    if ($discipline_id == 10012)  $label = "Manifestações Culturais e Artísticas Globais e Regionais";
-    if ($discipline_id == 10013)  $label = "Gestão Sustentável de Detinos Turísticos";
-    if ($discipline_id == 10014)  $label = "Lazer, Esporte e Trabalho";
-    if ($discipline_id == 10015)  $label = "Inglês Instrumental";
-    if ($discipline_id == 10016)  $label = "Espanhol Instrumental";
-    if ($discipline_id == 10017)  $label = "Georgrafia Turística";
-    if ($discipline_id == 10018)  $label = "Desafios contemporâneos: Do Global ao Local";
-    if ($discipline_id == 10019)  $label = "História Regional";
-    if ($discipline_id == 10020)  $label = "Sociedade Buziana";
-    if ($discipline_id == 10021)  $label = "Antropologia Sociocultural";
-    if ($discipline_id == 10022)  $label = "Patrimônios Culturais";
-    if ($discipline_id == 10023)  $label = "Literatura na Era Digital";
-    if ($discipline_id == 10024)  $label = "Educação Financeira";
-    if ($discipline_id == 10025)  $label = "Expressão Oral e Escrita";
-    if ($discipline_id == 10026)  $label = "Projeto de Vida II";
-    if ($discipline_id == 10027)  $label = "Língua Espanhola";
-    if ($discipline_id == 10028)  $label = "Análise e Experimentação científica";
-    if ($discipline_id == 10029)  $label = "Consciência Ecológica e Educação Ambiental";
-    if ($discipline_id == 10030)  $label = "Ciências, Tecnologia e Sociedade";
-    if ($discipline_id == 10031)  $label = "Pensamento Lógico-Matemático";
-    if ($discipline_id == 10032)  $label = "Cálculo";
-    if ($discipline_id == 10033)  $label = "Sustentabilidade";
-    if ($discipline_id == 10034)  $label = "Eficiência Energética";
-    if ($discipline_id == 10035)  $label = "Corpo e Movimento";
-    if ($discipline_id == 10036)  $label = "Geometria";
-    if ($discipline_id == 10037)  $label = "Corpo e Saúde";
-    if ($discipline_id == 10038)  $label = "Ciências Biológicas, Agrárias e da Saúde";
-    if ($discipline_id == 10039)  $label = "Projeto de vida I";
-    if ($discipline_id == 10040)  $label = "Projeto de Leitura";
-    if ($discipline_id == 10041)  $label = "Pluralidade Cultural";
-    if ($discipline_id == 10042)  $label = "Estudo Dirigido: Atividade de Lingua Portuguesa e de Matmática";
-    if ($discipline_id == 10043)  $label = "Atividades Esportivas e Motoras";
-    
-    return $label;
+function classroomDisciplineLabelResumeArray($id) {
+    $disciplinas = array(
+        1 => 'Química',
+        2 => 'Física',
+        3 => 'Matemática',
+        4 => 'Biologia',
+        5 => 'Ciências',
+        6 => 'Português',
+        7 => 'Inglês',
+        8 => 'Espanhol',
+        9 => 'Outro Idioma',
+        10 => 'Artes',
+        11 => 'Educação Física',
+        12 => 'História',
+        13 => 'Geografia',
+        14 => 'Filosofia',
+        16 => 'Informática',
+        17 => 'Disc. Profissionalizante',
+        20 => 'Educação Especial',
+        21 => 'Sociedade&nbspe Cultura',
+        23 => 'Libras',
+        25 => 'Disciplinas pedagógicas',
+        26 => 'Ensino religioso',
+        27 => 'Língua indígena',
+        28 => 'Estudos Sociais',
+        29 => 'Sociologia',
+        30 => 'Francês',
+        99 => 'Outras Disciplinas',
+        10001 => 'Redação',
+        10002 => 'Linguagem oral e escrita',
+        10003 => 'Natureza e sociedade',
+        10004 => 'Movimento',
+        10005 => 'Música',
+        10006 => 'Artes visuais',
+        10007 => 'Acompanhamento Pedagógico',
+        10008 => 'Teatro',
+        10009 => 'Canteiro Sustentável',
+        10010 => 'Dança',
+        10011 => 'Cordel',
+        10012 => 'Física'
+    );
+
+    if (array_key_exists($id, $disciplinas)) {
+        return $disciplinas[$id];
+    } else {
+        return EdcensoDiscipline::model()->findByPk($id)->name;
+    }
 }
 $rows = count($baseDisciplines)+count($diversifiedDisciplines); // contador com a soma do total de disciplinas da matriz
 ?>
