9dd3b2320e76035b7009b9b1607eedc97cf1b844
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2023-07-04 11:16:55 -0300

    Feature/filiation birthday (version 3.28.70) (#361)
    * feature: query sql and update TAG version
    
    * fix: form student
    
    * feature: fields add

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $debug = getenv("YII_DEBUG");
 
 defined('YII_DEBUG') or define('YII_DEBUG', $debug === "true" );
 
-define("TAG_VERSION", '3.27.70');
+define("TAG_VERSION", '3.28.70');
 
 
 define("YII_VERSION", Yii::getVersion());
