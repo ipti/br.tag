605bf73070c3cf20a43e6529e08bc272a2a7ea19
Nathan <ti.natan@ipti.org.br>
2023-07-05 17:05:24 -0300

    Feat: added modification for data types

---
+++ app/migrations/2023-07-03_feat/querys.sql
@@ -0,0 +1,8 @@
+ALTER TABLE school_identification ADD hash BIGINT NULL;
+ALTER TABLE edcenso_nation ADD hash BIGINT NULL;
+ALTER TABLE classroom MODIFY COLUMN hash BIGINT NULL;
+ALTER TABLE instructor_identification MODIFY COLUMN hash BIGINT NULL;
+ALTER TABLE users ADD hash BIGINT NULL;
+ALTER TABLE student_identification MODIFY COLUMN hash BIGINT NULL;
+
+
