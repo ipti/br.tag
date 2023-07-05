57d0f8985d47d4dbd275457032f09eea3d3bd031
Carlos Henrique Santos Silveira <carlos.henrique@dcomp.ufs.br>
2023-07-05 10:01:15 -0300

    fix: alert no transfers

--- themes/default/views/reports/TransferReport.php
+++ themes/default/views/reports/TransferReport.php
@@ -19,49 +19,53 @@ $this->setPageTitle('TAG - ' . Yii::t('default', 'Reports'));
         </div>
     </div>
     <h4 style="text-align: center;"><?php echo $header?></h4>
-    <table class="table table-bordered table-striped" aria-labelledby="Transfer Report Table">
-        <thead>
-            <tr>
-                <th scope="col">Ordem</th>
-                <th scope="col">Nome do Aluno</th>
-                <th scope="col">CPF</th>
-                <th scope="col">Turma</th>
-                <th scope="col">Escola</th>
-                <th scope="col">Responsável</th>
-                <th scope="col">Telefone</th>
-                <th scope="col">Data de Transferência</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $ordem = 1;
-            foreach ($report as $r) {?>
+    <?php if(count($report) != 0) {?>
+        <table class="table table-bordered table-striped" aria-labelledby="Transfer Report Table">
+            <thead>
                 <tr>
-                    <td><?= $ordem <= 9 ? "0".$ordem : $ordem ?></td>
-                    <td><?= $r['name']?></td>
-                    <td><?= $r['cpf']?></td>
-                    <td><?= $r['classroom_name']?></td>
-                    <td><?= $r['school_name']?></td>
-                    <td><?= $r['responsable_name']?></td>
-                    <td><?= $r['responsable_telephone']?></td>
-                    <td><?= date('d/m/Y', strtotime($r['transfer_date']))?></td>
+                    <th scope="col">Ordem</th>
+                    <th scope="col">Nome do Aluno</th>
+                    <th scope="col">CPF</th>
+                    <th scope="col">Turma</th>
+                    <th scope="col">Escola</th>
+                    <th scope="col">Responsável</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Data de Transferência</th>
                 </tr>
-            <?php
-            $ordem++;
-            }?>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td style="text-align: right"><b>TOTAL:</b></td>
-                <td><b><?= count($r)-1 ?></b></td>
-            </tr>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php
+                $ordem = 1;
+                foreach ($report as $r) {?>
+                    <tr>
+                        <td><?= $ordem <= 9 ? "0".$ordem : $ordem ?></td>
+                        <td><?= $r['name']?></td>
+                        <td><?= $r['cpf']?></td>
+                        <td><?= $r['classroom_name']?></td>
+                        <td><?= $r['school_name']?></td>
+                        <td><?= $r['responsable_name']?></td>
+                        <td><?= $r['responsable_telephone']?></td>
+                        <td><?= date('d/m/Y', strtotime($r['transfer_date']))?></td>
+                    </tr>
+                <?php
+                $ordem++;
+                }?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td style="text-align: right"><b>TOTAL:</b></td>
+                    <td><b><?= count($report) ?></b></td>
+                </tr>
+            </tbody>
+        </table>
+    <?php }else {?>
+        <div class='no-enrollments'>Não há alunos transferidos</div>
+    <?php }?>
     <?php $this->renderPartial('footer'); ?>
 </div>
 
