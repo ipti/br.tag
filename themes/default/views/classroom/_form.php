634bc8f5cd479cf184cdb25a3fdf02cbd2ce7b7a
Tori Calasans <tori.ti@ipti.org.br>
2023-07-06 09:17:08 -0300

    retirado spam

--- themes/default/views/classroom/_form.php
+++ themes/default/views/classroom/_form.php
@@ -41,7 +41,6 @@ $form = $this->beginWidget('CActiveForm', array(
         </div>
     <?php endif ?>
     <div class="widget widget-tabs border-bottom-none">
-    <span class="t-icon-start"></span>
 
         <?php echo $form->errorSummary($modelClassroom); ?>
         <div class="alert alert-error classroom-error no-show"></div>
