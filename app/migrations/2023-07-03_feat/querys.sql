d5ce53256793d1d7b84ebcc8f5f276023a525b7a
Nathan Santos <ti.natan@ipti.ong.br>
2023-07-07 11:12:30 -0300

    Feat: Added hash column to instructor_documents_and_address, schedule, and curricular_matrix tables

--- app/migrations/2023-07-03_feat/querys.sql
+++ app/migrations/2023-07-03_feat/querys.sql
@@ -4,5 +4,7 @@ ALTER TABLE classroom MODIFY COLUMN hash BIGINT NULL;
 ALTER TABLE instructor_identification MODIFY COLUMN hash BIGINT NULL;
 ALTER TABLE users ADD hash BIGINT NULL;
 ALTER TABLE student_identification MODIFY COLUMN hash BIGINT NULL;
-
+ALTER TABLE instructor_documents_and_address MODIFY COLUMN hash BIGINT NULL;
+ALTER TABLE schedule ADD hash BIGINT NULL;
+ALTER TABLE curricular_matrix ADD hash BIGINT NULL;
 
