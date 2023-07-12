f0af4721433227f2ac8f643627b1b2bd0e762758
Nathan <ti.natan@ipti.org.br>
2023-07-12 16:58:27 -0300

    Refactor: sql for export json

--- app/migrations/2023-07-12_feat_exp_json/querys.sql
+++ app/migrations/2023-07-12_feat_exp_json/querys.sql
@@ -1,10 +1,12 @@
 ALTER TABLE school_identification ADD hash BIGINT NULL;
-ALTER TABLE edcenso_nation ADD hash BIGINT NULL;
 ALTER TABLE classroom MODIFY COLUMN hash BIGINT NULL;
-ALTER TABLE instructor_identification MODIFY COLUMN hash BIGINT NULL;
-ALTER TABLE users ADD hash BIGINT NULL;
-ALTER TABLE student_identification MODIFY COLUMN hash BIGINT NULL;
-ALTER TABLE instructor_documents_and_address MODIFY COLUMN hash BIGINT NULL;
 ALTER TABLE schedule ADD hash BIGINT NULL;
+
+ALTER TABLE instructor_identification MODIFY COLUMN hash BIGINT NULL;
+ALTER TABLE instructor_documents_and_address MODIFY COLUMN hash BIGINT NULL;
 ALTER TABLE curricular_matrix ADD hash BIGINT NULL;
+
+ALTER TABLE student_identification MODIFY COLUMN hash BIGINT NULL;
+ALTER TABLE student_identification ADD filiation_1_birthday VARCHAR(10) NULL;
+ALTER TABLE student_identification ADD filiation_2_birthday VARCHAR(10) NULL;
 
