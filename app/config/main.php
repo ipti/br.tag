d98fb55369cf3579581f949641f52bff0459c69e
Nathan Santos <ti.natan@ipti.ong.br>
2023-07-17 11:02:45 -0300

    Feat: added import for datasources

--- app/config/main.php
+++ app/config/main.php
@@ -42,6 +42,7 @@ return array(
         'application.modules.calendar.models.*',
         'application.modules.curricularmatrix.models.*',
         'application.modules.quiz.models.*',
+        'application.modules.sedsp.datasources.sed.*',
         'application.modules.sagres.soap.src.sagresEdu.*',
         'application.components.utils.TagUtils'
     ),
