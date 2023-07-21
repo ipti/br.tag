e570262e31cee71abea9893ebc0d821ad9afb652
Nathan <ti.natan@ipti.org.br>
2023-07-21 16:58:33 -0300

    Feat: edded EnrollmentSEDDataSource

---
+++ app/modules/sedsp/datasources/sed/Enrollment/EnrollmentSEDDataSource.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'app/vendor/autoload.php';
+
+
+class EnrollmentSEDDataSource extends SedDataSource
+{
+
+    /**
+     * Summary of getListarMatriculasRA
+     * @param InAluno $inAluno
+     * @return OutListarMatriculasRa, 
+     */
+    function getListarMatriculasRA($inAluno)
+    {
+        
+    }
+}
