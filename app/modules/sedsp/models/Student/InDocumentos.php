18e2bb4023e229dd13724a40379d5ac486a6a828
Nathan Santos <nathanaelsantos15@gmail.com>
2023-07-26 11:41:32 -0300

    Refactor: improvement in class InDocumentsStudent

--- app/modules/sedsp/models/Student/InDocumentos.php
+++ app/modules/sedsp/models/Student/InDocumentos.php
@@ -1,156 +1,63 @@
-<?php 
+<?php
 
-class InDocumentos implements JsonSerializable
+declare(strict_types=1);
+
+class InDocumentos
 {
-	private $inNumDoctoCivil;
+    /**
+     * @var ?string $inNumRG
+     */
+    public $inNumRG;
 
-	private $inDigitoDoctoCivil;
+    /**
+     * @var ?string $inDigitoRG
+     */
+    public $inDigitoRG;
 
-	private $inUfDoctoCivil;
+    /**
+     * @var ?string $inUFRG
+     */
+    public $inUFRG;
 
-	private $inDataEmissaoDoctoCivil;
+    /**
+     * @var ?string $inCPF
+     */
+    public $inCPF;
 
-	private $inNumNis;
+    /**
+     * @var ?string $inNumNIS
+     */
+    public $inNumNIS;
 
-	private $inCodigoInep;
+    /**
+     * @var ?string $inNumINEP
+     */
+    public $inNumINEP;
 
-	private $inCpf;
+    /**
+     * @var ?string $inNumCertidaoNova
+     */
+    public $inNumCertidaoNova;
 
-	private $inJustificativaDocumentos;
 
-	/**
-	 * @param string|null $inNumDoctoCivil
-	 * @param string|null $inDigitoDoctoCivil
-	 * @param string|null $inUfDoctoCivil
-	 * @param string|null $inDataEmissaoDoctoCivil
-	 * @param string|null $inNumNis
-	 * @param string|null $inCodigoInep
-	 * @param string|null $inCpf
-	 * @param string|null $inJustificativaDocumentos
-	 */
-	public function __construct(
-		?string $inNumDoctoCivil = null,
-		?string $inDigitoDoctoCivil = null,
-		?string $inUfDoctoCivil = null,
-		?string $inDataEmissaoDoctoCivil = null,
-		?string $inNumNis = null,
-		?string $inCodigoInep = null,
-		?string $inCpf = null,
-		?string $inJustificativaDocumentos = null
-	) {
-		$this->inNumDoctoCivil = $inNumDoctoCivil;
-		$this->inDigitoDoctoCivil = $inDigitoDoctoCivil;
-		$this->inUfDoctoCivil = $inUfDoctoCivil;
-		$this->inDataEmissaoDoctoCivil = $inDataEmissaoDoctoCivil;
-		$this->inNumNis = $inNumNis;
-		$this->inCodigoInep = $inCodigoInep;
-		$this->inCpf = $inCpf;
-		$this->inJustificativaDocumentos = $inJustificativaDocumentos;
-	}
 
-	/**
-	 * @param string|null $inNumDoctoCivil
-	 * @return self
-	 */
-	public function setInNumDoctoCivil(?string $inNumDoctoCivil): self
-	{
-		$this->inNumDoctoCivil = $inNumDoctoCivil;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inDigitoDoctoCivil
-	 * @return self
-	 */
-	public function setInDigitoDoctoCivil(?string $inDigitoDoctoCivil): self
-	{
-		$this->inDigitoDoctoCivil = $inDigitoDoctoCivil;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inUfDoctoCivil
-	 * @return self
-	 */
-	public function setInUfDoctoCivil(?string $inUfDoctoCivil): self
-	{
-		$this->inUfDoctoCivil = $inUfDoctoCivil;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inDataEmissaoDoctoCivil
-	 * @return self
-	 */
-	public function setInDataEmissaoDoctoCivil(?string $inDataEmissaoDoctoCivil): self
-	{
-		$this->inDataEmissaoDoctoCivil = $inDataEmissaoDoctoCivil;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inNumNis
-	 * @return self
-	 */
-	public function setInNumNis(?string $inNumNis): self
-	{
-		$this->inNumNis = $inNumNis;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inCodigoInep
-	 * @return self
-	 */
-	public function setInCodigoInep(?string $inCodigoInep): self
-	{
-		$this->inCodigoInep = $inCodigoInep;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inCpf
-	 * @return self
-	 */
-	public function setInCpf(?string $inCpf): self
-	{
-		$this->inCpf = $inCpf;
-		return $this;
-	}
-
-	/**
-	 * @param string|null $inJustificativaDocumentos
-	 * @return self
-	 */
-	public function setInJustificativaDocumentos(?string $inJustificativaDocumentos): self
-	{
-		$this->inJustificativaDocumentos = $inJustificativaDocumentos;
-		return $this;
-	}
-
-	/**
-	 * @param array $data
-	 * @return self
-	 */
-	public static function fromJson(array $data): self
-	{
-		return new self(
-			$data['outNumDoctoCivil'] ?? null,
-			$data['outDigitoDoctoCivil'] ?? null,
-			$data['outUFDoctoCivil'] ?? null,
-			$data['outDataEmissaoDoctoCivil'] ?? null,
-			$data['outNumNIS'] ?? null,
-			$data['outCodigoINEP'] ?? null,
-			$data['outCPF'] ?? null,
-			$data['outJustificativaDocumentos'] ?? null
-		);
-	}
-
-	public function jsonSerialize()
-    {
-        $vars = get_object_vars($this);
-        return $vars;
-    }
+    /**
+     * InListarAlunos constructor.
+     * @param ?string $inNumRG
+     * @param ?string $inDigitoRG
+     * @param ?string $inUFRG
+     * @param ?string $inCPF
+     * @param ?string $inNumNIS
+     * @param ?string $inNumINEP
+     * @param ?string $inNumCertidaoNova
+     */
+    public function __construct($inDocumentos) {
+        $this->inNumRG = $inDocumentos->inNumRG;
+        $this->inDigitoRG = $inDocumentos->inDigitoRG;
+        $this->inUFRG = $inDocumentos->inUFRG;
+        $this->inCPF = $inDocumentos->inCPF;
+        $this->inNumNIS = $inDocumentos->inNumNIS;
+        $this->inNumINEP = $inDocumentos->inNumINEP;
+        $this->inNumCertidaoNova = $inDocumentos->inNumCertidaoNova;
+    } 
 }
-
-?>
