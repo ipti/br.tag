ffe23d4ade2c8dacb9358b55d523c0bc2fb9c5dc
Nathan <ti.natan@ipti.org.br>
2023-07-28 16:57:00 -0300

    Feat: added construct for InFichaAluno

--- app/modules/sedsp/models/Student/InFichaAluno.php
+++ app/modules/sedsp/models/Student/InFichaAluno.php
@@ -26,14 +26,20 @@ class InFichaAluno implements JsonSerializable
     /** @var InRastreio */
     public $inRastreio;
 
+    function __construct($fichaAluno) {
+        $fichaAluno = (object) $fichaAluno;
+        $this->inDadosPessoais = new InDadosPessoais($fichaAluno->inDadosPessoais);
+        $this->inDocumentos = new InDocumentos($fichaAluno->inDocumentos);
+        $this->inCertidaoNova = new InCertidaoNova($fichaAluno->inCertidaoNova);
+        $this->inCertidaoAntiga = new InCertidaoAntiga($fichaAluno->InCertidaoAntiga);
+        $this->inEnderecoResidencial = new InEnderecoResidencial($fichaAluno->InEnderecoResidencial);
+        $this->inDeficiencia = new InDeficiencia($fichaAluno->InDeficiencia);
+        $this->inRecursoAvaliacao = new InRecursoAvaliacao($fichaAluno->InRecursoAvaliacao);
+        $this->inRastreio = new InRastreio($fichaAluno->InRastreio);
+    }
 
 	public function jsonSerialize()
     {
-
-        function __construct($fichaAluno) {
-           
-        }
-
 		$filteredProps = array_filter(get_object_vars($this), function ($value) {
             return $value !== null;
         });
