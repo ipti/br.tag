a106dc05d3235e05e1f1680790456a7cef77bc35
gabriel_chagas <112498725+GabrielChagasNext@users.noreply.github.com>
2023-07-31 12:12:27 -0300

    mudando método getClassroomsInstructor

--- app/modules/classdiary/services/ClassesService.php
+++ app/modules/classdiary/services/ClassesService.php
@@ -12,26 +12,88 @@
         }
 
         public function getClassroomsInstructor($discipline) {
+             if ($discipline != "") {
+                $sql = "SELECT itd.id, esvm.id as stage_fk, ii.name as instructor_name, ed.id as edcenso_discipline_fk, ed.name as discipline_name, esvm.name as stage_name, c.name  
+                from instructor_teaching_data itd 
+                join teaching_matrixes tm ON itd.id = tm.teaching_data_fk 
+                join instructor_identification ii on itd.instructor_fk = ii.id 
+                join curricular_matrix cm on tm.curricular_matrix_fk = cm.id 
+                JOIN edcenso_discipline ed on ed.id = cm.discipline_fk 
+                join classroom c on c.id = itd.classroom_id_fk  
+                Join edcenso_stage_vs_modality esvm on esvm.id = c.edcenso_stage_vs_modality_fk  
+                WHERE ii.users_fk = :users_fk and esvm.id NOT BETWEEN 14 AND 16 and ed.id = :discipline_id and c.school_year = :user_year
+                ORDER BY ii.name";
 
-            $sql = "SELECT c.id, esvm.id as stage_fk, ii.name as instructor_name, ed.id as edcenso_discipline_fk, ed.name as discipline_name, esvm.name as stage_name, c.name  
-            from instructor_teaching_data itd 
-            join teaching_matrixes tm ON itd.id = tm.teaching_data_fk 
-            join instructor_identification ii on itd.instructor_fk = ii.id 
-            join curricular_matrix cm on tm.curricular_matrix_fk = cm.id 
-            JOIN edcenso_discipline ed on ed.id = cm.discipline_fk 
-            join classroom c on c.id = itd.classroom_id_fk  
-            Join edcenso_stage_vs_modality esvm on esvm.id = c.edcenso_stage_vs_modality_fk  
-            WHERE ii.users_fk = :users_fk and ed.id = :discipline_id and c.school_year = :user_year
-            ORDER BY ii.name";
+                $command = Yii::app()->db->createCommand($sql);
+                $command->bindValue(':users_fk', Yii::app()->user->loginInfos->id, PDO::PARAM_INT)
+                ->bindValue(':discipline_id', $discipline, PDO::PARAM_INT)
+                ->bindValue(':user_year', Yii::app()->user->year, PDO::PARAM_INT);
+            } else {
+                $sql = "SELECT itd.id, esvm.id as stage_fk, ii.name as instructor_name, ed.id as edcenso_discipline_fk, ed.name as discipline_name, esvm.name as stage_name, c.name  
+                from instructor_teaching_data itd 
+                join teaching_matrixes tm ON itd.id = tm.teaching_data_fk 
+                join instructor_identification ii on itd.instructor_fk = ii.id 
+                join curricular_matrix cm on tm.curricular_matrix_fk = cm.id 
+                JOIN edcenso_discipline ed on ed.id = cm.discipline_fk 
+                join classroom c on c.id = itd.classroom_id_fk  
+                Join edcenso_stage_vs_modality esvm on esvm.id = c.edcenso_stage_vs_modality_fk  
+                WHERE ii.users_fk = :users_fk and esvm.id NOT BETWEEN 14 AND 16 and c.school_year = :user_year
+                ORDER BY ii.name";
 
-            $command = Yii::app()->db->createCommand($sql);
-            $command->bindValue(':users_fk', Yii::app()->user->loginInfos->id, PDO::PARAM_INT)
-            ->bindValue(':discipline_id', $discipline, PDO::PARAM_INT)
-            ->bindValue(':user_year', Yii::app()->user->year, PDO::PARAM_INT);
+                $command = Yii::app()->db->createCommand($sql);
+                $command->bindValue(':users_fk', Yii::app()->user->loginInfos->id, PDO::PARAM_INT)
+                ->bindValue(':user_year', Yii::app()->user->year, PDO::PARAM_INT);
+            }
+            
+            $classrooms = $command->queryAll(); 
+            $minorSchoolingClassroom = $this->getMinorSchoolingClassrooms();
+            /* echo "<pre>";
+            var_dump($minorSchoolingClassroom[0]["turma"]["name"]);
+            echo "</pre>"; */
+            return [
+                "valid" => true,
+                "classrooms" => $classrooms,
+                "minorSchoolingClassroom" => $minorSchoolingClassroom
+            ] ;
+        }
+        public function getMinorSchoolingClassrooms() {
+            $criteria = new CDbCriteria;
+            $criteria->alias = "c";
+            $criteria->select = "c.id, c.name, c.edcenso_stage_vs_modality_fk, esvm.name"; 
+            $criteria->join = ""
+                . " join instructor_teaching_data on instructor_teaching_data.classroom_id_fk = c.id "
+                . " join instructor_identification on instructor_teaching_data.instructor_fk = instructor_identification.id "
+                ."  Join edcenso_stage_vs_modality esvm on esvm.id = c.edcenso_stage_vs_modality_fk";
+            $criteria->condition = "c.school_year = :school_year and instructor_identification.users_fk = :users_fk";
+            $criteria->order = "name";
+            $criteria->addCondition('esvm.id BETWEEN :start_id AND :end_id');
+            $criteria->params = array(':school_year' => Yii::app()->user->year, ':users_fk' => Yii::app()->user->loginInfos->id, 'start_id' =>14,'end_id' => 16);
+            $minorSchoolingClassroom = Classroom::model()->findAll($criteria);
 
-            $classrooms = $command->queryAll();
+            // $results = array();
+            foreach ($minorSchoolingClassroom as $c) {
+                $disciplines = Yii::app()->db->createCommand(
+                    "select ed.id, ed.name as discipline_name from teaching_matrixes tm 
+                    join instructor_teaching_data itd on itd.id = tm.teaching_data_fk 
+                    join instructor_identification ii on ii.id = itd.instructor_fk
+                    join curricular_matrix cm on cm.id = tm.curricular_matrix_fk
+                    join edcenso_discipline ed on ed.id = cm.discipline_fk
+                    where ii.users_fk = :userid and itd.classroom_id_fk = :crid order by ed.name")
+                    ->bindParam(":userid", Yii::app()->user->loginInfos->id)->bindParam(":crid", $c->id)->queryAll();
+                    echo "<pre>";
+                    var_dump($c);
+                    echo "</pre>";
+                    exit();
+                    $result["turma"]["name"] = $c->name;
+                    $result["turma"]["stage_name"] = $c->stage_name;
+                    $result["disciplines"] = $disciplines;
+                    // array_push($results[$c->id], $result);
+                    $results[] = $result;
+            }
+            
+            
            
-            return $classrooms;
+            return $results;
         }
 
         /**
--- app/modules/classdiary/views/default/index.php
+++ app/modules/classdiary/views/default/index.php
@@ -7,13 +7,14 @@ $this->breadcrumbs=array(
 $baseScriptUrl = Yii::app()->controller->module->baseScriptUrl;
 $cs = Yii::app()->getClientScript();
 $cs->registerScriptFile($baseScriptUrl . '/index/functions.js', CClientScript::POS_END);
+$cs->registerScriptFile($baseScriptUrl . '/index/_initialization.js', CClientScript::POS_END);
 ?>
 <div class="main">
 	<h1>Diário de Classe</h1>
 	<div class="row">
 		<div class="column no-grow">
 			<div class="t-field-select">
-				<?php echo CHtml::dropDownList('discipline', null, $disciplines,  array('prompt' => 'Selecione a matéria', 'class' => 'select-search-on t-field-select__input js-select-disciplines',)); ?>
+				<?php echo CHtml::dropDownList('discipline', "", $disciplines,  array('prompt' => 'Selecione a matéria', 'class' => 'select-search-on t-field-select__input js-select-disciplines',)); ?>
 			</div> 
 		</div>
 	</div>
