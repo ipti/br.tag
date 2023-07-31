30f4bb10e9b143eb31b11a3a61fbab71ef861361
Nathan <ti.natan@ipti.org.br>
2023-07-31 16:38:22 -0300

    Fix: changed the access type for the attributes

--- app/modules/sedsp/models/Student/InCertidaoAntiga.php
+++ app/modules/sedsp/models/Student/InCertidaoAntiga.php
@@ -12,99 +12,171 @@ class InCertidaoAntiga implements JsonSerializable
 
 	public $inMunicipioComarca;
 
-	public $inUfComarca;
+	public $inUFComarca;
 
 	public $inDataEmissaoCertidao;
 
 	/**
-	 * @param ?string $inNumCertidao
-	 * @param ?string $inLivro
-	 * @param ?string $inFolha
-	 * @param ?string $inDistritoCertidao
-	 * @param ?string $inMunicipioComarca
-	 * @param ?string $inUfComarca
-	 * @param ?string $inDataEmissaoCertidao
+	 * @param string $inNumCertidao
+	 * @param string $inLivro
+	 * @param string $inFolha
+	 * @param string $inDistritoCertidao
+	 * @param string $inMunicipioComarca
+	 * @param string $inUFComarca
+	 * @param string $inDataEmissaoCertidao
 	 */
 	public function __construct($inCertidaoAntiga) {
+		$inCertidaoAntiga = (object) $inCertidaoAntiga;
 		$this->inNumCertidao = $inCertidaoAntiga->inNumCertidao;
 		$this->inLivro = $inCertidaoAntiga->inLivro;
 		$this->inFolha = $inCertidaoAntiga->inFolha;
 		$this->inDistritoCertidao = $inCertidaoAntiga->inDistritoCertidao;
 		$this->inMunicipioComarca = $inCertidaoAntiga->inMunicipioComarca;
-		$this->inUfComarca = $inCertidaoAntiga->inUfComarca;
+		$this->inUFComarca = $inCertidaoAntiga->inUfComarca;
 		$this->inDataEmissaoCertidao = $inCertidaoAntiga->inDataEmissaoCertidao;
 	}
 
 	/**
-	 * @param string|null $inNumCertidao
-	 * @return self
-	 */
-	public function setInNumCertidao(?string $inNumCertidao): self
+	 * Get the value of inNumCertidao
+	 */ 
+	public function getInNumCertidao()
+	{
+		return $this->inNumCertidao;
+	}
+
+	/**
+	 * Set the value of inNumCertidao
+	 *
+	 * @return  self
+	 */ 
+	public function setInNumCertidao($inNumCertidao)
 	{
 		$this->inNumCertidao = $inNumCertidao;
+
 		return $this;
 	}
 
 	/**
-	 * @param string|null $inLivro
-	 * @return self
-	 */
-	public function setInLivro(?string $inLivro): self
+	 * Get the value of inLivro
+	 */ 
+	public function getInLivro()
+	{
+		return $this->inLivro;
+	}
+
+	/**
+	 * Set the value of inLivro
+	 *
+	 * @return  self
+	 */ 
+	public function setInLivro($inLivro)
 	{
 		$this->inLivro = $inLivro;
+
 		return $this;
 	}
 
 	/**
-	 * @param string|null $inFolha
-	 * @return self
-	 */
-	public function setInFolha(?string $inFolha): self
+	 * Get the value of inFolha
+	 */ 
+	public function getInFolha()
+	{
+		return $this->inFolha;
+	}
+
+	/**
+	 * Set the value of inFolha
+	 *
+	 * @return  self
+	 */ 
+	public function setInFolha($inFolha)
 	{
 		$this->inFolha = $inFolha;
+
 		return $this;
 	}
 
 	/**
-	 * @param string|null $inDistritoCertidao
-	 * @return self
-	 */
-	public function setInDistritoCertidao(?string $inDistritoCertidao): self
+	 * Get the value of inDistritoCertidao
+	 */ 
+	public function getInDistritoCertidao()
+	{
+		return $this->inDistritoCertidao;
+	}
+
+	/**
+	 * Set the value of inDistritoCertidao
+	 *
+	 * @return  self
+	 */ 
+	public function setInDistritoCertidao($inDistritoCertidao)
 	{
 		$this->inDistritoCertidao = $inDistritoCertidao;
+
 		return $this;
 	}
 
 	/**
-	 * @param string|null $inMunicipioComarca
-	 * @return self
-	 */
-	public function setInMunicipioComarca(?string $inMunicipioComarca): self
+	 * Get the value of inMunicipioComarca
+	 */ 
+	public function getInMunicipioComarca()
+	{
+		return $this->inMunicipioComarca;
+	}
+
+	/**
+	 * Set the value of inMunicipioComarca
+	 *
+	 * @return  self
+	 */ 
+	public function setInMunicipioComarca($inMunicipioComarca)
 	{
 		$this->inMunicipioComarca = $inMunicipioComarca;
+
 		return $this;
 	}
 
 	/**
-	 * @param string|null $inUfComarca
-	 * @return self
-	 */
-	public function setInUfComarca(?string $inUfComarca): self
+	 * Get the value of inUfComarca
+	 */ 
+	public function getInUfComarca()
 	{
-		$this->inUfComarca = $inUfComarca;
+		return $this->inUFComarca;
+	}
+
+	/**
+	 * Set the value of inUfComarca
+	 *
+	 * @return  self
+	 */ 
+	public function setInUfComarca($inUfComarca)
+	{
+		$this->inUFComarca = $inUfComarca;
+
 		return $this;
 	}
 
 	/**
-	 * @param string|null $inDataEmissaoCertidao
-	 * @return self
-	 */
-	public function setInDataEmissaoCertidao(?string $inDataEmissaoCertidao): self
+	 * Get the value of inDataEmissaoCertidao
+	 */ 
+	public function getInDataEmissaoCertidao()
+	{
+		return $this->inDataEmissaoCertidao;
+	}
+
+	/**
+	 * Set the value of inDataEmissaoCertidao
+	 *
+	 * @return  self
+	 */ 
+	public function setInDataEmissaoCertidao($inDataEmissaoCertidao)
 	{
 		$this->inDataEmissaoCertidao = $inDataEmissaoCertidao;
+
 		return $this;
 	}
 
+	
 	function jsonSerialize()
     {
         $filteredProps = array_filter(get_object_vars($this), function ($value) {
