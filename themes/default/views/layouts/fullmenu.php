dd10337fb071896a91fbe8f2e8044815c2e971df
Carlos Henrique Santos Silveira <carlos.henrique@dcomp.ufs.br>
2023-07-31 11:55:37 -0300

    feature: report card icon side bar

--- themes/default/views/layouts/fullmenu.php
+++ themes/default/views/layouts/fullmenu.php
@@ -248,7 +248,8 @@ $cs->registerCssFile(Yii::app()->baseUrl . "/sass/css/main.css?v=" . TAG_VERSION
                                                                             strpos($_SERVER['REQUEST_URI'], "?r=courseplan") ||
                                                                                 strpos($_SERVER['REQUEST_URI'], "?r=classes/classContents") ||
                                                                                 strpos($_SERVER['REQUEST_URI'], "?r=classes/frequency") ||
-                                                                                strpos($_SERVER['REQUEST_URI'], "?r=enrollment/grades")
+                                                                                strpos($_SERVER['REQUEST_URI'], "?r=enrollment/grades") ||
+                                                                                strpos($_SERVER['REQUEST_URI'], "?r=enrollment/reportCard")
                                                                                 ? 'active' : '' ?>">
                             <i class="submenu-icon fa fa-chevron-right"></i>
                             <i class="submenu-icon fa fa-chevron-down"></i>
@@ -261,7 +262,8 @@ $cs->registerCssFile(Yii::app()->baseUrl . "/sass/css/main.css?v=" . TAG_VERSION
                                                 strpos($_SERVER['REQUEST_URI'], "?r=courseplan") ||
                                                     strpos($_SERVER['REQUEST_URI'], "?r=classes/classContents") ||
                                                     strpos($_SERVER['REQUEST_URI'], "?r=classes/frequency") ||
-                                                    strpos($_SERVER['REQUEST_URI'], "?r=enrollment/grades") ? 'in' : '' ?>" id="submenu-electronic-diary">
+                                                    strpos($_SERVER['REQUEST_URI'], "?r=enrollment/grades") ||
+                                                    strpos($_SERVER['REQUEST_URI'], "?r=enrollment/reportCard") ? 'in' : '' ?>" id="submenu-electronic-diary">
 
                                 <li class="t-menu-item <?= strpos($_SERVER['REQUEST_URI'], "?r=courseplan") ? 'active' : '' ?>">
                                     <a class="t-menu-item__link" href="<?php echo yii::app()->createUrl('courseplan') ?>">
@@ -293,7 +295,7 @@ $cs->registerCssFile(Yii::app()->baseUrl . "/sass/css/main.css?v=" . TAG_VERSION
                                 </li>
                                 <li class="t-menu-item <?= strpos($_SERVER['REQUEST_URI'], "?r=enrollment/reportCard") ? 'active' : '' ?>">
                                     <a class="t-menu-item__link" href="<?php echo yii::app()->createUrl('enrollment/reportCard') ?> ">
-                                        <span class="t-icon-edition t-menu-item__icon"></span>
+                                        <span class="t-report_card t-menu-item__icon"></span>
                                         <span class="t-menu-item__text">Boletim</span>
                                     </a>
                                 </li>
