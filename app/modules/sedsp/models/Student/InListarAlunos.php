2de8d7347e0352f44355f9cbc827a323d6d987cd
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-02 13:51:10 -0300

    Feat: added all getter & setter for InFichaAluno and InListarAlunos

--- app/modules/sedsp/models/Student/InListarAlunos.php
+++ app/modules/sedsp/models/Student/InListarAlunos.php
@@ -2,34 +2,22 @@
 
 class InListarAlunos implements JsonSerializable
 {
-    /**
-     * @var  InDadosPessoais
-     */
     public $inDadosPessoais;
-
-    /**
-     * @var InDocumentos
-     */
     public $inDocumentos;
 
-
     /**
-     * 
      * Summary of __construct
-     * @param array $searchListOfStudents
-     * @return InListarAlunos
+     * @param InDadosPessoais $inDadosPessoais
+     * @param InDocumentos $inDocumentos
      * 
+     * @return InListarAlunos
      */
-    public function __construct(array $searchListOfStudents)
+    public function __construct(InDadosPessoais $inDadosPessoais, InDocumentos $inDocumentos)
     {
-        $this->inDadosPessoais = new InDadosPessoais((object) $searchListOfStudents['InDadosPessoais']);
-        $this->inDocumentos = new InDocumentos((object) $searchListOfStudents['InDocumentos']);
+        $this->inDadosPessoais = $inDadosPessoais;
+        $this->inDocumentos = $inDocumentos;
     }
 
-    /**
-     * Summary of jsonSerialize
-     * @return array
-     */
     function jsonSerialize()
     {
         $filteredProps = array_filter(get_object_vars($this), function ($value) {
@@ -37,5 +25,41 @@ class InListarAlunos implements JsonSerializable
         });
 
         return $filteredProps;
+    }
+
+    /**
+     * Get the value of inDadosPessoais
+     */
+    public function getInDadosPessoais()
+    {
+        return $this->inDadosPessoais;
+    }
+
+    /**
+     * Set the value of inDadosPessoais
+     */
+    public function setInDadosPessoais($inDadosPessoais): self
+    {
+        $this->inDadosPessoais = $inDadosPessoais;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of inDocumentos
+     */
+    public function getInDocumentos()
+    {
+        return $this->inDocumentos;
+    }
+
+    /**
+     * Set the value of inDocumentos
+     */
+    public function setInDocumentos($inDocumentos): self
+    {
+        $this->inDocumentos = $inDocumentos;
+
+        return $this;
     }
 }
