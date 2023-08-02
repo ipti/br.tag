da36a44e342505478a77e9467e414a351b69839c
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-02 15:19:38 -0300

    Feat: added All properties nullable and static function fromJson from class OutDeficiencia

--- app/modules/sedsp/models/Student/OutDeficiencia.php
+++ app/modules/sedsp/models/Student/OutDeficiencia.php
@@ -10,12 +10,12 @@ class OutDeficiencia
 	public $outDescProfSaude;
 
 	public function __construct(
-		string $outMobilidadeReduzida,
-		string $outDescMobilidadeReduzida,
-		string $outCuidador,
-		string $outDescCuidador,
-		string $outProfSaude,
-		string $outDescProfSaude
+		?string $outMobilidadeReduzida,
+		?string $outDescMobilidadeReduzida,
+		?string $outCuidador,
+		?string $outDescCuidador,
+		?string $outProfSaude,
+		?string $outDescProfSaude
 	) {
 		$this->outMobilidadeReduzida = $outMobilidadeReduzida;
 		$this->outDescMobilidadeReduzida = $outDescMobilidadeReduzida;
@@ -25,111 +25,85 @@ class OutDeficiencia
 		$this->outDescProfSaude = $outDescProfSaude;
 	}
 
-	/**
-	 * Get the value of outMobilidadeReduzida
-	 */
-	public function getOutMobilidadeReduzida()
+	public function getOutMobilidadeReduzida(): ?string
 	{
 		return $this->outMobilidadeReduzida;
 	}
 
-	/**
-	 * Set the value of outMobilidadeReduzida
-	 */
-	public function setOutMobilidadeReduzida($outMobilidadeReduzida): self
-	{
-		$this->outMobilidadeReduzida = $outMobilidadeReduzida;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outDescMobilidadeReduzida
-	 */
-	public function getOutDescMobilidadeReduzida()
+	public function getOutDescMobilidadeReduzida(): ?string
 	{
 		return $this->outDescMobilidadeReduzida;
 	}
 
-	/**
-	 * Set the value of outDescMobilidadeReduzida
-	 */
-	public function setOutDescMobilidadeReduzida($outDescMobilidadeReduzida): self
-	{
-		$this->outDescMobilidadeReduzida = $outDescMobilidadeReduzida;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outCuidador
-	 */
-	public function getOutCuidador()
+	public function getOutCuidador(): ?string
 	{
 		return $this->outCuidador;
 	}
 
-	/**
-	 * Set the value of outCuidador
-	 */
-	public function setOutCuidador($outCuidador): self
-	{
-		$this->outCuidador = $outCuidador;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outDescCuidador
-	 */
-	public function getOutDescCuidador()
+	public function getOutDescCuidador(): ?string
 	{
 		return $this->outDescCuidador;
 	}
 
-	/**
-	 * Set the value of outDescCuidador
-	 */
-	public function setOutDescCuidador($outDescCuidador): self
-	{
-		$this->outDescCuidador = $outDescCuidador;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outProfSaude
-	 */
-	public function getOutProfSaude()
+	public function getOutProfSaude(): ?string
 	{
 		return $this->outProfSaude;
 	}
 
-	/**
-	 * Set the value of outProfSaude
-	 */
-	public function setOutProfSaude($outProfSaude): self
-	{
-		$this->outProfSaude = $outProfSaude;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outDescProfSaude
-	 */
-	public function getOutDescProfSaude()
+	public function getOutDescProfSaude(): ?string
 	{
 		return $this->outDescProfSaude;
 	}
 
-	/**
-	 * Set the value of outDescProfSaude
-	 */
-	public function setOutDescProfSaude($outDescProfSaude): self
+	public function setOutMobilidadeReduzida(?string $outMobilidadeReduzida): self
+	{
+		$this->outMobilidadeReduzida = $outMobilidadeReduzida;
+		return $this;
+	}
+
+	public function setOutDescMobilidadeReduzida(?string $outDescMobilidadeReduzida): self
+	{
+		$this->outDescMobilidadeReduzida = $outDescMobilidadeReduzida;
+		return $this;
+	}
+
+	public function setOutCuidador(?string $outCuidador): self
+	{
+		$this->outCuidador = $outCuidador;
+		return $this;
+	}
+
+	public function setOutDescCuidador(?string $outDescCuidador): self
+	{
+		$this->outDescCuidador = $outDescCuidador;
+		return $this;
+	}
+
+	public function setOutProfSaude(?string $outProfSaude): self
+	{
+		$this->outProfSaude = $outProfSaude;
+		return $this;
+	}
+
+	public function setOutDescProfSaude(?string $outDescProfSaude): self
 	{
 		$this->outDescProfSaude = $outDescProfSaude;
-
 		return $this;
+	}
+
+	/**
+	 * @param array $data
+	 * @return self
+	 */
+	public static function fromJson(array $data): self
+	{
+		return new self(
+			$data['outMobilidadeReduzida'] ?? null,
+			$data['outDescMobilidadeReduzida'] ?? null,
+			$data['outCuidador'] ?? null,
+			$data['outDescCuidador'] ?? null,
+			$data['outProfSaude'] ?? null,
+			$data['outDescProfSaude'] ?? null
+		);
 	}
 }
