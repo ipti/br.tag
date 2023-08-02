22187034200a94f4f9d2428d5a2122bfc20d58b8
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-02 16:19:20 -0300

    Feat: added All properties nullable and static function fromJson from class OutListarAlunos

--- app/modules/sedsp/models/Student/OutListarAlunos.php
+++ app/modules/sedsp/models/Student/OutListarAlunos.php
@@ -10,150 +10,134 @@ class OutListarAlunos
 	public $outSiglaUFRA;
 	public $outNomeAluno;
 	public $outNomeMae;
-	public $outDataNascimento;
 	public $outNomePai;
+	public $outNomeSocial;
+	public $outDataNascimento;
 
 	public function __construct(
-		string $outNumRA,
-		string $outDigitoRA,
-		string $outSiglaUFRA,
-		string $outNomeAluno,
-		string $outNomeMae,
-		string $outDataNascimento,
-		?string $outNomePai
+		?string $outNumRA,
+		?string $outDigitoRA,
+		?string $outSiglaUFRA,
+		?string $outNomeAluno,
+		?string $outNomeMae,
+		?string $outNomePai,
+		?string $outNomeSocial,
+		?string $outDataNascimento
 	) {
 		$this->outNumRA = $outNumRA;
 		$this->outDigitoRA = $outDigitoRA;
 		$this->outSiglaUFRA = $outSiglaUFRA;
 		$this->outNomeAluno = $outNomeAluno;
 		$this->outNomeMae = $outNomeMae;
-		$this->outDataNascimento = $outDataNascimento;
 		$this->outNomePai = $outNomePai;
+		$this->outNomeSocial = $outNomeSocial;
+		$this->outDataNascimento = $outDataNascimento;
 	}
 
-    /**
-     * Get the value of outNumRA
-     */
-    public function getOutNumRA()
-    {
-        return $this->outNumRA;
-    }
+	public function getOutNumRa(): ?string
+	{
+		return $this->outNumRA;
+	}
 
-    /**
-     * Set the value of outNumRA
-     */
-    public function setOutNumRA($outNumRA): self
-    {
-        $this->outNumRA = $outNumRA;
-
-        return $this;
-    }
-
-	/**
-	 * Get the value of outDigitoRA
-	 */
-	public function getOutDigitoRA()
+	public function getOutDigitoRa(): ?string
 	{
 		return $this->outDigitoRA;
 	}
 
-	/**
-	 * Set the value of outDigitoRA
-	 */
-	public function setOutDigitoRA($outDigitoRA): self
-	{
-		$this->outDigitoRA = $outDigitoRA;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outSiglaUFRA
-	 */
-	public function getOutSiglaUFRA()
+	public function getOutSiglaUfra(): ?string
 	{
 		return $this->outSiglaUFRA;
 	}
 
-	/**
-	 * Set the value of outSiglaUFRA
-	 */
-	public function setOutSiglaUFRA($outSiglaUFRA): self
-	{
-		$this->outSiglaUFRA = $outSiglaUFRA;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outNomeAluno
-	 */
-	public function getOutNomeAluno()
+	public function getOutNomeAluno(): ?string
 	{
 		return $this->outNomeAluno;
 	}
 
-	/**
-	 * Set the value of outNomeAluno
-	 */
-	public function setOutNomeAluno($outNomeAluno): self
-	{
-		$this->outNomeAluno = $outNomeAluno;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outNomeMae
-	 */
-	public function getOutNomeMae()
+	public function getOutNomeMae(): ?string
 	{
 		return $this->outNomeMae;
 	}
 
-	/**
-	 * Set the value of outNomeMae
-	 */
-	public function setOutNomeMae($outNomeMae): self
-	{
-		$this->outNomeMae = $outNomeMae;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outDataNascimento
-	 */
-	public function getOutDataNascimento()
-	{
-		return $this->outDataNascimento;
-	}
-
-	/**
-	 * Set the value of outDataNascimento
-	 */
-	public function setOutDataNascimento($outDataNascimento): self
-	{
-		$this->outDataNascimento = $outDataNascimento;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of outNomePai
-	 */
-	public function getOutNomePai()
+	public function getOutNomePai(): ?string
 	{
 		return $this->outNomePai;
 	}
 
-	/**
-	 * Set the value of outNomePai
-	 */
-	public function setOutNomePai($outNomePai): self
+	public function getOutNomeSocial(): ?string
+	{
+		return $this->outNomeSocial;
+	}
+
+	public function getOutDataNascimento(): ?string
+	{
+		return $this->outDataNascimento;
+	}
+
+	public function setOutNumRa(?string $outNumRA): self
+	{
+		$this->outNumRA = $outNumRA;
+		return $this;
+	}
+
+	public function setOutDigitoRa(?string $outDigitoRA): self
+	{
+		$this->outDigitoRA = $outDigitoRA;
+		return $this;
+	}
+
+	public function setOutSiglaUfra(?string $outSiglaUFRA): self
+	{
+		$this->outSiglaUFRA = $outSiglaUFRA;
+		return $this;
+	}
+
+	public function setOutNomeAluno(?string $outNomeAluno): self
+	{
+		$this->outNomeAluno = $outNomeAluno;
+		return $this;
+	}
+
+	public function setOutNomeMae(?string $outNomeMae): self
+	{
+		$this->outNomeMae = $outNomeMae;
+		return $this;
+	}
+
+	public function setOutNomePai(?string $outNomePai): self
 	{
 		$this->outNomePai = $outNomePai;
-
 		return $this;
+	}
+
+	public function setOutNomeSocial(?string $outNomeSocial): self
+	{
+		$this->outNomeSocial = $outNomeSocial;
+		return $this;
+	}
+
+	public function setOutDataNascimento(?string $outDataNascimento): self
+	{
+		$this->outDataNascimento = $outDataNascimento;
+		return $this;
+	}
+
+	/**
+	 * Summary of fromJson
+	 * @param array $data
+	 * @return OutListarAlunos
+	 */
+	public static function fromJson(array $data): self
+	{
+		return new self(
+			$data['outNumRA'] ?? null,
+			$data['outDigitoRA'] ?? null,
+			$data['outSiglaUFRA'] ?? null,
+			$data['outNomeAluno'] ?? null,
+			$data['outNomeMae'] ?? null,
+			$data['outNomePai'] ?? null,
+			$data['outNomeSocial'] ?? null,
+			$data['outDataNascimento'] ?? null
+		);
 	}
 }
