ac0bddcbdef1e739badee99e6f06568753428ad3
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-02 13:43:16 -0300

    Feat: added all getter & setter for OutEnderecoIndicativo

--- app/modules/sedsp/models/Student/OutEnderecoIndicativo.php
+++ app/modules/sedsp/models/Student/OutEnderecoIndicativo.php
@@ -2,68 +2,155 @@
 
 class OutEnderecoIndicativo
 {
-    private $outLogradouro;
-    private $outNumero;
-    private $outBairro;
-    private $outNomeCidade;
-    private $outUFCidade;
-    private $outLatitude;
-    private $outLongitude;
-    private $outCep;
+	public $outLogradouro;
+	public $outNumero;
+	public $outBairro;
+	public $outNomeCidade;
+	public $outLatitude;
+	public $outLongitude;
+	public $outCep;
+
+	public function __construct(
+		string $outLogradouro,
+		string $outNumero,
+		string $outBairro,
+		string $outNomeCidade,
+		string $outLatitude,
+		string $outLongitude,
+		string $outCep
+	) {
+		$this->outLogradouro = $outLogradouro;
+		$this->outNumero = $outNumero;
+		$this->outBairro = $outBairro;
+		$this->outNomeCidade = $outNomeCidade;
+		$this->outLatitude = $outLatitude;
+		$this->outLongitude = $outLongitude;
+		$this->outCep = $outCep;
+	}
 
 	/**
-	 * Summary of __construct
-	 * @param OutEnderecoIndicativo $enderecoIndicativo
+	 * Get the value of outLogradouro
 	 */
-	public function __construct($enderecoIndicativo) {
-		$this->outLogradouro = $enderecoIndicativo->outLogradouro;
-		$this->outNumero = $enderecoIndicativo->outNumero;
-		$this->outBairro = $enderecoIndicativo->outBairro;
-		$this->outNomeCidade = $enderecoIndicativo->outNomeCidade;
-		$this->outUFCidade = $enderecoIndicativo->outUFCidade;
-		$this->outLatitude = $enderecoIndicativo->outLatitude;
-		$this->outLongitude = $enderecoIndicativo->outLongitude;
-		$this->outCep = $enderecoIndicativo->outCep;
-	}
-	
-
-	public function getOutLogradouro(): string
+	public function getOutLogradouro()
 	{
 		return $this->outLogradouro;
 	}
 
-	public function getOutNumero(): string
+	/**
+	 * Set the value of outLogradouro
+	 */
+	public function setOutLogradouro($outLogradouro): self
+	{
+		$this->outLogradouro = $outLogradouro;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of outNumero
+	 */
+	public function getOutNumero()
 	{
 		return $this->outNumero;
 	}
 
-	public function getOutBairro(): string
+	/**
+	 * Set the value of outNumero
+	 */
+	public function setOutNumero($outNumero): self
+	{
+		$this->outNumero = $outNumero;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of outBairro
+	 */
+	public function getOutBairro()
 	{
 		return $this->outBairro;
 	}
 
-	public function getOutNomeCidade(): string
+	/**
+	 * Set the value of outBairro
+	 */
+	public function setOutBairro($outBairro): self
+	{
+		$this->outBairro = $outBairro;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of outNomeCidade
+	 */
+	public function getOutNomeCidade()
 	{
 		return $this->outNomeCidade;
 	}
 
-	public function getOutUfCidade(): string
+	/**
+	 * Set the value of outNomeCidade
+	 */
+	public function setOutNomeCidade($outNomeCidade): self
 	{
-		return $this->outUFCidade;
+		$this->outNomeCidade = $outNomeCidade;
+
+		return $this;
 	}
 
-	public function getOutLatitude(): string
+	/**
+	 * Get the value of outLatitude
+	 */
+	public function getOutLatitude()
 	{
 		return $this->outLatitude;
 	}
 
-	public function getOutLongitude(): string
+	/**
+	 * Set the value of outLatitude
+	 */
+	public function setOutLatitude($outLatitude): self
+	{
+		$this->outLatitude = $outLatitude;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of outLongitude
+	 */
+	public function getOutLongitude()
 	{
 		return $this->outLongitude;
 	}
 
-	public function getOutCep(): string
+	/**
+	 * Set the value of outLongitude
+	 */
+	public function setOutLongitude($outLongitude): self
+	{
+		$this->outLongitude = $outLongitude;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of outCep
+	 */
+	public function getOutCep()
 	{
 		return $this->outCep;
+	}
+
+	/**
+	 * Set the value of outCep
+	 */
+	public function setOutCep($outCep): self
+	{
+		$this->outCep = $outCep;
+
+		return $this;
 	}
 }
