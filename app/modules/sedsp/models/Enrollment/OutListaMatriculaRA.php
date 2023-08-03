a2a194e0c82cf4120bcad76c3ec84ae3c460bbb0
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-03 13:28:31 -0300

    Feat: added All properties nullable and static function fromJson from class OutListaMatriculaRA

--- app/modules/sedsp/models/Enrollment/OutListaMatriculaRA.php
+++ app/modules/sedsp/models/Enrollment/OutListaMatriculaRA.php
@@ -4,20 +4,76 @@ class OutListaMatriculaRA
 {
 	public $outAluno;
 
-	/** @var OutListaMatriculas[]*/
+	/** @var OutListaMatriculas[]|null */
 	public $outListaMatriculas;
 	public $outProcessoID;
 
-	
+	/**
+	 * @param OutListaMatriculas[]|null $outListaMatriculas
+	 */
+	public function __construct(
+		?OutAluno $outAluno,
+		?array $outListaMatriculas,
+		?string $outProcessoID
+	) {
+		$this->outAluno = $outAluno;
+		$this->outListaMatriculas = $outListaMatriculas;
+		$this->outProcessoID = $outProcessoID;
+	}
+
+	public function getOutAluno(): ?OutAluno
+	{
+		return $this->outAluno;
+	}
 
 	/**
-	 * Summary of __construct
-	 * @param OutListaMatriculaRA $outListaMatriculaRA
+	 * @return OutListaMatriculas[]|null
+	 */
+	public function getOutListaMatriculas(): ?array
+	{
+		return $this->outListaMatriculas;
+	}
+
+	public function getOutProcessoId(): ?string
+	{
+		return $this->outProcessoID;
+	}
+
+	public function setOutAluno(?OutAluno $outAluno): self
+	{
+		$this->outAluno = $outAluno;
+		return $this;
+	}
+
+	/**
+	 * @param OutListaMatriculas[]|null $outListaMatriculas
+	 */
+	public function setOutListaMatriculas(?array $outListaMatriculas): self
+	{
+		$this->outListaMatriculas = $outListaMatriculas;
+		return $this;
+	}
+
+	public function setOutProcessoId(?string $outProcessoID): self
+	{
+		$this->outProcessoID = $outProcessoID;
+		return $this;
+	}
+
+	/**
+	 * Summary of fromJson
+	 * @param array $data
 	 * @return OutListaMatriculaRA
 	 */
-	public function __construct($outListaMatriculaRA) {
-		$this->outAluno = new OutDadosPessoais($outListaMatriculaRA->outAluno);
-		$this->outListaMatriculas = new OutListaMatriculas($outListaMatriculaRA->outListaMatriculas);
-		$this->outProcessoID = $outListaMatriculaRA->outProcessoID;
-    }
+	public static function fromJson(array $data): self
+	{
+		return new self(
+			$data['outAluno'] ?? null,
+			($data['outListaMatriculas'] ?? null) !== null ? array_map(static function($data) {
+				return OutListaMatriculas::fromJson($data);
+			}, 
+			$data['outListaMatriculas']) : null,
+			$data['outProcessoID'] ?? null
+		);
+	}	
 }
