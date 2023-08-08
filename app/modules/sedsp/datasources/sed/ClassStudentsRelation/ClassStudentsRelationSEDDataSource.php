86af9b80d98b96a5e2bb6dd5aadf50840b74fda8
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-08 17:04:03 -0300

    Feat: added function in its source class

--- app/modules/sedsp/datasources/sed/ClassStudentsRelation/ClassStudentsRelationSEDDataSource.php
+++ app/modules/sedsp/datasources/sed/ClassStudentsRelation/ClassStudentsRelationSEDDataSource.php
@@ -7,6 +7,9 @@ require 'app/vendor/autoload.php';
 
 class ClassStudentsRelationSEDDataSource extends SedDataSource
 {
+    const LENGTH_IN_NUM_CLASS = 9;
+    const LENGTH_IN_ANO_LETIVO = 4;
+    
     /**
      * Summary of getRelacaoClasses
      * @param InRelacaoClasses $inRelacaoClasses
@@ -24,6 +27,35 @@ class ClassStudentsRelationSEDDataSource extends SedDataSource
             return new OutErro($e);
         } catch (Exception $exception) {
             throw $exception;
+        }
+    }
+
+        /**
+     * Summary of getClassroom
+     * @param InFormacaoClasse $inClassroom
+     * @return OutFormacaoClasse|OutErro
+     */
+    public function getClassroom(InFormacaoClasse $inClassroom)
+    {
+        try {
+            if (empty($inClassroom->inNumClasse)) {
+                throw new InvalidArgumentException("Entrada inválida: dado incompleto.");
+            }
+
+            if (strlen($inClassroom->inNumClasse) > self::LENGTH_IN_NUM_CLASS) {
+                throw new InvalidArgumentException("Entrada inválida: tamanho máximo excedido.");
+            };
+
+            $url = '/ncaapi/api/RelacaoAlunosClasse/FormacaoClasse';
+            $response = $this->getApiResponse('GET', $url, $inClassroom);
+            return OutFormacaoClasse::fromJson($response);
+
+        } catch (InvalidArgumentException $e) {
+            return new OutErro($e);
+        } catch (ClientException $e) {
+            return new OutErro($e);
+        } catch (Exception $e) {
+            return new OutErro($e);
         }
     }
 
