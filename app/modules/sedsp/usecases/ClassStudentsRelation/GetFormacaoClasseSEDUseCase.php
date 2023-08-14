0b4a1ca802c4aad306459a6705c973a8ffad2004
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-13 23:58:29 -0300

    Feat: added class GetFormacaoClasseSEDUseCase

---
+++ app/modules/sedsp/usecases/ClassStudentsRelation/GetFormacaoClasseSEDUseCase.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * UseCase class GetFormacaoClasseSED
+ */
+class GetFormacaoClasseSEDUseCase 
+{
+
+    /**
+     * Summary of exec
+     * @param InFormacaoClasse $inNumClasse
+     * @throws InvalidArgumentException 
+     */
+    function exec(InFormacaoClasse $inNumClasse)
+    {
+        if (empty($inNumClasse->getInNumClasse())) {
+            throw new InvalidArgumentException("Número da classe (turma) é obrigatório.");
+        }
+        
+        if (strlen($inNumClasse->getInNumClasse()) > 9) {
+            throw new InvalidArgumentException("Número da classe (turma) deve ter no máximo 9 caracteres.");
+        }
+
+        $formacaoClasseDataSource = new ClassStudentsRelationSEDDataSource(); 
+        $formacaoClasse = $formacaoClasseDataSource->getClassroom($inNumClasse);
+    
+        return ClassroomMapper::parseToTAGFormacaoClasse($formacaoClasse);
+    }
+}
