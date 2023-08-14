0a9fd8af30af2c72008167462950371181501d74
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-14 10:54:29 -0300

    Feat: added class GetConsultaTurmaClasseSEDUseCase

---
+++ app/modules/sedsp/usecases/Classroom/GetConsultaTurmaClasseSEDUseCase.php
@@ -0,0 +1,27 @@
+<?php
+
+class GetConsultaTurmaClasseSEDUseCase
+{
+    /**
+     * Summary of exec
+     * @param InConsultaTurmaClasse $inConsultaTurmaClasse
+     * @throws InvalidArgumentException
+     */
+    public function exec(InConsultaTurmaClasse $inConsultaTurmaClasse)
+    {
+        $inAnoLetivo = $inConsultaTurmaClasse->getInAnoLetivo();
+        $inNumClasse = $inConsultaTurmaClasse->getInNumClasse();
+
+        if (empty($inAnoLetivo) || strlen($inAnoLetivo) > 4) {
+            throw new InvalidArgumentException('O campo "inAnoLetivo" deve conter exatamente 4 caracteres.');
+        }
+
+        if (empty($inNumClasse) || strlen($inNumClasse) > 9) {
+            throw new InvalidArgumentException('O campo "inNumClasse" deve conter exatamente 9 caracteres.');
+        }
+
+        $classroomSEDDataSource = new ClassroomSEDDataSource();
+        $response = $classroomSEDDataSource->getConsultClass($inConsultaTurmaClasse);
+        return ClassroomMapper::parseToTAGFormacaoClasse($response);
+    }
+}
