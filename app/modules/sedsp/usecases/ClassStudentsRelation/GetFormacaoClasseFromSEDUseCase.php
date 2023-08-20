916b7f4f7f728d7819b5015cbfc7c77383063d49
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-19 22:34:03 -0300

    Refactoring: code improvement

--- app/modules/sedsp/usecases/ClassStudentsRelation/GetFormacaoClasseFromSEDUseCase.php
+++ app/modules/sedsp/usecases/ClassStudentsRelation/GetFormacaoClasseFromSEDUseCase.php
@@ -14,47 +14,44 @@ class GetFormacaoClasseFromSEDUseCase
      */
     function exec(InFormacaoClasse $inNumClasse)
     {
-        if (empty($inNumClasse->getInNumClasse())) {
+        if (empty($inNumClasse->getInNumClasse()))
             throw new InvalidArgumentException("Número da classe (turma) é obrigatório.");
-        }
-        
-        if (strlen($inNumClasse->getInNumClasse()) > 9) {
+
+        if (strlen($inNumClasse->getInNumClasse()) > 9)
             throw new InvalidArgumentException("Número da classe (turma) deve ter no máximo 9 caracteres.");
+
+        $formacaoClasseDataSource = new ClassStudentsRelationSEDDataSource();
+        $response = $formacaoClasseDataSource->getClassroom($inNumClasse);
+        $classroom = Classroom::model()->find('inep_id = :inep_id', [':inep_id' => $response->getOutNumClasse()]);
+        
+        if ($classroom !== null) {
+            CVarDumper::dump('A Classe {' . $classroom->school_inep_fk . '-' . $classroom->name . '} já está cadastrada no TAG!', 10, true);
+            return;
         }
 
-        $formacaoClasseDataSource = new ClassStudentsRelationSEDDataSource(); 
-        $response = $formacaoClasseDataSource->getClassroom($inNumClasse);
-        $mapper = (object) ClassroomMapper::parseToTAGFormacaoClasse($response);
-        
-        $classroom = Classroom::model()->find('inep_id = :inep_id', [':inep_id' => $mapper->Classroom->inep_id]);
-
-        if ($classroom !== null) {
-            Yii::app()->user->setFlash('error', Yii::t('default', 'Unidade gestora solicitada não existe!'));
-        } else {
+        $transaction = Yii::app()->db->beginTransaction();
+        try {
+            $mapper = (object) ClassroomMapper::parseToTAGFormacaoClasse($response);
             $classroom = new Classroom();
             $classroom->attributes = $mapper->Classroom->getAttributes();
+            
+            if ($classroom->validate() && $classroom->save()) {
+                foreach ($mapper->Students as $student) {
+                    $studentIdentification = new StudentIdentification();
+                    $studentIdentification->attributes = $student->getAttributes();
 
-            $transaction = Yii::app()->db->beginTransaction();
-            try {
-                if ($classroom->validate() && $classroom->save()) {
-                    foreach ($mapper->Students as $student) {
-                        $studentIdentification = new StudentIdentification();
-                        $studentIdentification->attributes = $student->getAttributes();
-
-                        if ($studentIdentification->validate() && $studentIdentification->save()) {
-                            CVarDumper::dump('Classroom cadastrada com sucesso.', 10, true);
-                        } else {
-                            throw new SedspException('Não foi possível salvar os dados do aluno no banco de dados.');
-                        }
-                    }
-                    $transaction->commit();
-                } else {
-                    throw new SedspException('Os dados da classe não são válidos.');
-                }
-            } catch (Exception $e) {
-                CVarDumper::dump($e->getMessage(), 10, true);
-                $transaction->rollback();
+                    if ($studentIdentification->validate() && $studentIdentification->save()) 
+                        CVarDumper::dump('Aluno ' . $student->getOutNumRa . ' cadastrada com sucesso.', 10, true);
+                    else 
+                        throw new SedspException('Não foi possível salvar os dados do aluno no banco de dados.');
+                }     
+            } else {
+                throw new SedspException('Os dados da classe não são válidos.');
             }
+            $transaction->commit();
+        } catch (Exception $e) {
+            CVarDumper::dump($e->getMessage(), 10, true);
+            $transaction->rollback();
         }
     }
 }
