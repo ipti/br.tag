44155c716ed258845182400efdd4991aa9d89e52
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-25 01:22:31 -0300

    Feat: register StudentEnrollment

--- app/modules/sedsp/usecases/ClassStudentsRelation/GetFormacaoClasseFromSEDUseCase.php
+++ app/modules/sedsp/usecases/ClassStudentsRelation/GetFormacaoClasseFromSEDUseCase.php
@@ -14,21 +14,43 @@ class GetFormacaoClasseFromSEDUseCase
      */
     function exec(InFormacaoClasse $inNumClasse)
     {
-
         $formacaoClasseDataSource = new ClassStudentsRelationSEDDataSource();
         $response = $formacaoClasseDataSource->getClassroom($inNumClasse);
 
         try {
             $mapper = (object) ClassroomMapper::parseToTAGFormacaoClasse($response);
-           
+            $classroom = $mapper->Classroom;
+            
             foreach ($mapper->Students as $student) {
-                $studentIdentification = new StudentIdentification();
-                $studentIdentification->attributes = $student->getAttributes();
 
-                if ($studentIdentification->validate() && $studentIdentification->save()) 
-                    CVarDumper::dump('Aluno cadastrado com sucesso.', 10, true);
-                else 
-                    CVarDumper::dump($studentIdentification->getErrors(), 10, true);
+                $inAluno = new InAluno($student->inep_id, null, $student->uf);          
+                
+                //Verifica se está cadastrado em student_identification
+                $registerStudent = new GetExibirFichaAlunoFromSEDUseCase();
+                $registerStudent->exec($inAluno);
+
+                $studentEnrollmentModel =  StudentEnrollment::model()->find(
+                    'school_inep_id_fk = :school_inep_id_fk AND student_fk = :student_fk AND classroom_fk = :classroom_fk',
+                    [
+                        ':school_inep_id_fk' => $classroom->school_inep_fk,
+                        ':student_fk' => $student->inep_id,
+                        ':classroom_fk' => $classroom->inep_id
+                    ]
+                );
+
+                //Verifica se está cadastrado em student_enrollment
+                if($studentEnrollmentModel === null){
+                    $studentEnrollment = new StudentEnrollment();
+                    $studentEnrollment->school_inep_id_fk = $classroom->school_inep_fk;
+                    $studentEnrollment->student_inep_id = $student->inep_id;
+                    $studentEnrollment->student_fk = StudentIdentification::model()->find('inep_id = :inep_id', [':inep_id' => $student->inep_id])->id;
+                    $studentEnrollment->classroom_fk = Classroom::model()->find('inep_id = :inep_id', [':inep_id' => $classroom->inep_id])->id ;
+    
+                    if ($studentEnrollment->validate() && $studentEnrollment->save()) 
+                        CVarDumper::dump('Aluno matriculado com sucesso.', 10, true);
+                    else 
+                        CVarDumper::dump($studentEnrollment->getErrors(), 10, true);
+                } 
             }      
         } catch (Exception $e) {
             CVarDumper::dump($e->getMessage(), 10, true);
