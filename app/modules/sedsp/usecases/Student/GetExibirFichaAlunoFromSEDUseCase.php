99f5114acd0e65d4541b2f1053d60e5d91522a1f
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-28 11:18:08 -0300

    Refactor: clean architecture applied to code

--- app/modules/sedsp/usecases/Student/GetExibirFichaAlunoFromSEDUseCase.php
+++ app/modules/sedsp/usecases/Student/GetExibirFichaAlunoFromSEDUseCase.php
@@ -6,46 +6,49 @@ class GetExibirFichaAlunoFromSEDUseCase
     {
         $studentDatasource = new StudentSEDDataSource();
         $response = $studentDatasource->exibirFichaAluno($inAluno);
-        $doc = $response->getOutDocumentos();
-        $dados = $response->getOutDadosPessoais();
+       
+        $cpf = $response->getOutDocumentos()->getOutCpf();
+        $name = $response->getOutDadosPessoais()->getOutNomeAluno();
 
-        $studentCpf = StudentDocumentsAndAddress::model()->find('cpf = :cpf', [':cpf' => $doc->getOutCpf()])->cpf;
-        $studentName = StudentIdentification::model()->find('name = :name', [':name' => $dados->getOutNomeAluno()])->name;
+        $studentExists = $this->checkIfStudentIsEnrolled($cpf, $name);
         
-        //Verifica se o cpf ou nome do aluno já está no cadastrado
-		if($studentCpf !== null or $studentName !== null)
+		if($studentExists)
             return false;
+
         try {
             $mapper = (object) StudentMapper::parseToTAGExibirFichaAluno($response);
-            $studentIdentification = new StudentIdentification();
-            $studentIdentification->attributes = $mapper->StudentIdentification->getAttributes();
-            $studentIdentification->gov_id = $mapper->StudentIdentification->gov_id;
-
-            $studentDocumentsAndAddress = new StudentDocumentsAndAddress();
-            $studentDocumentsAndAddress->attributes = $mapper->StudentDocumentsAndAddress->getAttributes();
-
-            $this->validateStudentData($studentIdentification, $studentDocumentsAndAddress);
-
-            if ($studentIdentification->save() && $studentDocumentsAndAddress->save()) {
-                return true;
-            } else {
-                throw new SedspException('Não foi possível salvar os dados no banco de dados.');
-            }
+            
+            $this->createAndSaveStudentIdentification($mapper->StudentIdentification);
+            $this->createAndSaveStudentDocumentsAndAddress($mapper->StudentDocumentsAndAddress->getAttributes());
+            
         } catch (Exception $e) {
             CVarDumper::dump($e->getMessage(), 10, true);
         }
     }
 
-    
 
-    private function validateStudentData(StudentIdentification $studentIdentification, StudentDocumentsAndAddress $studentDocumentsAndAddress)
+    function createAndSaveStudentDocumentsAndAddress($attributes)
     {
-        if (!$studentIdentification->validate()) {
-            throw new SedspException('Os dados de identificação do aluno não são válidos.');
-        }
+        $studentDocumentsAndAddress = new StudentDocumentsAndAddress();
+        $studentDocumentsAndAddress->attributes = $attributes;
 
-        if (!$studentDocumentsAndAddress->validate()) {
-            throw new SedspException('Os dados dos documentos e endereço do aluno não são válidos.');
-        }
+        return ($studentDocumentsAndAddress->validate() && $studentDocumentsAndAddress->save()) ? true : false;
+    }
+
+    function createAndSaveStudentIdentification($attributes)
+    {
+        $studentIdentification = new StudentIdentification();
+        $studentIdentification->attributes = $attributes->getAttributes();
+        $studentIdentification->gov_id = $attributes->gov_id;
+
+        return ($studentIdentification->validate() && $studentIdentification->save()) ? true : false;
+    }
+
+    function checkIfStudentIsEnrolled($cpf, $name)
+    {
+        $studentCpf = StudentDocumentsAndAddress::model()->find('cpf = :cpf', [':cpf' => $cpf])->cpf;
+        $studentName = StudentIdentification::model()->find('name = :name', [':name' => $name])->name;
+        
+        return ($studentCpf !== null or $studentName !== null) ? true : false;
     }
 }
