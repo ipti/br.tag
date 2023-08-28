959a89c7c150949ddc7e1c69d948aaebfb326af6
Nathan Santos <nathanaelsantos15@gmail.com>
2023-08-28 11:27:10 -0300

    Refactor: clean architecture applied to code

--- app/modules/sedsp/usecases/School/GetEscolasFromSEDUseCase.php
+++ app/modules/sedsp/usecases/School/GetEscolasFromSEDUseCase.php
@@ -2,43 +2,79 @@
 
 class GetEscolasFromSEDUseCase
 {
-
     function exec(InEscola $inEscola)
-    {
-        $schoolSEDDataSource = new SchoolSEDDataSource();
-        $response = $schoolSEDDataSource->getSchool($inEscola);
+    {      
+        $result = $this->fetchSchoolData($inEscola);
 
-        $inepId = '35' . $response->getOutEscolas()[0]->getOutCodEscola();
-        $schoolModel = SchoolIdentification::model()->find('inep_id = :inep_id', [':inep_id' => $inepId]);
-        $mapper = (object) SchoolMapper::parseToTAGSchool($response);
-        if ($schoolModel->inep_id !== null) {
-            $inAnoLetivo = Yii::app()->user->year;
-            $inCodEscola = substr($inepId, 2); // remove os 2 dígitos iniciais do código da escola, referente ao código do estado
-            $inRelacaoClasses = new InRelacaoClasses($inAnoLetivo, $inCodEscola, null, null, null, null);
-                
-            $classes = new GetRelacaoClassesFromSEDUseCase();
-            $classes->exec($inRelacaoClasses);
-    
-            CVarDumper::dump('Escola '. $mapper->SchoolIdentification->inep_id . ' - ' . $mapper->SchoolIdentification->name . ' salva com sucesso no TAG.', 10, true);
+        $schoolId  = $this->buildSchoolId($result);
+        $schoolModel = $this->findSchoolById($schoolId);
+        
+        $mapper = (object) SchoolMapper::parseToTAGSchool($result);
+
+        if ($schoolModel->inep_id !== null) {  
+            $inRelacaoClasses = $this->getClassesFromSED($schoolId);        
+            $this->getSchoolClasses($inRelacaoClasses);
+
             return true;
         } else {
-            $schoolIdentification = new SchoolIdentification();
-            $schoolIdentification->attributes = $mapper->SchoolIdentification->getAttributes();
-    
-            if ($schoolIdentification->validate() && $schoolIdentification->save()) {   
-                $inAnoLetivo = Yii::app()->user->year;
-                $inCodEscola = substr($inepId, 2); // remove os 2 dígitos iniciais do código da escola, referente ao código do estado
-                $inRelacaoClasses = new InRelacaoClasses($inAnoLetivo, $inCodEscola, null, null, null, null);
-                
-                $classes = new GetRelacaoClassesFromSEDUseCase();
-                $classes->exec($inRelacaoClasses);
-    
-                CVarDumper::dump('Escola '. $mapper->SchoolIdentification->inep_id . ' - ' . $mapper->SchoolIdentification->name . ' salva com sucesso no TAG.', 10, true);
+            $schoolAttributes = $mapper->SchoolIdentification->getAttributes();
+            $createdSchool = $this->createAndSaveNewSchool($schoolAttributes);
+            
+            $transaction = Yii::app()->db->beginTransaction();
+            
+            if ($createdSchool) {    
+                $transaction->commit();
+                $inRelacaoClasses = $this->getClassesFromSED($schoolId); 
+                $this->getSchoolClasses($inRelacaoClasses);
+
                 return true;
-            }else {
-                CVarDumper::dump($schoolIdentification->getErrors(), 10, true);
+            } else {
+                $transaction->rollback();
                 throw new SedspException('Não foi possível salvar a escola no banco de dados.');
             }
         }
-    }         
+    } 
+
+    function buildSchoolId($schoolData)
+    {
+        return '35' . $schoolData->getOutEscolas()[0]->getOutCodEscola();
+    }
+
+    function fetchSchoolData(InEscola $inEscola)
+    {
+        $dataSource = new SchoolSEDDataSource();
+        return $dataSource->getSchool($inEscola);
+    }
+
+    private function findSchoolById($schoolId)
+    {
+        return SchoolIdentification::model()->find('inep_id = :inep_id', [':inep_id' => $schoolId]);
+    }
+
+    function getClassesFromSED($schoolId)
+    {
+        $inAnoLetivo = Yii::app()->user->year;
+        $inCodEscola = $this->extractStateCode($schoolId); 
+        return new InRelacaoClasses($inAnoLetivo, $inCodEscola, null, null, null, null);
+    }
+
+    function createAndSaveNewSchool($schoolAttributes)
+    {
+        $school = new SchoolIdentification();
+        $school->attributes = $schoolAttributes;
+        
+        return ($school->validate() && $school->save()) ? true : false;
+    }
+
+    function extractStateCode($schoolId)
+    {
+        // remove os 2 dígitos iniciais do código da escola, referente ao código do estado
+        return substr($schoolId, 2);
+    }
+
+    function getSchoolClasses(InRelacaoClasses $inRelacaoClasses)
+    {
+        $classes = new GetRelacaoClassesFromSEDUseCase();
+        $classes->exec($inRelacaoClasses);
+    }
 }
