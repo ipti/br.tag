5c9ff3a6a2a7fa7f13c12375b17a87ea816f9a27
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2023-08-30 09:51:57 -0300

    feat: map class

--- app/extensions/bncc-import/FundamentalDisciplinesMap.php
+++ app/extensions/bncc-import/FundamentalDisciplinesMap.php
@@ -3,6 +3,7 @@
 class FundamentalDisciplinesMap
 {
     /**
+     * LÍNGUA PORTUGUESA
      * Colunas do CSV:
      * Competências específicas de componente
      * Campos de atuação
@@ -65,6 +66,130 @@ class FundamentalDisciplinesMap
     }
 
     /**
+     * LÍNGUA INGLESA
+     * Colunas do CSV:
+     * Competências específicas de componente
+     * Eixo
+     * Unidades temáticas
+     * Objetos de conhecimento
+     * Habilidades
+     * @return array
+     */
+    public static function parseEnglishLanguage($line)
+    {
+        $parsedData = [];
+
+        $component = trim($line[0]);
+        $skills = trim($line[1]);
+        $axle = trim($line[2]);
+        $unity = trim($line[3]);
+        $objective = trim($line[4]);
+        $abilitie = trim($line[5]);
+
+        if (!empty($component)) {
+            // Inicializar a categoria atual
+            $currentCategory = $component;
+            if (!isset($parsedData[$currentCategory])) {
+                $parsedData[$currentCategory] = [
+                    'name' => $currentCategory,
+                    'type' => "COMPONENTE",
+                    'children' => []
+                ];
+            }
+            $currentSubCategory = '';
+        }
+
+        // Competências específicas de componente
+        if (!empty($skills)) {
+            if (!isset($parsedData[$component]['children'][$skills])) {
+                $parsedData[$component]['children'][$skills] = ['name' => $skills, 'type' => "COMPETÊNCIAS ESPECÍFICAS DE COMPONENTE", 'children' => []];
+            }
+        }
+
+        // Eixo
+        if (!empty($fields)) {                                
+            $parsedData[$component]['children'][$skills]['children'][$axle] = ["name" => $axle, 'type' => "EIXO", 'children' => []];
+        }
+
+        // Unidades temáticas
+        if (!empty($fields)) {                                
+            $parsedData[$component]['children'][$skills]['children'][$axle]['children'][$unity] = ["name" => $unity, 'type' => "UNIDADE TEMÁTICA", 'children' => []];
+        }
+
+        // Objetos de conhecimento	
+        if (!empty($objective)) {                                
+            $parsedData[$component]['children'][$skills]['children'][$axle]['children'][$unity]['children'][$objective] = ["name" => $objective, 'type' => "OBJETO DE CONHECIMENTO", 'children' => []];
+        }
+
+        // Habilidades
+        if (!empty($abilitie)) {
+            $parsedData[$component]['children'][$skills]['children'][$axle]['children'][$unity]['children'][$objective]['children'][$abilitie]['children'][] = ["name" => $abilitie, 'type' => "HABILIDADE"];                
+        }
+
+        return $parsedData;
+    }
+
+    /**
+     * ARTE - EDUCAÇÃO FÍSICA - GEOGRAFIA
+     * HISTÓRIA - ENSINO RELIGIOSO
+     * Colunas do CSV:
+     * Competências específicas de componente
+     * Unidades temáticas
+     * Objetos de conhecimento
+     * Habilidades
+     * @return array
+     */
+    public static function parseArtEducationPhysicalHumanitiesAndReligion($line)
+    {
+        $parsedData = [];
+
+        $component = trim($line[0]);
+        $skills = trim($line[1]);
+        $unity = trim($line[2]);
+        $objective = trim($line[3]);
+        $abilitie = trim($line[4]);
+
+        if (!empty($component)) {
+            // Inicializar a categoria atual
+            $currentCategory = $component;
+            if (!isset($parsedData[$currentCategory])) {
+                $parsedData[$currentCategory] = [
+                    'name' => $currentCategory,
+                    'type' => "COMPONENTE",
+                    'children' => []
+                ];
+            }
+            $currentSubCategory = '';
+        }
+
+        // Competências específicas de componente
+        if (!empty($skills)) {
+            if (!isset($parsedData[$component]['children'][$skills])) {
+                $parsedData[$component]['children'][$skills] = ['name' => $skills, 'type' => "COMPETÊNCIAS ESPECÍFICAS DE COMPONENTE", 'children' => []];
+            }
+        }
+
+        // Unidades temáticas
+        if (!empty($fields)) {                                
+            $parsedData[$component]['children'][$skills]['children'][$unity] = ["name" => $unity, 'type' => "UNIDADE TEMÁTICA", 'children' => []];
+        }
+
+        // Objetos de conhecimento	
+        if (!empty($objective)) {                                
+            $parsedData[$component]['children'][$skills]['children'][$unity]['children'][$objective] = ["name" => $objective, 'type' => "OBJETO DE CONHECIMENTO", 'children' => []];
+        }
+
+        // Habilidades
+        if (!empty($abilitie)) {
+            $parsedData[$component]['children'][$skills]['children'][$unity]['children'][$objective]['children'][$abilitie]['children'][] = ["name" => $abilitie, 'type' => "HABILIDADE"];                
+        }
+
+        return $parsedData;
+    }
+
+
+    /**
+     * MATEMÁTICA
      * Colunas do CSV:
      * Unidades temáticas	
      * Objetos de conhecimento	
@@ -72,7 +197,7 @@ class FundamentalDisciplinesMap
      * Habilidades	
      * @return array
      */
-    public static function parseMathemathicData($line)
+    public static function parseMathemathicAndSciencesData($line)
     {
         $parsedData = [];
 
