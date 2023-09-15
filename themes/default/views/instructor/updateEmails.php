e4cfcaed424a83c37220a53d7a6d90a29dd581d1
Tori Calasans <tori.ti@ipti.org.br>
2023-09-15 12:23:31 -0300

    feito alteração na responsividade da tela de emails dos professores

--- themes/default/views/instructor/updateEmails.php
+++ themes/default/views/instructor/updateEmails.php
@@ -30,20 +30,27 @@ $form = $this->beginWidget('CActiveForm', [
         <div class="alert alert-danger">Preencha os e-mails corretamente.</div>
         <div class="row wrap">
             <?php $half_count = ceil(count($instructors) / 2); ?>
-            <div class="column is-one-third t-field-text">
+            <div class="column is-two-fifths t-field-text">
                 <?php foreach (array_slice($instructors, 0, $half_count) as $instructor) : ?>
-                    <label class="t-field-text__label" for="instructor"><?= $instructor->name ?></label>
+                    <label class="t-field-text__label" style="white-space:nowrap; overflow-y: hidden; overflow-x: auto; text-overflow:ellipsis" for="instructor"><?= $instructor->name ?></label>
                     <input name="<?= $instructor->id ?>" class="t-field-text__input" type="email">
                 <?php endforeach ?>
             </div>
-            <div class="column is-one-third t-field-text">
+            <div class="column is-two-fifths t-field-text">
                 <?php foreach (array_slice($instructors, $half_count) as $instructor) : ?>
-                    <label class="t-field-text__label" for="instructor"><?= $instructor->name ?></label>
+                    <label class="t-field-text__label" style="white-space:nowrap; overflow-y: hidden; overflow-x: auto;  text-overflow:ellipsis" for="instructor"><?= $instructor->name ?></label>
                     <input name="<?= $instructor->id ?>" class="t-field-text__input" type="email">
                 <?php endforeach ?>
             </div>
         </div>
         <div class="clear"></div>
+    </div>
+    <div class="row reverse show--tablet">
+        <div class="tag-buttons-container buttons">
+            <button class="t-button-primary  last save-school-button" type="button">
+                <?php echo Yii::t('default', 'Save'), array('id' => 'save-emails'); ?>
+            </button>
+        </div>
     </div>
 </div>
 <?php $this->endWidget(); ?>
