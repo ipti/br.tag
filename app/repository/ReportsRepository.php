ddd0a7fe6aefbabecf2ffeef772903a2b81b135f
Carlos Henrique Santos Silveira <carlos.henrique@dcomp.ufs.br>
2023-09-28 11:55:14 -0300

    fix: change include

--- app/repository/ReportsRepository.php
+++ app/repository/ReportsRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/yiisoft/yii/framework/db/schema/CDbCriteria.php';
+include_once __DIR__.'/../vendor/yiisoft/yii/framework/db/schema/CDbCriteria.php';
 
 class ReportsRepository {
 
