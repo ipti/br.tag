da894012de451835e330fc4c9a4def9ae7c55119
Igor Gonçalves <igor_goncalves@live.com>
2023-10-02 22:00:21 -0300

    feat: chage script to auto generate aliases

--- app/migrations/2023-09-19_enrollment_statistics_report/scripts.sql
+++ app/migrations/2023-09-19_enrollment_statistics_report/scripts.sql
@@ -1,2 +1,6 @@
-ALTER TABLE `edcenso_stage_vs_modality`
+ALTER TABLE
 ADD `alias` varchar(20) COLLATE 'utf8_unicode_ci' NULL AFTER `name`;
+
+
+UPDATE `edcenso_stage_vs_modality`
+ set alias = SUBSTRING(trim(SUBSTRING_INDEX(SUBSTRING_INDEX(name , '-', -1), '-', -1)), 1, 20)
