24ba1120b0e93d6acd091499e79d1ac4b4f59f81
Igor Gonçalves <igor_goncalves@live.com>
2023-10-16 10:57:18 -0300

    Fix/reports count enrollment (#491)
    * fix: order reports
    
    * fix: student counts based on status
    
    * fix: remove code smells

--- app/migrations/2023-09-04_create_abbreviation_component_curricular/create_abbreviation.sql
+++ app/migrations/2023-09-04_create_abbreviation_component_curricular/create_abbreviation.sql
@@ -1,2 +1,9 @@
 -- Adiciona coluna na tabela original de disciplinas
 ALTER TABLE edcenso_discipline ADD abbreviation varchar(15);
+
+ALTER TABLE
+ADD `alias` varchar(20) COLLATE 'utf8_unicode_ci' NULL AFTER `name`;
+
+
+UPDATE `edcenso_stage_vs_modality`
+ set alias = SUBSTRING(trim(SUBSTRING_INDEX(SUBSTRING_INDEX(name , '-', -1), '-', -1)), 1, 20)
