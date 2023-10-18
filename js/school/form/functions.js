e0d2e0c7a734a4da031c370f7e2feb75bb64b3d7
gabriel-IPTI <ti.gabriel@ipti.org.br>
2023-10-18 11:21:31 -0300

    Mudando contado nas tealas de erro

--- js/school/form/functions.js
+++ js/school/form/functions.js
@@ -1,3 +1,5 @@
+const { template } = require("modernizr");
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
