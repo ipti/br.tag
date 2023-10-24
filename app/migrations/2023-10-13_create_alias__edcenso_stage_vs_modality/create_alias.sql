bd5aa3ed7fbebc608769ce09b4eb8690115a37fc
TI GUSTAVO <ti.gustavo@ipti.org.br>
2023-10-24 11:22:30 -0300

    feat(admin): Removendo o controller adicional de gerenciar etapas e adicionando script de abreviação na migration de criar abreviação de gerencia de etapas

--- app/migrations/2023-10-13_create_alias__edcenso_stage_vs_modality/create_alias.sql
+++ app/migrations/2023-10-13_create_alias__edcenso_stage_vs_modality/create_alias.sql
@@ -1,2 +1,5 @@
 -- Adiciona coluna alias na tabela edcenso_stage_vs_modality
 ALTER TABLE edcenso_stage_vs_modality ADD alias varchar(15);
+
+UPDATE `edcenso_stage_vs_modality`
+ set alias = SUBSTRING(trim(SUBSTRING_INDEX(SUBSTRING_INDEX(name , '-', -1), '-', -1)), 1, 20)
