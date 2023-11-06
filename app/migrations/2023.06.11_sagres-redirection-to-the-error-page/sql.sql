fd1674cf100a0aedfb35ee58af26e60b54cc36ca
Nathan <ti.natan@ipti.org.br>
2023-11-06 13:39:33 -0300

    Feat: added migrations

---
+++ app/migrations/2023.06.11_sagres-redirection-to-the-error-page/sql.sql
@@ -0,0 +1,5 @@
+ALTER TABLE inconsistency_sagres ADD idClass INTEGER NULL;
+ALTER TABLE inconsistency_sagres ADD idSchool varchar(10) NULL;
+ALTER TABLE inconsistency_sagres ADD identifier TINYINT NULL;
+ALTER TABLE inconsistency_sagres ADD idStudent INT NULL;
+ALTER TABLE inconsistency_sagres ADD idProfessional INT NULL;
