81e8f4c1e1edf16ae43755c6efd0dc6074496db3
Evellyn Jade <97754536+evellynreis@users.noreply.github.com>
2023-11-06 09:48:01 -0300

    tests/ add tests in matrix

---
+++ tests/builders/MatrixBuilder.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'app/vendor/autoload.php';
+require_once __DIR__.'/../providers/CustomProvider.php';
+
+$yiit= __DIR__.'\..\..\app\vendor\yiisoft\yii\framework\yiit.php';
+require_once($yiit);
+
+$config= __DIR__.'/../../app/config/test.php';
+
+Yii::createWebApplication($config);
+
+/**
+ * @property [] $matrix
+ * @property Faker\Generator $faker
+ * @property CustomProvider $fakerCustom
+*/
+class MatrixBuilder
+{
+    private $faker = null;
+    private $fakerCustom = null;
+
+     /**
+     * Summary of matrix
+     * @var $matrix
+     */
+    public function __construct ()
+    {
+        $this->faker = Faker\Factory::create('pt_BR');
+        $this->fakerCustom = new CustomProvider($this->faker);
+        $this->matrix = [];
+    }
+
+    public function builderAddMatrix ()
+    {
+        $this->matrix['stages'] = '1'; // Educação Infantil - Creche (0 a 3 anos)
+        $this->matrix['disciplines'] = $this->faker->randomElement(array (1,2,3,4,5,6,7,8,9,10));
+        $this->matrix['workload'] = $this->faker->randomDigit();
+        $this->matrix['credits'] = $this->faker->randomDigit();
+
+        return $this->matrix;
+    }
+
+}
