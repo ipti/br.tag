c4827c6f9261e239b4624c06bf2f107c919d98a5
Nathan <ti.natan@ipti.org.br>
2023-11-06 14:12:39 -0300

    Fix: Use VARCHAR2 instead of VARCHAR.

--- app/migrations/2023.06.11_sagres-redirection-to-the-error-page/sql.sql
+++ app/migrations/2023.06.11_sagres-redirection-to-the-error-page/sql.sql
@@ -1,5 +1,5 @@
 ALTER TABLE inconsistency_sagres ADD idClass INTEGER NULL;
-ALTER TABLE inconsistency_sagres ADD idSchool varchar(10) NULL;
+ALTER TABLE inconsistency_sagres ADD idSchool VARCHAR(10) NULL;
 ALTER TABLE inconsistency_sagres ADD identifier TINYINT NULL;
 ALTER TABLE inconsistency_sagres ADD idStudent INT NULL;
 ALTER TABLE inconsistency_sagres ADD idProfessional INT NULL;
