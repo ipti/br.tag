5afb56b7803a20a3623e8d9acad933e47380aa2a
TI GUSTAVO <ti.gustavo@ipti.org.br>
2023-11-08 12:09:57 -0300

    fix(phpmd): Alterando a regra de tamanho de variável do phpmd e removendo a regra de importação de classes

--- app/models/StudentEnrollment.php
+++ app/models/StudentEnrollment.php
@@ -302,12 +302,12 @@ class StudentEnrollment extends AltActiveRecord
     public function alreadyExists()
     {
         $sql = "SELECT count(student_fk) as qtd FROM student_enrollment WHERE student_fk = :student_fk  AND classroom_fk = :classroom_fk";
-        
+
         $count = Yii::app()->db->createCommand($sql)
             ->bindParam(":student_fk", $this->student_fk)
             ->bindParam(":classroom_fk", $this->classroom_fk)
             ->queryRow();
-        return $count["qtd"] > 0; 
+        return $count["qtd"] > 0;
     }
     public function getDailyOrder()
     {
@@ -316,7 +316,7 @@ class StudentEnrollment extends AltActiveRecord
             ->bindParam(":classroom_fk", $this->classroom_fk)
             ->queryRow();
 
-        return $count["qtd"] + 1; 
+        return $count["qtd"] + 1;
     }
 
     /**
@@ -366,7 +366,7 @@ class StudentEnrollment extends AltActiveRecord
         foreach ($this->classFaults as $fault) {
             $date = new DateTime($fault->scheduleFk->day . "-" . $fault->scheduleFk->month . "-" . yii::app()->user->year);
             if ($date > $initial && $date <= $final) {
-                array_push($faults, $fault);    
+                array_push($faults, $fault);
             }
         }
         return $faults;
