dbf9e868b74f9243996b6e40594303a15b28c7cc
Nathan Santos <ti.natan@ipti.org.br>
2023-11-18 19:01:12 -0300

    Feat: parseToSEDEnrollment

--- app/modules/sedsp/mappers/EnrollmentMapper.php
+++ app/modules/sedsp/mappers/EnrollmentMapper.php
@@ -4,32 +4,24 @@ class EnrollmentMapper
 {
     public function parseToSEDEnrollment($studentIdentificationTag, $studentEnrollmentTag)
     {
-
         $parseResult = [];
 
         //Matrículas
         $inEnrollment = new InMatricularAluno;
         $inEnrollment->setInAnoLetivo(Yii::app()->user->year);
         $inEnrollment->setInAluno(
-            new InAluno(
-                $studentIdentificationTag->gov_id, 
-                null, 'SP'
-            )
+            new InAluno($studentIdentificationTag->gov_id, null, 'SP')
         );
 
         $numClass = Classroom::model()->findByPk($studentEnrollmentTag->classroom_fk);
         $inNumClass = $numClass->gov_id !== null ? $numClass->gov_id : $numClass->inep_id;
 
         $inEnrollment->setInMatricula(
-            new InMatricula(
-                $studentEnrollmentTag->create_date, 
-                "00", 
-                $inNumClass
-            )
+            new InMatricula($studentEnrollmentTag->create_date, "00", $inNumClass)
         );
 
         $modality = EdcensoStageVsModality::model()->findByPk($studentEnrollmentTag->edcenso_stage_vs_modality_fk);
-        $inEnrollment->setInNivelEnsino(new InNivelEnsino($modality, '4'));
+        $inEnrollment->setInNivelEnsino(new InNivelEnsino('6', '1'));
 
 
         $parseResult["Enrollment"] = $inEnrollment;
