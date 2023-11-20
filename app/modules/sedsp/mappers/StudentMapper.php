d9f3d18412c7dbc83cabe71b29e3408053f5f373
Nathan Santos <ti.natan@ipti.org.br>
2023-11-20 02:20:13 -0300

    Feat: added mapSituationEnrollmentToSed

--- app/modules/sedsp/mappers/StudentMapper.php
+++ app/modules/sedsp/mappers/StudentMapper.php
@@ -386,7 +386,7 @@ class StudentMapper
             $studentEnrollment->school_inep_id_fk = $codSchool;
             $studentEnrollment->student_fk = $studentFk;
             $studentEnrollment->classroom_fk = $classroomFk;
-            $studentEnrollment->status = self::mapSituationEnrollment($enrollment->getOutCodSitMatricula());
+            $studentEnrollment->status = self::mapSituationEnrollmentToTag($enrollment->getOutCodSitMatricula());
            
             $arrayMapEnrollments[] = $studentEnrollment;
         }
@@ -480,7 +480,7 @@ class StudentMapper
     /**
      * Summary of getListEnrollmentsActives
      * @param OutListaMatriculaRA $list
-     * @return array
+     * @return OutListaMatriculas[]
      */
     public static function getListEnrollmentsActives(OutListaMatriculaRA $list)
     {
@@ -510,7 +510,7 @@ class StudentMapper
         return self::CODIGO_UF . $sedInepId;
     }
 
-    public static function mapSituationEnrollment($codSitMatriculaSed)
+    public static function mapSituationEnrollmentToTag($codSitMatriculaSed)
     {
         //SED => TAG
         $map = [
@@ -538,5 +538,22 @@ class StudentMapper
         ];
 
         return $map[$codSitMatriculaSed]; 
+    }
+
+    public static function mapSituationEnrollmentToSed($codSitMatriculaTag)
+    {
+        //TAG                                       =>    SED
+        $map = [
+            '1' => '0',                             // ATIVO / ENCERRADO
+            '2' => ['1', '16', '19', '20'],         // TRANSFERIDO, TRANSFERIDO (CONVERSÃO DO ABANDONO), TRANSFERIDO - CEEJA / EAD, NÃO COMPARECIMENTO - CEEJA / EAD 
+            '3' => ['9', '11', '12', '13', '14'],   // CESSÃO POR DESISTÊNCIA, CESSÃO POR EXAME, CESSÃO POR NÚMERO REDUZIDO DE ALUNOS, CESSÃO POR FALTA DE DOCENTE, CESSÃO POR DISPENSA 
+            '4' => ['2', '7', '8', '18'],           // ABANDONOU, CESSÃO POR NÃO FREQUÊNCIA, CESSÃO POR TRANSFERÊNCIA/REMANEJAMENTO, NÃO COMPARECIMENTO / FORA DO PRAZO  
+            '5' => ['3', '10', '17'],               // RECLASSIFICADO, REMANEJAMENTO, REMANEJADO (CONVERSÃO DO ABANDONO)
+            '6' => '5',                             // NÃO COMPARECIMENTO        
+            '9' => '15',                            // CESSÃO POR CONCLUSÃO DO CURSO
+            '11' => '4'                             // FALECIDO
+        ];
+
+        return $map[$codSitMatriculaTag][array_rand($map[$codSitMatriculaTag])];
     }
 }
