5c8fb3fec52f92075e958a9e2d3e5cf3cbd3e62f
Nathan Santos <ti.natan@ipti.org.br>
2023-11-24 21:03:43 -0300

    Fix: update status sync

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -593,14 +593,14 @@ class StudentController extends Controller implements AuthenticateSEDTokenInterf
                 }
     
                 if($statusAdd->outErro === null) {
-                    $stdi = StudentIdentification::model()->findByPk($id);
-                    $stdi->gov_id = $statusAdd->outAluno->outNumRA;
-                    $stdi->sedsp_sync = 1;
+                    $studentFromSed = StudentIdentification::model()->findByPk($id);
+                    $studentFromSed->gov_id = $statusAdd->outAluno->outNumRA;
+                    $studentFromSed->sedsp_sync = 1;
                                   
-                    $stdi->save();
+                    $studentFromSed->save();
 
                     if($modelEnrollment->id !== null) {
-                        $enrollmentResult = $this->processEnrollment($modelStudentIdentification, $modelEnrollment);
+                        $enrollmentResult = $this->processEnrollment($studentFromSed, $modelEnrollment);
                     }
                 }
                 $result["identification"] = $statusAdd;
