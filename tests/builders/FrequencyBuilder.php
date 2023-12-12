8b1c2c53b44035c80dcb0900008c46799a106326
Evellyn Jade <97754536+evellynreis@users.noreply.github.com>
2023-12-12 11:27:12 -0300

    tests/ builder frequency

---
+++ tests/builders/FrequencyBuilder.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @property [] $frequency
+ * @property Faker\Generator $faker
+ * @property CustomProvider $fakerCustom
+ */
+class FrequencyBuilder
+{
+    private $faker = null;
+    private $fakerCustom = null;
+
+    /**
+     * Summary of frequency
+     * @var $frequency
+     */
+    public function __construct()
+    {
+        $this->faker = Faker\Factory::create('pt_BR');
+        $this->fakerCustom = new CustomProvider($this->faker);
+        $this->frequency = [];
+    }
+
+    public function buildCompleted()
+    {
+        $this->frequency['month'] = $this->faker->randomElement(array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+
+        return $this->frequency;
+    }
+}
