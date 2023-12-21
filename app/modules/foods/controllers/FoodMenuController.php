bd00c968e1c92c40baec3af5c24373b9985b2b74
Igor Gonçalves <igor_goncalves@live.com>
2023-12-21 17:05:49 -0300

    fix: remove rules

--- app/modules/foods/controllers/FoodMenuController.php
+++ app/modules/foods/controllers/FoodMenuController.php
@@ -2,53 +2,8 @@
 
 class FoodMenuController extends Controller
 {
-    /**
-     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-     * using two-column layout. See 'protected/views/layouts/column2.php'.
-     */
-    public $layout = '//layouts/column2';
 
     public $MODEL_FOOD_MENU = 'FoodMenu';
-
-    /**
-     * @return array action filters
-     */
-    public function filters()
-    {
-        return array(
-            'accessControl', // perform access control for CRUD operations
-            'postOnly + delete', // we only allow deletion via POST request
-        );
-    }
-
-    /**
-     * Specifies the access control rules.
-     * This method is used by the 'accessControl' filter.
-     * @return array access control rules
-     */
-    public function accessRules()
-    {
-        return array(
-            array('allow',  // allow all users to perform 'index' and 'view' actions
-                'actions' => array(
-                    'index',
-                    'view',
-                    'create',
-                    'update',
-                    'plateAccordion',
-                    'getFood',
-                    'getTacoFoods',
-                    'getPublicTarget',
-                    'getMealType',
-                    'getFoodMeasurement',
-                    'viewLunch'),
-				'users'=>array('@'),
-			),
-			array('deny',  // deny all users
-				'users'=> array('*'),
-			),
-		);
-	}
 
     /**
      * Displays a particular model.
