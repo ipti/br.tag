2301ef8b81b0a08962a8e17c09ea487f29c138f9
GabrielChagas14 <gmelo123455@gmail.com>
2024-01-15 11:23:13 -0300

    criada tabela de crdápio no relatorio de foodmenu

--- themes/default/views/reports/FoodMenu.php
+++ themes/default/views/reports/FoodMenu.php
@@ -1,15 +1,23 @@
-<?php 
-
+<?php
+    $baseUrl = Yii::app()->baseUrl;
+    $cs = Yii::app()->getClientScript();
+    $cs->registerCssFile($baseUrl . '/sass/css/main.css');
 ?>
 <style>
     th, td {
         text-align: center !important;
         vertical-align: middle !important;
+        font-size: 14px;
     }
-    /* Landscape orientation */
-    @page{
-        size: landscape;
+    td {
+        padding: 8px !important;
     }
+    h2 {
+        font-size:16px;
+        color:#252A31;
+        text-align: center;
+    }
+
     /* Hidden the print button */
     @media print {
         #print {
@@ -17,7 +25,49 @@
     }
 }
 </style>
-<h1>aaaaaa</h1>
+<div class="row buttons">
+    <a id="print" class="t-button-secondary" onclick="imprimirPagina()">
+        <span class="t-icon-printer"></span>imprimir
+    </a>
+</div>
+<div class="pageA4H">
+    <h2>DEPARTAMENTO DA ALIMENTAÇÃO ESCOLAR - DAE</h2>
+    <div class="row t-margin-medium--top">
+        <table class="column table table-bordered">
+            <thead>
+                <tr>
+                    <th colspan="6" class="text-align--center" style="background: none !important;">
+                        <b>SECRETARIA MUNICIPAL DE EDUCAÇÃO SANTA LUZIA DO ITANHI</b>
+                        <br/>
+                        <b>PROGRAMA NACIONAL DE ALIMENTAÇÃO ESCOLAR - PNAE</b>
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="6" class="text-align--center" style="background: none !important;">
+                        <b>FUNDAMENTAL I (06-10 anos), FUNDAMENTAL II, QUILOMBOLA</b>
+                        <br>
+                        <b>ZONA RURAL/ PERÍODO PARCIAL</b>
+                        <br>
+                        <b>4ª SEMANA - CARDÁPIO 2024</b>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr  style="background-color: lightgray; font-weight: bold;">
+                    <td>&nbsp;</td>
+                    <td>
+                        2ª FEIRA
+                    </td>
+                    <td>3ª FEIRA</td>
+                    <td>4ª FEIRA</td>
+                    <td>5ª FEIRA</td>
+                    <td>6ª FEIRA</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 <script>
     function imprimirPagina() {
         // printButton = document.getElementsByClassName('span12');
