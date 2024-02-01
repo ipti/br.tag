91bcf12a435c1c15d13899d6e61ecf5a9cdc6791
Gustavo Santos <gustavo.santos23703@gmail.com>
2024-02-01 14:02:24 -0300

    Feat/add report card concept grades (#609)
    * fix(grades): corrigindo a tela de lançamento de notas de buzios
    
    * feat(grades): Corrigindo o cálculo de média final na tela de lançamento de notas de buzios e adicionando notas por conceito em lançamento de notas
    
    * Modificando versão do changelog e fazendo o merge com a dev
    
    * feat(reportCard): Adicionando notas por conceito no lançamento de notas de búzios
    
    ---------
    
    Co-authored-by: TI GUSTAVO <ti.gustavo@ipti.org.br>
    Co-authored-by: Igor Gonçalves <igor_goncalves@live.com>

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $debug = getenv("YII_DEBUG");
 defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 
-define("TAG_VERSION", 'Versão 3.68.104');
+define("TAG_VERSION", '3.68.103');
 
 
 define("YII_VERSION", Yii::getVersion());
