bf0b7b96d3c3465156f89f0f27d9650309697420
AnotherOne07 <93009549+AnotherOne07@users.noreply.github.com>
2024-02-02 12:42:24 -0300

    fix/adjustment in final grade view

--- themes/default/views/forms/IndividualRecord.php
+++ themes/default/views/forms/IndividualRecord.php
@@ -184,7 +184,11 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOneGivenClasses[2]?></td>
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOnefaultsTotal?></td>
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOneGivenClassesTotal?></td>
-                    <td><?= $portuguese[0]['final_media']?></td>
+                    <td><?php echo
+                        $portuguese[0]['grade1'] == null ||
+                        $portuguese[0]['grade2'] == null ||
+                        $portuguese[0]['grade3'] == null ? "-" : $portuguese[0]['final_media']
+                    ?></td>
                     <?php
                     if($portuguese[0]['final_media'] < $gradeRules->approvation_media) {
                         $reprovado = true;
@@ -218,7 +222,11 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOnefaultsTotal?></td>
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOneGivenClassesTotal?></td>
                     <?php } ?>
-                    <td><?= $history[0]['final_media']?></td>
+                    <td><?php echo
+                        $history[0]['grade1'] == null ||
+                        $history[0]['grade2'] == null ||
+                        $history[0]['grade3'] == null ? "-" : $history[0]['final_media']
+                    ?></td>
                     <?php
                     if($history[0]['final_media'] < $gradeRules->approvation_media) {
                         $reprovado = true;
@@ -252,7 +260,11 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOnefaultsTotal?></td>
                     <td rowspan="<?php echo $rOneDisiciplinesCount?>"><?= $rOneGivenClassesTotal?></td>
                     <?php } ?>
-                    <td><?= $geography[0]['final_media']?></td>
+                    <td><?php echo
+                        $geography[0]['grade1'] == null ||
+                        $geography[0]['grade2'] == null ||
+                        $geography[0]['grade3'] == null ? "-" : $geography[0]['final_media']
+                    ?></td>
                     <?php
                     if($geography[0]['final_media'] < $gradeRules->approvation_media) {
                         $reprovado = true;
@@ -282,7 +294,11 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
                     <td rowspan="<?php echo $rTwoDisciplinesCount?>"><?= $rTwoGivenClasses[2]?></td>
                     <td rowspan="<?php echo $rTwoDisciplinesCount?>"><?= $rTwofaultsTotal?></td>
                     <td rowspan="<?php echo $rTwoDisciplinesCount?>"><?= $rTwoGivenClassesTotal?></td>
-                    <td><?= $mathematics[0]['final_media']?></td>
+                    <td><?php echo
+                        $mathematics[0]['grade1'] == null ||
+                        $mathematics[0]['grade2'] == null ||
+                        $mathematics[0]['grade3'] == null ? "-" : $mathematics[0]['final_media']
+                    ?></td>
                     <?php
                     if($mathematics[0]['final_media'] < $gradeRules->approvation_media) {
                         $reprovado = true;
@@ -316,7 +332,11 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
                     <td rowspan="<?php echo $rTwoDisciplinesCount?>"><?= $rTwofaultsTotal?></td>
                     <td rowspan="<?php echo $rTwoDisciplinesCount?>"><?= $rTwoGivenClassesTotal?></td>
                     <?php } ?>
-                    <td><?= $sciences[0]['final_media']?></td>
+                    <td><?php echo
+                        $sciences[0]['grade1'] == null ||
+                        $sciences[0]['grade2'] == null ||
+                        $sciences[0]['grade3'] == null ? "-" : $sciences[0]['final_media']
+                    ?></td>
                     <?php
                     if($sciences[0]['final_media'] < $gradeRules->approvation_media) {
                         $reprovado = true;
@@ -356,9 +376,14 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
                         <?php $totalGivenClasses += $d['givenClasses1'] + $d['givenClasses2'] + $d['givenClasses3'] ?>
                         <td>
                             <?php
-                                if (isset($d['final_media']) && !empty($disciplines[0]['grade3'])){
+                                if (
+                                    isset($d['final_media'])
+                                    && (
+                                        ($d['grade3'] !== NULL) &&
+                                        ($d['grade2'] !== NULL) &&
+                                        ($d['grade1'] !== NULL))){
                                     echo $d['final_media'];
-                                }
+                                }else{echo "-";}
                             ?>
                         </td>
                         <?php
@@ -383,12 +408,16 @@ $rTwoDisciplinesCount = $mathematicsCount + $sciencesCount;
         <div class="footer-container" style="display: flex; justify-content:space-between; margin-bottom: 10px">
             <p>Total de aulas dadas: <?= $totalGivenClasses ?></p>
             <p>Frequência: <?= $frequency."%"?></p>
+            <p>Resultado:
             <?php
-                if ($reprovado && !empty($disciplines[0]['grade3'])){
-                    echo "<p>Resultado: Reprovado</p>";
-                }elseif (!empty($disciplines[0]['grade3'])){
-                    echo "<p>Resultado: Aprovado</p>";
+                if((
+                    ($d['grade3'] !== NULL) &&
+                    ($d['grade2'] !== NULL) &&
+                    ($d['grade1'] !== NULL)))
+                {
+                    echo $reprovado ? "Reprovado</p>" : "Aprovado</p>";
                 }
+
             ?>
         </div>
         <p class="footer-container">
