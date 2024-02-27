77f89a7cd700874a28831e0377da51f1c1752f40
Nathan Santos <ti.natan@ipti.org.br>
2024-02-27 13:44:57 -0300

    Fix: added new @XmlNamespace year

--- app/modules/sagres/soap/src/sagresEdu/EducacaoTType.php
+++ app/modules/sagres/soap/src/sagresEdu/EducacaoTType.php
@@ -16,7 +16,7 @@ use JMS\Serializer\Annotation\XmlElement;
  * 
  * XSD Type: educacao_t
  * @XmlRoot("edu:educacao")
- * @XmlNamespace(uri="http://www.tce.se.gov.br/sagres2023/xml/sagresEdu", prefix="edu")
+ * @XmlNamespace(uri="http://www.tce.se.gov.br/sagres2024/xml/sagresEdu", prefix="edu")
  */
 class EducacaoTType
 {
