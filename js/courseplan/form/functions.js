89865cb9388c755c02ecd89e23c7e81439ac3741
AnotherOne07 <93009549+AnotherOne07@users.noreply.github.com>
2024-03-01 08:55:13 -0300

    :label:fix/removing var declared variables

--- js/courseplan/form/functions.js
+++ js/courseplan/form/functions.js
@@ -58,10 +58,10 @@ function initDatatable() {
 }
 
 function addCoursePlanRow() {
-    var lastTr = $('#course-classes tbody tr.dt-hasChild').last();
-    var index = 0;
+    let lastTr = $('#course-classes tbody tr.dt-hasChild').last();
+    let index = 0;
     if (lastTr.length > 0) {
-        var row = table.row(lastTr);
+        let row = table.row(lastTr);
         index = row.data().class;
     }
 
@@ -81,47 +81,47 @@ function addCoursePlanRow() {
 }
 
 function removeCoursePlanRow(element) {
-    var tr = $(element).closest( "tr" ).prev()
+    let tr = $(element).closest( "tr" ).prev()
     table.row(tr).remove().draw();
 }
 
 function format(d) {
-    var $div = $('<div id="course-class[' + d.class + ']" class="course-class course-class-' + d.class + ' row"></div>');
-    var $column1 = $('<div   class="column no-grow"></div>');
-    var $id = $('<input type="hidden" name="course-class[' + d.class + '][id]" value="' + d.courseClassId + '">');
-    var $objective = $('<div class="t-field-tarea objective-input"></div>');
-    var $objectiveLabel = $('<div><label class="t-field-tarea__label" for="course-class[' + d.class + '][objective]">Objetivo *</label></span>');
-    var $objectiveInput = $('<textarea class="t-field-tarea__input course-class-objective" placeholder="Digite o Objetivo do Plano" id="objective-' + d.class + '" name="course-class[' + d.class + '][objective]">' + d.objective + '</textarea>');
+    let $div = $('<div id="course-class[' + d.class + ']" class="course-class course-class-' + d.class + ' row"></div>');
+    let $column1 = $('<div   class="column no-grow"></div>');
+    let $id = $('<input type="hidden" name="course-class[' + d.class + '][id]" value="' + d.courseClassId + '">');
+    let $objective = $('<div class="t-field-tarea objective-input"></div>');
+    let $objectiveLabel = $('<div><label class="t-field-tarea__label" for="course-class[' + d.class + '][objective]">Objetivo *</label></span>');
+    let $objectiveInput = $('<textarea class="t-field-tarea__input course-class-objective" placeholder="Digite o Objetivo do Plano" id="objective-' + d.class + '" name="course-class[' + d.class + '][objective]">' + d.objective + '</textarea>');
 
-    var $ability = $('<div class="control-group courseplan-ability-container"></div>');
-    var $abilityLabel = $('<label class="" for="course-class[' + d.class + '][ability][]">Habilidade(s)</label>');
-    var $abilityButton = $('<button class="t-button-primary add-abilities" style="height: 28px;gap: 5px" ><icon class="t-icon-start"></icon>Adicionar habilidades</button>');
-    var $abilitiesContainer = $('<div class="courseplan-abilities-selected">');
+    let $ability = $('<div class="control-group courseplan-ability-container"></div>');
+    let $abilityLabel = $('<label class="" for="course-class[' + d.class + '][ability][]">Habilidade(s)</label>');
+    let $abilityButton = $('<button class="t-button-primary add-abilities" style="height: 28px;gap: 5px" ><icon class="t-icon-start"></icon>Adicionar habilidades</button>');
+    let $abilitiesContainer = $('<div class="courseplan-abilities-selected">');
 
-    var $type = $('<div class="t-field-text control-group courseplan-type-container"></div>');
-    var $typeLabel = $('<label class="t-field-text__label" for="course-class[' + d.class + '][type][]">Tipo</label>');
-    var $typeInput = $('<input type="text" class="t-field-text__input" name="course-class[' + d.class + '][type]"></input>');
+    let $type = $('<div class="t-field-text control-group courseplan-type-container"></div>');
+    let $typeLabel = $('<label class="t-field-text__label" for="course-class[' + d.class + '][type][]">Tipo</label>');
+    let $typeInput = $('<input type="text" class="t-field-text__input" name="course-class[' + d.class + '][type]"></input>');
     $typeInput.val(d.type);
 
-    var $resourceButtonContainer = $('<div class="t-buttons-container control-group no-margin"></div>');
-    var $resourceButton = $('<button class="t-button-primary add-new-resource" style="height: 28px;gap: 5px" ><icon class="t-icon-start"></icon>Adicionar recursos</button>');
-    var $resource = $('<div class="t-field-select control-group"></div>');
-    var $resourceLabel = $('<label class="t-field-select__label" for="resource">Recurso(s)</label>');
-    var $resourceInput = $('<div class="t-field-select__input resource-input"></div>');
-    var $resourceValue = $('<select id="resource-select" class="resource-select" name="resource"><option value=""></option>' + $(".js-all-resources")[0].innerHTML + '</select>');
-    var $resourceAmount = $('<input class="resource-amount" style="width:35px; padding: 0px 5px;;margin-left: 5px;" type="number" name="amount" step="1" min="1" value="1" max="999">');
-    var $resourceAdd = $('<button class="add-resource" style="height: 30px;width: 30px;margin-left:10px;" ><icon class="t-icon-plus"></icon></button>');
-    var $deleteButton = "";
+    let $resourceButtonContainer = $('<div class="t-buttons-container control-group no-margin"></div>');
+    let $resourceButton = $('<button class="t-button-primary add-new-resource" style="height: 28px;gap: 5px" ><icon class="t-icon-start"></icon>Adicionar recursos</button>');
+    let $resource = $('<div class="t-field-select control-group"></div>');
+    let $resourceLabel = $('<label class="t-field-select__label" for="resource">Recurso(s)</label>');
+    let $resourceInput = $('<div class="t-field-select__input resource-input"></div>');
+    let $resourceValue = $('<select id="resource-select" class="resource-select" name="resource"><option value=""></option>' + $(".js-all-resources")[0].innerHTML + '</select>');
+    let $resourceAmount = $('<input class="resource-amount" style="width:35px; padding: 0px 5px;;margin-left: 5px;" type="number" name="amount" step="1" min="1" value="1" max="999">');
+    let $resourceAdd = $('<button class="add-resource" style="height: 30px;width: 30px;margin-left:10px;" ><icon class="t-icon-plus"></icon></button>');
+    let $deleteButton = "";
     if(d.deleteButton === 'js-unavailable'){
         $deleteButton = $('<div class="t-buttons-container"><a class="t-button-danger js-remove-course-class js-unavailable t-button-danger--disabled" data-toggle="tooltip" data-placement="left" title="Aula já ministrada em alguma turma. Não é possível removê-la do plano de aula.">Excluir Plano</a></div>')
     } else {
         $deleteButton = $('<div class="t-buttons-container"><a class="t-button-danger js-remove-course-class">Excluir Plano</a></div>')
     }
 
-    var $resources = $('<div class="row wrap no-gap resources"></div>');
+    let $resources = $('<div class="row wrap no-gap resources"></div>');
     if (d.abilities !== null) {
         $.each(d.abilities, function (i, v) {
-            var div = '<div class="ability-panel-option"><input type="hidden" class="ability-panel-option-id" value="' + v.id + '" name="course-class[' + d.class + '][ability][' + i + ']"><i class="fa fa-check-square"></i><span>(<b>' + v.code + '</b>) ' + v.description + '</span></div>';
+            let div = '<div class="ability-panel-option"><input type="hidden" class="ability-panel-option-id" value="' + v.id + '" name="course-class[' + d.class + '][ability][' + i + ']"><i class="fa fa-check-square"></i><span>(<b>' + v.code + '</b>) ' + v.description + '</span></div>';
             $abilitiesContainer.append(div);
         });
     }
@@ -130,15 +130,15 @@ function format(d) {
     }
     if (d.resources !== null) {
         $.each(d.resources, function (i, v) {
-            var resourceId = v.id;
-            var resourceValue = v.value;
-            var resourceName = $resourceValue.find("option[value=" + v.value + "]").text();
-            var resourceAmount = v.amount;
-            var div = $('<div class="row t-badge-content course-class-resource"></div>');
-            var values = $('<input class="resource-id" type="hidden" name="course-class[' + d.class + '][resource][' + i + '][id]" value="' + resourceId + '"/>'
+            let resourceId = v.id;
+            let resourceValue = v.value;
+            let resourceName = $resourceValue.find("option[value=" + v.value + "]").text();
+            let resourceAmount = v.amount;
+            let div = $('<div class="row t-badge-content course-class-resource"></div>');
+            let values = $('<input class="resource-id" type="hidden" name="course-class[' + d.class + '][resource][' + i + '][id]" value="' + resourceId + '"/>'
                 + '<input class="resource-value" type="hidden" name="course-class[' + d.class + '][resource][' + i + '][value]" value="' + resourceValue + '"/>'
                 + '<input class="resource-amount" type="hidden" name="course-class[' + d.class + '][resource][' + i + '][amount]" value="' + resourceAmount + '"/>');
-            var label = $('<span class="row"><span class="fa fa-times remove-resource"><i></i></span><span><span class="resource-amount-text">' + resourceAmount + '</span>x - ' + resourceName + ' </span></span>');
+            let label = $('<span class="row"><span class="fa fa-times remove-resource"><i></i></span><span><span class="resource-amount-text">' + resourceAmount + '</span>x - ' + resourceName + ' </span></span>');
             div.append(values);
             div.append(label);
             $resources.append(div);
@@ -233,26 +233,26 @@ function updateSelectResources(){
 }
 
 function addResource(button) {
-    var div = $(button).parent();
-    var resources = div.parent().children(".resources");
-    var resourceAmount = div.children("input[name=amount]").val();
+    let div = $(button).parent();
+    let resources = div.parent().children(".resources");
+    let resourceAmount = div.children("input[name=amount]").val();
     if (resources.find(".resource-value[value=" + div.children("select").val() + "]").length) {
-        var resource = resources.find(".resource-value[value=" + div.children("select").val() + "]").parent();
+        let resource = resources.find(".resource-value[value=" + div.children("select").val() + "]").parent();
         resource.find(".resource-amount-text").text(resourceAmount);
         resource.find(".resource-amount").val(resourceAmount);
     } else {
-        var resourceValue = div.children("select").val();
-        var resourceName = div.children("select").select2('data').text;
+        let resourceValue = div.children("select").val();
+        let resourceName = div.children("select").select2('data').text;
         if (resourceAmount > 0 && resourceAmount < 1000 && resourceValue !== "") {
-            var tr = div.closest('tr').prev();
-            var row = table.row(tr);
-            var d = row.data();
-            var count = $(resources).children('.course-class-resource').length;
-            var div = $('<div class="t-badge-content course-class-resource"></div>');
-            var values = $('<input class="resource-id" type="hidden" name="course-class[' + d.class + '][resource][' + count + '][id]" value=""/>'
+            let tr = div.closest('tr').prev();
+            let row = table.row(tr);
+            let d = row.data();
+            let count = $(resources).children('.course-class-resource').length;
+            let div = $('<div class="t-badge-content course-class-resource"></div>');
+            let values = $('<input class="resource-id" type="hidden" name="course-class[' + d.class + '][resource][' + count + '][id]" value=""/>'
                 + '<input class="resource-value" type="hidden" name="course-class[' + d.class + '][resource][' + count + '][value]" value="' + resourceValue + '"/>'
                 + '<input class="resource-amount" type="hidden" name="course-class[' + d.class + '][resource][' + count + '][amount]" value="' + resourceAmount + '"/>');
-            var label = $('<span class="row"><span class="fa fa-times remove-resource"><i></i></span><span><span class="resource-amount-text">' + resourceAmount + '</span>x - ' + resourceName + ' </span></span>');
+            let label = $('<span class="row"><span class="fa fa-times remove-resource"><i></i></span><span><span class="resource-amount-text">' + resourceAmount + '</span>x - ' + resourceName + ' </span></span>');
             div.append(values);
             div.append(label);
             resources.append(div);
@@ -261,14 +261,14 @@ function addResource(button) {
 }
 
 function removeResource(button) {
-    var resource = $(button).closest(".course-class-resource");
-    var resources = resource.parent();
-    var classe = resources.closest("tr").prev().children(".details-control").next().text();
+    let resource = $(button).closest(".course-class-resource");
+    let resources = resource.parent();
+    let classe = resources.closest("tr").prev().children(".details-control").next().text();
     resource.remove();
 }
 
 function buildAbilityStructureSelect(data) {
-    var div = '<div class="control-group ability-structure-container"><label>' + data.selectTitle + '</label><select class="ability-structure-select"><option value="">Selecione...</option>';
+    let div = '<div class="control-group ability-structure-container"><label>' + data.selectTitle + '</label><select class="ability-structure-select"><option value="">Selecione...</option>';
     $.each(data.options, function () {
         div += '<option value="' + this.id + '">' + this.description + '</option>';
     });
@@ -277,9 +277,9 @@ function buildAbilityStructureSelect(data) {
 }
 
 function buildAbilityStructurePanel(data) {
-    var panel = '<div><label>' + data.selectTitle + '</label>';
+    let panel = '<div><label>' + data.selectTitle + '</label>';
     $.each(data.options, function () {
-        var selected = $(".js-abilities-selected").find(".ability-panel-option-id[value=" + this.id + "]").length ? "selected" : "";
+        let selected = $(".js-abilities-selected").find(".ability-panel-option-id[value=" + this.id + "]").length ? "selected" : "";
         panel += '<div class="ability-panel-option ' + selected + '"><input type="hidden" class="ability-panel-option-id" value="' + this.id + '"><i class="fa fa-plus-square"></i><span>(<b>' + this.code + '</b>) ' + this.description + '</span></div>';
     });
     panel += "</div>";
