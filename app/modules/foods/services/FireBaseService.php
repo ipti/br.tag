61ba035e7ac9ddad50faa2bf1a17e439078bb22b
gabriel-IPTI <ti.gabriel@ipti.org.br>
2024-03-11 16:50:25 -0300

    conectado com o firebase

--- app/modules/foods/services/FireBaseService.php
+++ app/modules/foods/services/FireBaseService.php
@@ -1,47 +1,45 @@
 <?php
 
-class FireBaseService {
-    public $baseUrl = "https://br-nham-agriculto.firebaseio.com";
-    public $token = "AIzaSyAf7EefR1VXllpmE60kiQwl6xictSDO-Tc ";
+// require 'vendor/autoload.php';
+
+use MrShan0\PHPFirestore\FirestoreClient;
+use MrShan0\PHPFirestore\FirestoreDocument;
+// Optional, depending on your usage
+use MrShan0\PHPFirestore\Fields\FirestoreTimestamp;
+use MrShan0\PHPFirestore\Fields\FirestoreArray;
+use MrShan0\PHPFirestore\Fields\FirestoreBytes;
+use MrShan0\PHPFirestore\Fields\FirestoreGeoPoint;
+use MrShan0\PHPFirestore\Fields\FirestoreObject;
+use MrShan0\PHPFirestore\Fields\FirestoreReference;
+use MrShan0\PHPFirestore\Attributes\FirestoreDeleteAttribute;
 
 
-
-public function createNotice()
+class FireBaseService
 {
-        // Dados a serem enviados para o Firebase (por exemplo, para adicionar um novo item)
-        $data = [
-            'name' => 'teste nome',
-            'date' => '10/03/2024',
-            // adicione mais campos conforme necessário
-        ];
 
-        // Converte os dados para formato JSON
-        $jsonData = json_encode($data);
+    public $firestoreClient;
 
-        // Inicia a sessão cURL
-        $ch = curl_init();
+    public function __construct()
+    {
+      /*   $this->firestoreClient = new FirestoreClient('31058192374', 'AIzaSyAf7EefR1VXllpmE60kiQwl6xictSDO-Tc ', [
+            'database' => '(default)',
+        ]); */
+    }
 
-        // Configura as opções da solicitação cURL
-        curl_setopt($ch, CURLOPT_URL, $this->baseUrl."/Edital"); // Substitua 'seu-caminho' pelo caminho específico no seu banco de dados
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); // Use 'PUT' para atualizações ou 'GET' para consultas
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $token));
+    public function createNotice()
+    {
+        $this->firestoreClient = new FirestoreClient('br-nham-agrigultor', 'AIzaSyAf7EefR1VXllpmE60kiQwl6xictSDO-Tc ', [
+            'database' => '(default)',
+        ]);
+        $collection = 'Edital';
 
-        // Executa a solicitação cURL e obtém a resposta
-        $response = curl_exec($ch);
+        $document = new FirestoreDocument;
+        $document->fillValues([
+            'NomedoEdital' => 'Edital teste',
+            'Alimento' => 'Batata',
+        ]);
 
-        // Verifica erros
-        if (curl_errno($ch)) {
-            echo 'Erro ao se conectar ao Firebase: ' . curl_error($ch);
-        } else {
-            // Manipule a resposta conforme necessário (pode ser um JSON)
-            echo 'Resposta do Firebase: ' . $response;
-        }
-
-        var_dump($response);
-        // Fecha a sessão cURL
-        curl_close($ch);
+        $this->firestoreClient->addDocument($collection, $document,);
     }
 
 }
