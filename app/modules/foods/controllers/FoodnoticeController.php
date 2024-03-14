1760c50fe0926b7228e370c0ef10caaba49b0870
gabriel-IPTI <ti.gabriel@ipti.org.br>
2024-03-14 11:17:26 -0300

    github issues

--- app/modules/foods/controllers/FoodnoticeController.php
+++ app/modules/foods/controllers/FoodnoticeController.php
@@ -228,8 +228,6 @@ class FoodNoticeController extends Controller
         // $this->loadModel($id)->delete();
 
         // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-        if (!isset($_GET['ajax']))
-            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
     }
 
     /**
