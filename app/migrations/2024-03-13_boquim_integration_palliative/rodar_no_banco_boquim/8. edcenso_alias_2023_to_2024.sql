eb4e83188601c4a886969d370272bceb32dc4fa0
Paulo <paulones89@gmail.com>
2024-03-14 23:33:25 -0300

    ORGANIZADO SQLS

--- app/migrations/2024-03-13_boquim_integration_palliative/rodar_no_banco_boquim/8. edcenso_alias_2023_to_2024.sql
+++ app/migrations/2024-03-13_boquim_integration_palliative/rodar_no_banco_boquim/8. edcenso_alias_2023_to_2024.sql
@@ -1,7 +1,4 @@
--- RODAR ISSO NO BANCO AUXILIAR E NO BANCO BOQUIM!!
-
-
--- CRIAR O EDCENSO_ALIAS 2024 PRA CONSEGUIR GERAR O ARQUIVO DO ANO VIGENTE E IMPORTAR NO BANCO BOQUIM
+-- CRIAR O EDCENSO_ALIAS 2024 PRA IMPORTAR OS BANCOS AUXILIARES
 
 insert into edcenso_alias (register, corder, attr, cdesc, `default`, stable, `year`)
 select register, corder, attr, cdesc, `default`, stable, '2024' from edcenso_alias where `year` = '2023';
