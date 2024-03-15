77cf50dfa78e9746c650173567f3aa4bd089c33e
Paulo <paulones89@gmail.com>
2024-03-15 18:53:38 -0300

    Adaptado educacenso para importar dados quando não há professor; Melhorado tela de turma para não remover o teaching data quando não tem teaching_matrix; organizado melhor os arquivos SQL

--- app/migrations/2024-03-13_boquim_integration_palliative/rodar_nos_bancos_legados/all_in_one_file.sql
+++ app/migrations/2024-03-13_boquim_integration_palliative/rodar_nos_bancos_legados/all_in_one_file.sql
@@ -2646,5 +2646,14 @@ set equipments_material_professional_education = 0;
 
 -- CRIAR O EDCENSO_ALIAS 2024 SÓ PRA CONSEGUIR GERAR O ARQUIVO DO ANO VIGENTE
 
+
 insert into edcenso_alias (register, corder, attr, cdesc, `default`, stable, `year`)
 select register, corder, attr, cdesc, `default`, stable, '2024' from edcenso_alias where `year` = '2023';
+
+
+
+
+-- RESOLVER QUESTÃO DO BCRYPT NO LOGIN
+
+
+ALTER TABLE users MODIFY COLUMN password varchar(255);
