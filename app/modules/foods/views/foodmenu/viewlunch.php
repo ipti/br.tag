52fdae7b04438495667606ba326e4acab9826316
TI GUSTAVO <ti.gustavo@ipti.org.br>
2024-03-18 11:21:57 -0300

    fix: creating notice button and fixing delete notice redirection

--- app/modules/foods/views/foodmenu/viewlunch.php
+++ app/modules/foods/views/foodmenu/viewlunch.php
@@ -53,6 +53,9 @@ $cs->registerScriptFile($baseScriptUrl . '/mealsOfWeek/_initialization.js', CCli
             <a class="t-button-secondary" style="margin-right:10px;" href="<?php echo yii::app()->createUrl('foods/farmerregister') ?>">
                 Agricultor
             </a>
+            <a class="t-button-secondary" style="margin-right:10px;" href="<?php echo yii::app()->createUrl('foods/foodnotice') ?>">
+                Editais
+            </a>
             <a class="t-button-secondary js-expansive-panel show--mobile">
                 Filtros
             </a>
