271c0f1bc5a6cef7fb2a50a5b61ebc612cef4d21
TI GUSTAVO <ti.gustavo@ipti.org.br>
2024-03-21 11:22:58 -0300

    Fixing tag version number

--- config.php
+++ config.php
@@ -4,11 +4,7 @@
 $debug = getenv("YII_DEBUG");
 defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 
-<<<<<<< HEAD
-define("TAG_VERSION", 'Versão 3.73.119');
-=======
 define("TAG_VERSION", '3.74.121');
->>>>>>> dev
 
 
 define("YII_VERSION", Yii::getVersion());
