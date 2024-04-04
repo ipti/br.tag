92513f6e875be428b13a51ce0175bd5a2b0166a5
AnotherOne07 <93009549+AnotherOne07@users.noreply.github.com>
2024-04-04 12:34:12 -0300

    models adjustment and module configs

---
+++ app/modules/courseplan/views/default/create.php
@@ -0,0 +1,24 @@
+<?php
+/* @var $this DefaultController */
+/* @var $model CoursePlan */
+
+$this->breadcrumbs=array(
+	'Courseplan' => array('index'),
+    'Create',
+);
+
+$this->setPageTitle('TAG - ' . Yii::t('default', 'Criar Plano de Aula'));
+$this->menu=array(
+	array('label'=>'Create CoursePlan', 'url'=>array('index')),
+	array('label'=>'List ClassPlan', 'url'=>array('admin')),
+    // array('label'=>'List FoodMenu', 'url'=>array('index')),
+	// array('label'=>'Manage FoodMenu', 'url'=>array('admin')),
+);
+?>
+<div class="row main">
+	<div class="column">
+		<h1>Criar Plano de Aula</h1>
+	</div>
+</div>
+
+<?php $this->renderPartial('_form', array('model'=>$modelCoursePlan,'stages'=>$stages)); ?>
--- app/modules/courseplan/views/default/index.php
+++ app/modules/courseplan/views/default/index.php
@@ -4,14 +4,113 @@
 $this->breadcrumbs=array(
 	$this->module->id,
 );
-?>
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
 
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+$this->menu=array(
+	array('label'=>'Create CoursePlan', 'url'=>array('index')),
+	array('label'=>'List ClassPlan', 'url'=>array('admin')),
+);
+
+?>
+
+<?php
+/* @var $this CoursePlanController */
+/* @var $dataProvider CActiveDataProvider */
+
+
+$this->setPageTitle('TAG - ' . Yii::t('default', 'Course Plan'));
+
+$this->menu = array(
+    array('label' => 'List Courseplan', 'url' => array('index')),
+    array('label' => '')
+);
+
+$baseScriptUrl = Yii::app()->controller->module->baseScriptUrl;
+// $themeUrl = Yii::app()->theme->baseUrl;
+$cs = Yii::app()->getClientScript();
+$cs->registerScriptFile($baseScriptUrl . '/_initialization.js');
+
+?>
+
+<div id="mainPage" class="main">
+    <div class="row-fluid">
+        <div class="span12">
+            <h1><?php echo Yii::t('default', 'Course Plan') ?></h1>
+            <div class="t-buttons-container">
+                <a href="<?php echo Yii::app()->createUrl('courseplan/default/create') ?>"
+                    class="t-button-primary"><?= Yii::t('default', 'Create Plan'); ?> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="tag-inner">
+        <div class="columnone" style="padding-right: 1em">
+            <div class="widget clearmargin">
+                <div class="alert courseplan-alert <?= Yii::app()->user->hasFlash('success') ? "alert-success" : "no-show" ?>"><?php echo Yii::app()->user->getFlash('success') ?></div>
+                <div class="widget-body">
+                    <?php $this->widget('zii.widgets.grid.CGridView', array(
+                        'dataProvider' => $dataProvider,
+                        'enablePagination' => false,
+                        'enableSorting' => false,
+                        'itemsCssClass' => 'js-tag-table tag-table-primary table table-condensed table-striped table-hover table-primary table-vertical-center checkboxs',
+                        'columns' => array(
+                            array(
+                                'header' => Yii::t('default', 'Name'),
+                                'name' => 'name',
+                                'type' => 'raw',
+                                'value' => 'CHtml::link($data->name,Yii::app()->createUrl("courseplan/update",array("id"=>$data->id)))',
+                                'htmlOptions' => array('width' => '25%', 'class' => 'link-update-grid-view')
+                            ),
+                            array(
+                                'header' => Yii::t('default', 'Stage'),
+                                'name' => 'modality_fk',
+                                'type' => 'raw',
+                                'value' => '$data->modalityFk->name',
+                                'htmlOptions' => array('width' => '25%'),
+                            ),
+                            array(
+                                'header' => Yii::t('default', 'Discipline'),
+                                'name' => 'discipline_fk',
+                                'value' => '$data->disciplineFk->name',
+                                'htmlOptions' => array('width' => '20%'),
+                                'filter' => false
+                            ),
+                            array(
+                                'header' => Yii::t('default', 'Autor'),
+                                'name' => 'users_fk',
+                                'value' => '$data->usersFk->name',
+                                'htmlOptions' => array('width' => '25%'),
+                                'filter' => false
+                            ),
+                            array(
+                                'header' => 'Ações',
+                                'class' => 'CButtonColumn',
+                                'template' => '{delete}',
+                                'buttons' => array(
+                                    'delete' => array(
+                                        'imageUrl' => Yii::app()->theme->baseUrl.'/img/deletar.svg',
+                                    )
+                                ),
+                                'afterDelete' => 'function(link, success, data){
+                                    data = JSON.parse(data);
+                                    if (data.valid) {
+                                        $(".alert").text(data.message).addClass("alert-success").removeClass("alert-error");
+                                    } else {
+                                        $(".alert").text(data.message).addClass("alert-error").removeClass("alert-success");
+                                    }
+                                    $(".courseplan-alert").show();
+                                }',
+
+                            ),
+                        ),
+                    )); ?>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="columntwo">
+        </div>
+
+    </div>
+</div>
+
