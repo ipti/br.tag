85eeb5374e825817a0f71cde7c8f4837accdea8c
Fabricia Resende <fabricia.resende@academico.ufs.br>
2024-04-16 11:10:28 -0300

    update js courseplan

--- app/modules/courseplan/CourseplanModule.php
+++ app/modules/courseplan/CourseplanModule.php
@@ -4,13 +4,25 @@ class CourseplanModule extends CWebModule
 {
 
 	public $defaultController = "courseplan";
+
+	public $baseScriptUrl;
+	public $baseUrl;
+
+
 	public function init()
 	{
+
+		$this->baseUrl = Yii::app()->createUrl("courseplan");
+		$this->baseScriptUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.professional.resources'));
+
 		$this->setImport(array(
 			'courseplan.models.*',
 			'courseplan.components.*',
 		));
 	}
+
+
+
 
 	public function beforeControllerAction($controller, $action){
 		
