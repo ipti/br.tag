76d6014baf4d4c213e36a1039cfa9b0c8b317abe
Nathan Santos <ti.natan@ipti.org.br>
2024-04-17 15:02:34 -0300

    Feat: added get days faults

--- app/controllers/ClassesController.php
+++ app/controllers/ClassesController.php
@@ -177,10 +177,20 @@ class ClassesController extends Controller
                 }
             }
 
+            $command = Yii::app()->db->createCommand("
+                SELECT s.day
+                FROM instructor_faults if2 
+                JOIN schedule s ON if2.schedule_fk = s.id 
+                WHERE if2.instructor_fk = :instructor_id
+            ");
+            $command->bindValue(':instructor_id', 305);
+            $daysFaults = $command->queryColumn();
+
             echo json_encode([
                 "valid" => true,
                 "classContents" => $classContents,
                 "courseClasses" => $courseClasses,
+                "daysFaults" =>  $daysFaults
             ]);
         } else {
             echo json_encode(["valid" => false, "error" => "Mês/Ano " . ($_POST["fundamentalMaior"] == "1" ? "e Disciplina" : "") . " sem aula no Quadro de Horário."]);
