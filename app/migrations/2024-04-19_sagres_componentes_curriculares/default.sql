31bd0da22911b7646d814cf2f509ac0c8b7d2bbd
Nathan Santos <ti.natan@ipti.org.br>
2024-04-19 16:46:12 -0300

    Feat: added migration

---
+++ app/migrations/2024-04-19_sagres_componentes_curriculares/default.sql
@@ -0,0 +1,3 @@
+ALTER TABLE inconsistency_sagres MODIFY COLUMN school varchar(200) NOT NULL;
+ALTER TABLE inconsistency_sagres MODIFY COLUMN description varchar(250) NOT NULL;
+ALTER TABLE inconsistency_sagres MODIFY COLUMN `action` varchar(200) NOT NULL;
