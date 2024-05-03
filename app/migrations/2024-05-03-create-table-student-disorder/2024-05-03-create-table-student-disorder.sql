57174625535f2a301fd65364fc17abbeb4a8c547
Evellyn Jade <97754536+evellynreis@users.noreply.github.com>
2024-05-03 12:25:06 -0300

    feat/create table student_disorder

---
+++ app/migrations/2024-05-03-create-table-student-disorder/2024-05-03-create-table-student-disorder.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `student_disorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_fk` int(11) NOT NULL,
+  `tdah` tinyint(1) NOT NULL DEFAULT '0',
+  `depressao` tinyint(1) NOT NULL DEFAULT '0',
+  `tab` tinyint(1) NOT NULL DEFAULT '0',
+  `toc` tinyint(1) NOT NULL DEFAULT '0',
+  `tag` tinyint(1) NOT NULL DEFAULT '0',
+  `tod` tinyint(1) NOT NULL DEFAULT '0',
+  `tcne` tinyint(1) NOT NULL DEFAULT '0',
+  `others` varchar(200)  NULL DEFAULT "",
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`student_fk`) REFERENCES `student_identification` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
