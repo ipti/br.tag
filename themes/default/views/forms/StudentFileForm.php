234abf43c7bca21c1ecbb33bfba9766b89588847
AnotherOne07 <93009549+AnotherOne07@users.noreply.github.com>
2024-05-06 10:55:31 -0300

    fix/report file and studentfile view updated

--- themes/default/views/forms/StudentFileForm.php
+++ themes/default/views/forms/StudentFileForm.php
@@ -145,7 +145,12 @@ $turns = ['M' => 'Manhã', 'T' => 'Tarde', 'N' => 'Noite'];
         </tr>
         <tr>
             <td>
-                <div class="span4"><b>03 - Naturalidade: <span class="nationality"><?= $data['nation'] ?></span></b></div>
+                <div class="span4"><b>03 - Naturalidade: <span class="nationality"><?= $data['nationality'] ?></span></b></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="span4"><b>03 - País: <span class="nationality"><?= $data['nation'] ?></span></b></div>
                 <div class="span8"><b>Município: </b>&nbsp;<span class="birth_city"><?= $data['birth_city'] ?></span>/<?php echo $birth_uf ?> </div>
                 <div class="span12">
                     <b>ENDEREÇO: </b><span class="address"><?= $data['address'] ?></span>, <span class="number"><?= $data['number'] ?></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
