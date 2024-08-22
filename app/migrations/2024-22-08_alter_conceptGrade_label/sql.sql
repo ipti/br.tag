3fe4c4cab6c21e075d47e6b6723a6bf01c0193d0
AnotherOne07 <93009549+AnotherOne07@users.noreply.github.com>
2024-08-22 11:08:04 -0300

    first commit

---
+++ app/migrations/2024-22-08_alter_conceptGrade_label/sql.sql
@@ -0,0 +1,7 @@
+DELETE FROM grade_concept;
+
+INSERT INTO grade_concept (name, acronym) VALUES('Não Observado', 'NO');
+INSERT INTO grade_concept (name, acronym) VALUES('Em Processo', 'EM');
+INSERT INTO grade_concept (name, acronym) VALUES('Bom', 'B');
+INSERT INTO grade_concept (name, acronym) VALUES('Ótimo', 'O');
+INSERT INTO grade_concept (name, acronym) VALUES('Excelente', 'E');
