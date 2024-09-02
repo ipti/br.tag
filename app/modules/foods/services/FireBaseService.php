67d723f84b2ab826c66b9e8e09ba18056bd543b9
TI GUSTAVO <ti.gustavo@thp.org.br>
2024-09-02 10:53:25 -0300

    Corrigindo erros do sonarlint

--- app/modules/foods/services/FireBaseService.php
+++ app/modules/foods/services/FireBaseService.php
@@ -238,9 +238,13 @@ function mask($val, $mask) {
     $k = 0;
     for($i = 0; $i<=strlen($mask)-1; $i++) {
         if($mask[$i] == '#') {
-            if(isset($val[$k])) $maskared .= $val[$k++];
+            if(isset($val[$k])) {
+                $maskared .= $val[$k++];
+            }
         } else {
-            if(isset($mask[$i])) $maskared .= $mask[$i];
+            if(isset($mask[$i])) {
+                $maskared .= $mask[$i];
+            }
         }
     }
     return $maskared;
