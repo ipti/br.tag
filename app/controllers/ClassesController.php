733b3bbe292f0bf6cc6127b67f022d0e71cb8359
Gustavo Santos <gustavo.santos23703@gmail.com>
2024-09-13 09:33:33 -0300

    Feat/class contents school manager (#820)
    * Corrigindo a duplicidade ao criar planos de aula
    
    * Criando a tela de validar aulas ministradas, permitindo somente a visualização das aulas
    
    * Modificando o changelog
    
    * Corrigindo o instance
    
    ---------
    
    Co-authored-by: TI GUSTAVO <ti.gustavo@thp.org.br>

--- app/controllers/ClassesController.php
+++ app/controllers/ClassesController.php
@@ -42,6 +42,7 @@ class ClassesController extends Controller
                     'saveFrequency',
                     'SaveFrequencies',
                     'classContents',
+                    'validateClassContents',
                     'getClassContents',
                     'saveClassContents',
                     'getmonthsanddisciplines',
@@ -95,6 +96,40 @@ class ClassesController extends Controller
         }
         $this->render(
             'classContents',
+            array(
+                'classrooms' => $classrooms
+            )
+        );
+    }
+
+    public function actionValidateClassContents()
+    {
+        if (Yii::app()->getAuthManager()->checkAccess('instructor', Yii::app()->user->loginInfos->id)) {
+            $criteria = new CDbCriteria;
+            $criteria->alias = "c";
+            $criteria->join = ""
+                . " join instructor_teaching_data on instructor_teaching_data.classroom_id_fk = c.id "
+                . " join instructor_identification on instructor_teaching_data.instructor_fk = instructor_identification.id ";
+            $criteria->condition = "c.school_year = :school_year and c.school_inep_fk = :school_inep_fk and instructor_identification.users_fk = :users_fk";
+            $criteria->order = "name";
+            $criteria->params = array(
+                ':school_year' => Yii::app()->user->year,
+                ':school_inep_fk' => Yii::app()->user->school,
+                ':users_fk' => Yii::app()->user->loginInfos->id
+            );
+
+            $classrooms = Classroom::model()->findAll($criteria);
+        } else {
+            $classrooms = Classroom::model()->findAll(
+                'school_year = :school_year and school_inep_fk = :school_inep_fk order by name',
+                [
+                    'school_year' => Yii::app()->user->year,
+                    'school_inep_fk' => Yii::app()->user->school
+                ]
+            );
+        }
+        $this->render(
+            'validateClassContents',
             array(
                 'classrooms' => $classrooms
             )
