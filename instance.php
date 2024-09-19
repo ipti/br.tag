e91e32b088453c3044f6a4bfa2fe9d7210c47790
TI GUSTAVO <ti.gustavo@thp.org.br>
2024-09-19 12:29:16 -0300

    corrigindo problema ao criar plano de aula como administrador

--- instance.php
+++ instance.php
@@ -15,7 +15,7 @@ $domain = array_shift($host_array);
 $newdb = $domain . '.tag.ong.br';
 
 if ($domain == "localhost") {
-    $newdb = 'boquim.tag.ong.br';
+    $newdb = 'boquim11/09.tag.ong.br';
 }
 
 $_GLOBALGROUP = 0;
