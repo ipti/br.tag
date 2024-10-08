725503ceced93cee2a1fdaee489056c0c97348ab
Paulo <paulones89@gmail.com>
2024-10-08 16:49:51 -0300

    fix no select da estrutura de unidades, "Forma de Cálculo" da rec. final

--- instance.php
+++ instance.php
@@ -29,12 +29,12 @@ $USER = getenv("USER_DB_TAG");
 $PWD = getenv("PWD_DB_TAG");
 
 define ("DBCONFIG", serialize (array(
-    'connectionString' => "mysql:host=localhost;dbname=boquim.tag.ong.br",
+    'connectionString' => "mysql:host=$HOST;dbname=$newdb",
     'emulatePrepare' => true,
     'enableProfiling' => YII_DEBUG,
     'enableParamLogging' => YII_DEBUG,
-    'username' => "user.tag",
-    'password' => "123456",
+    'username' => $USER,
+    'password' => $PWD,
     'charset' => 'utf8',
 )));
 
