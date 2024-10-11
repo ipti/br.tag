89116b4c2e97ae83807eac6ced3c08ebc69d907c
Gusta-Santos <gustavo.santos23703@gmail.com>
2024-10-11 12:04:03 -0300

    Corrigido o erro ao carregar a tabela de visualização de aulas ministradas quando não havia nenhuma aula cadastrada

--- js/classes/class-contents/validateFunctions.js
+++ js/classes/class-contents/validateFunctions.js
@@ -13,15 +13,6 @@ function createValidateTable(data) {
     $('#class-contents > thead').html('<tr><th class="center">Dias</th><th style="text-align:left">Conteúdo ministrado em sala de aula</th></tr>');
     $('#class-contents > tbody').html('');
 
-    let options = "";
-    let i = 1;
-    let pivotChanger = "";
-    $.each(data.courseClasses, function () {
-        console.log(this);
-        i = pivotChanger !== this.cpid ? 1 : i;
-        pivotChanger = this.cpid;
-        options += '<div class="t-badge-info" value="' + this.id + '" disciplineid="' + this.edid + '" disciplinename="' + this.edname + '">' + this.cpname + "|" + i++ + "|" + this.content + "|" + this.edname + '</div>';
-    });
     let accordionBuilt = false;
     let accordionHtml = "";
     accordionHtml += `<div id='accordion' class='t-accordeon-primary' style='overflow: auto; height: 400px;'>`
@@ -30,17 +21,17 @@ function createValidateTable(data) {
         if (Object.keys(classContent.students).length) {
             $.each(classContent.students, function (index, studentArray) {
                 $.each(studentArray, function (index, student) {
-                    studentInputs += "<input type='hidden' class='student-diary-of-the-day' studentid='" + this.id + "' value='" + this.diary + "'>";
+                    studentInputs += "<input type='hidden' class='student-diary-of-the-day' studentid='" + student.id + "' value='" + student.diary + "'>";
                     if (!accordionBuilt) {
                         accordionHtml +=
                             `<div class='align-items--center'>
                                 <h4 class='t-title'>
                                     <span class='t-icon-person icon-color'></span>
-                                    ${this.name}
+                                    ${student.name}
                                 </h4>
                             </div>
                             <div class='ui-accordion-content js-std-classroom-diaries'>
-                                <textarea ${urlIsValidate ? 'readonly' : ''} class='t-field-tarea__input js-student-classroom-diary' studentid='${this.id}' style='resize: vertical;height: 37px;'></textarea>
+                                <textarea ${urlIsValidate ? 'readonly' : ''} class='t-field-tarea__input js-student-classroom-diary' studentid='${student.id}' style='resize: vertical;height: 37px;'></textarea>
                             </div>`
                     }
                 });
@@ -55,19 +46,30 @@ function createValidateTable(data) {
             $(".classroom-diary-no-students").show();
         }
         let courseClasses = "";
-        $.each(data.courseClasses, function (index, courseClass) {
-            $.each(classContent.contents, function (index, content) {
-                if(courseClass.id == content) {
-                    courseClasses += '<div class="t-badge-info column text-align--left t-padding-medium--x" style="width:80%">'
-                    + "<div>"
-                    + "<b>Aula:</b> " + courseClass.order + " <b>Plano de aula:</b> " + courseClass.cpname + "<br>"
-                    + "<b>Conteúdo:</b> " + courseClass.content
-                    + "</div>"
-                    + '</div>';
-                }
-            });
+        let isContentsEmpty = false;
 
+        $.each(data.courseClasses, function (index, courseClass) {
+            if (classContent.contents && Object.keys(classContent.contents).length) {
+                $.each(classContent.contents, function (index, content) {
+                    if(courseClass.id == content) {
+                        courseClasses += '<div class="t-badge-info column text-align--left t-padding-medium--x" style="width:80%">'
+                        + "<div>"
+                        + "<b>Aula:</b> " + courseClass.order + " <b>Plano de aula:</b> " + courseClass.cpname + "<br>"
+                        + "<b>Conteúdo:</b> " + courseClass.content
+                        + "</div>"
+                        + '</div>';
+                    }
+                });
+            } else {
+                isContentsEmpty =  true;
+            }
         });
+
+        if(isContentsEmpty) {
+            courseClasses += '<div class="t-badge-info column text-align--left t-padding-medium--x">'
+            + 'Nenhuma aula ministrada neste dia'
+            + '</div>';
+        }
 
 
         let head = '<th class="center vmiddle contents-day ">' + ((day < 10) ? '0' : '') + day + '</th>';
