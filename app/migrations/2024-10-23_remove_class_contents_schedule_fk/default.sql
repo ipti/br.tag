6de8f6e6fa4bf3ac032f6a76a31b58e6cdd67469
Gusta-Santos <gustavo.santos23703@gmail.com>
2024-10-25 12:01:00 -0300

    Retirando a chave estrangeira de schedule em class_contents e adicionando campos relacionadoos a data na sua tabela

--- app/migrations/2024-10-23_remove_class_contents_schedule_fk/default.sql
+++ app/migrations/2024-10-23_remove_class_contents_schedule_fk/default.sql
@@ -7,10 +7,10 @@ ALTER TABLE class_contents ADD COLUMN year INT(11);
 ALTER TABLE class_contents ADD COLUMN classroom_fk INT(11);
 ALTER TABLE class_contents ADD COLUMN discipline_fk INT(11);
 
--- ALTER TABLE class_contents MODIFY schedule_fk INT(11) NULL;
-
--- ALTER TABLE class_contents
--- ADD CONSTRAINT class_contents_ibfk_1
--- FOREIGN KEY (schedule_fk) REFERENCES schedule(id)
--- ON DELETE SET NULL
--- ON UPDATE CASCADE;
+UPDATE class_contents cc
+JOIN schedule s ON cc.schedule_fk = s.id
+SET cc.day = s.day,
+    cc.month = s.month,
+    cc.year = s.year,
+    cc.classroom_fk = s.classroom_fk,
+    cc.discipline_fk = s.discipline_fk;
