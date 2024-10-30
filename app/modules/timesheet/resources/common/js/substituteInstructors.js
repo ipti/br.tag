b345b09df9a95d3a147614ba16230c5d2cecaf57
AnotherOne07 <93009549+AnotherOne07@users.noreply.github.com>
2024-10-30 12:44:25 -0300

    js functions added

---
+++ app/modules/timesheet/resources/common/js/substituteInstructors.js
@@ -0,0 +1,63 @@
+$("#substituteInstructor").on("click", function() {
+    if (
+        $('#classrooms').val() !== "" &&
+        $("#instructor").val() !== "" &&
+        $("#month").val() !== ""
+        ){
+            $.ajax({
+                type: "POST",
+                url: "?r=timesheet/timesheet/getFrequency",
+                cache: false,
+                data: {
+                    instructor: $("#instructor").val(),
+                    classroom: $("#classrooms").val(),
+                    month: $("month").val()
+                },
+                beforeSend: function () {
+                    $(".loading-frequency").css("display", "inline-block");
+                    $(".table-frequency").css("opacity", 0.3).css("pointer-events", "none");
+                    $("#classrooms, #month, #instructor").attr("disabled", "disabled");
+                },
+                success: function (response) {
+                    var data = JSON.parse(response);
+                    if (data.valid) {
+                        var html = "";
+                        html += "" +
+                        "<table class='t-accordion table-frequency table table-bordered table-striped table-hover'>" +
+                        "<thead class='t-accordion__head'>" +
+                        "<tr><th class='table-title' colspan='" + (Object.keys(data.instructors[0].schedules).length + 1) + "'>" + ($('#disciplines').select2('data').text) + "aaaaaaa</th></tr>";
+                        var daynameRow = "";
+                        var dayRow = "";
+                        var scheduleRow = "";
+                        var checkboxRow = "";
+                        $.each(data.schedules, function() {
+                            dayRow += "<th>" + (pad(this.day, 2) + "/" + pad($("#month").val(), 2)) + "</th>";
+                            daynameRow += "<th>" + this.week_day + "</th>";
+                            scheduleRow += "<th>" + this.schedule + "º Horário</th>";
+                            checkboxRow += "<th class='frequency-checkbox-general frequency-checkbox-container " + "'><input class='frequency-checkbox' type='checkbox' " + "classroomId='" + $("$classrooms").val() + "' day='" + this.day + "' month='" + $("#month").val() + "' schedule='" + this.schedule + "></th>";
+                        });
+                        html += "<tr class='day-row'><th></th>" + dayRow + "</tr><tr class='dayname-row'><th></th>" + daynameRow + "</tr>" + ("<tr class='schedule-row'><th></th>" + scheduleRow + "</tr>");
+                        html += "</thead><tbody class='t-accordion__body'>";
+                        html += "</tbody></table>";
+                        $("#frequency-container").html(html).show();
+                        $(".frequency-checkbox-general").each(function () {
+                            var day = $(this).find(".frequency-checkbox").attr("day");
+                            $(this).find(".frequency-checkbox").prop("checked", $(".frequency-checkbox-instructor .frequency-checkbox[day=" + day + "]:checked").length === $(".frequency-checkbox-instructor .frequency-checkbox[day=" + day + "]").length);
+
+                        });
+                        $('[data-toggle="tooltip"]').tooltip({ container: "body" });
+                    } else {
+                        $("#frequency-container").hide();
+                        $(".alert-incomplete-data").html(data.error).show();
+                    }
+                }, complete: function (response) {
+                    $(".loading-frequency").hide();
+                    $(".table-frequency").css("opacity", 1).css("pointer-events", "auto");
+                    $("#classroom, #month, #disciplines, #classesSearch").removeAttr("disabled");
+                },
+            })
+        } else {
+            $(".alert-required-fields").show();
+            $("#frequency-container, .alert-incomplete-data").hide();
+        }
+})
--- themes/default/views/timesheet/timesheet/substituteInstructor.php
+++ themes/default/views/timesheet/timesheet/substituteInstructor.php
@@ -7,17 +7,58 @@
 
 <div class="main">
 
+    <?php if (Yii::app()->user->hasFlash('success')) : ?>
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="alert alert-success">
+                    <?php echo Yii::app()->user->getFlash('success') ?>
+                </div>
+            </div>
+        </div>
+    <?php endif ?>
+    <?php if (Yii::app()->user->hasFlash('error')) : ?>
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="alert alert-error">
+                    <?php echo Yii::app()->user->getFlash('error') ?>
+                </div>
+            </div>
+        </div>
+    <?php endif ?>
+
+    <div class="row-fluid">
+        <div class="span12">
+            <h1><?= yii::t('timesheetModule.timesheet', 'Assign Substitute Instructor') ?></h1>
+            <div class="buttons span9">
+            </div>
+        </div>
+    </div>
+
 <div class="tag-inner">
     <div class="row wrap filter-bar margin-bottom-none">
         <div>
-            <?php echo CHtml::label(yii::t('default', 'Instructor') . " *", 'substituteInstructor', array('class' => 'control-label required', 'style' => 'width: 80px;')); ?>
+        <div>
+            <?php echo CHtml::label(yii::t('default', 'Classroom') . " *", 'classroom', array('class' => 'control-label required small-label')); ?>
+                <?= CHtml::dropDownList('classroom_fk', "", CHtml::listData(Classroom::model()->findAll("school_inep_fk = :school_inep_fk and school_year = :school_year order by name", ["school_inep_fk" => Yii::app()->user->school, "school_year" => Yii::app()->user->year]), 'id', 'name'), ["prompt" => yii::t("timesheetModule.timesheet", "Select a Classroom"), "class" => "select-search-on control-input classroom-id"]); ?>
+            </div>
 
+            <?php echo CHtml::label(yii::t('default', 'Instructor') . " *", 'substituteInstructor', array('class' => 'control-label required', 'style' => 'width: 80px;')); ?>
             <select class="select-search-on control-input frequency-input" id="substituteInstructor">
                 <option>Selecione o professor</option>
                 <?php foreach ($instructors as $instructor) : ?>
                     <option value="<?= $instructor["id"] ?>"> <?= $instructor["name"] ?> </option>
                 <?php endforeach; ?>
             </select>
+
+            <div class="t-field-select">
+                <?php echo CHtml::label(yii::t('default', 'Month') . "/Ano",
+                    'month', array('class' => 't-field-select__label--required')); ?>
+                <select
+                    class="select-search-on t-field-select__input js-load-frequency"
+                    id="month"
+                    style="min-width: 185px;">
+                </select>
+            </div>
         </div>
     </div>
 </div>
