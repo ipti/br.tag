be1fd6b3dba10e737490bf2154f9256061e30c2f
João Felipe <j.felipe.dev.18@gmail.com>
2024-12-06 17:54:44 -0300

    update file

--- app/modules/sagres/soap/scripts/convert-notation.php
+++ app/modules/sagres/soap/scripts/convert-notation.php
@@ -1,7 +1,7 @@
 <?php
 
-$inputDir = 'modules/sagres/soap/src2'; // Diretório onde estão as classes PHP geradas
-$outputDir = 'modules/sagres/soap/src2_converted'; // Diretório para salvar as classes convertidas
+$inputDir = 'modules/sagres/soap/src2'; // Diretório de entrada com os arquivos gerados
+$outputDir = 'modules/sagres/soap/src3'; // Diretório de saída com os arquivos convertidos
 
 if (!is_dir($outputDir)) {
     mkdir($outputDir, 0777, true);
@@ -16,33 +16,58 @@ foreach ($files as $file) {
 
     $content = file_get_contents($file->getPathname());
 
-    // Converte @Serializer\SerializedName para #[Serializer\SerializedName]
-    $content = preg_replace(
-        '/\*\s+@Serializer\\\SerializedName\("([^"]+)"\)/',
-        '    #[Serializer\SerializedName("$1")]',
+    // Adiciona a linha "use JMS\Serializer\Annotation as Serializer;"
+    if (!str_contains($content, 'use JMS\\Serializer\\Annotation as Serializer;')) {
+        $content = preg_replace(
+            '/namespace (.*?);/s',
+            "namespace $1;\n\nuse JMS\\Serializer\\Annotation as Serializer;",
+            $content
+        );
+    }
+
+    // Para cada propriedade, adiciona as anotações do JMS
+    $content = preg_replace_callback(
+        '/\/\*\*\s+\*\s+@var\s+([^\s]+)\s+\*\s+\*\//',
+        function ($matches) {
+            $phpType = match ($matches[1]) {
+                'string' => '?string',
+                '\DateTime' => '?\DateTime',
+                'int' => '?int',
+                'bool' => '?bool',
+                default => $matches[1],
+            };
+
+            $serializedName = strtolower(preg_replace('/[A-Z]/', '_$0', lcfirst($matches[1])));
+
+            $annotations = <<<ANNOTATIONS
+    #[Serializer\SerializedName("edu:$serializedName")]
+    #[Serializer\XmlElement(cdata: false)]
+ANNOTATIONS;
+
+            if ($phpType === '?\DateTime') {
+                $annotations .= PHP_EOL . '    #[Serializer\Type("DateTime<\'Y-m-d\'>")]';
+            }
+
+            return $annotations . PHP_EOL . "    private {$phpType} \${$serializedName} = null;";
+        },
         $content
     );
 
-    // Converte @Serializer\XmlElement para #[Serializer\XmlElement]
+    // Ajusta os métodos getters e setters
     $content = preg_replace(
-        '/\*\s+@Serializer\\\XmlElement\(cdata=(true|false)\)/',
-        '    #[Serializer\XmlElement(cdata=$1)]',
+        '/public function get([A-Z][a-zA-Z0-9]+)\(\)/',
+        'public function get$1(): ?$2',
         $content
     );
-
-    // Converte @Serializer\Type para #[Serializer\Type]
     $content = preg_replace(
-        '/\*\s+@Serializer\\\Type\("([^"]+)"\)/',
-        '    #[Serializer\Type("$1")]',
+        '/public function set([A-Z][a-zA-Z0-9]+)\(\$([a-z][a-zA-Z0-9]+)\)/',
+        'public function set$1(?$2 $2): self',
         $content
     );
-
-    // Remove comentários de docblock que ficaram vazios
-    $content = preg_replace('/\/\*\*\s+\*\s+\*\//', '', $content);
 
     // Salva o arquivo convertido
     $outputFile = $outputDir . DIRECTORY_SEPARATOR . $file->getFilename();
     file_put_contents($outputFile, $content);
 }
 
-echo "Conversão concluída. Classes convertidas estão em: $outputDir\n";
+echo "Conversão concluída. Arquivos convertidos estão em $outputDir.\n";
