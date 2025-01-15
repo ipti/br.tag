f7bdb160dc4347bef7ef3957dd8969a3574a7a70
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-01-15 18:12:49 -0300

    feat: completed changes in the module to generate documents

---
+++ app/modules/sagres/soap/validator.py
@@ -0,0 +1,48 @@
+from lxml import etree
+
+
+xsd_path = "/home/felipe/ipti/br.tag/app/modules/sagres/soap/Educacao.xsd"
+xml_path = "/home/felipe/Downloads/Educacao.xml"
+
+
+# Função para validar XML com o XSD principal apenas
+
+def validate_xml_with_main_xsd_only(main_xsd_file, xml_file):
+
+    try:
+
+        # Parse do esquema XSD principal
+
+        with open(main_xsd_file, 'r') as main_xsd:
+
+            schema_root = etree.XML(main_xsd.read())
+
+        schema = etree.XMLSchema(schema_root)
+
+
+
+        # Parse do arquivo XML
+
+        with open(xml_file, 'r') as xml:
+
+            xml_doc = etree.parse(xml)
+
+
+
+        # Validação
+
+        schema.assertValid(xml_doc)
+
+        return "O XML é válido em relação ao XSD principal.", None
+
+    except etree.DocumentInvalid as e:
+
+        return "O XML é inválido em relação ao XSD principal.", str(e)
+
+
+
+# Validar XML com o XSD principal apenas
+
+validation_result_main_only, error_details_main_only = validate_xml_with_main_xsd_only(xsd_path, xml_path)
+
+validation_result_main_only, error_details_main_only
