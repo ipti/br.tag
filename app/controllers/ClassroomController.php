ef6c0db401df155bab59943111825df020374703
Igor Gonçalves <igor_goncalves@live.com>
2025-04-11 15:27:17 -0300

    fix: delete classroom

--- app/controllers/ClassroomController.php
+++ app/controllers/ClassroomController.php
@@ -716,6 +716,7 @@ class ClassroomController extends Controller
             } else {
                 foreach ($enrollments as $enrollment) {
                     $studentEnrollment = StudentEnrollment::model()->findByPk($enrollment);
+
                     $frequencyAndMean = FrequencyAndMeanByDiscipline::model()
                         ->findAllByAttributes(array('enrollment_fk' => $studentEnrollment->id));
                     $gradeResults = GradeResults::model()
@@ -732,6 +733,7 @@ class ClassroomController extends Controller
                     foreach ($frequencyByExam as $frequencyExam) {
                         $frequencyExam->delete();
                     }
+                    StudentEnrollmentHistory::model()->deleteAll("student_enrollment_fk = :enrollment_fk", [":enrollment_fk" => $studentEnrollment->id]);
                     $studentEnrollment->delete();
                     Yii::app()->user->setFlash('success', 'Matrículas de alunos excluídas com sucesso');
                 }
