122fd2ee2bd0d88f52baf8e5a0aeab3b2f0f72d2
TI GUSTAVO <ti.gustavo@thp.org.br>
2025-04-24 11:05:48 -0300

    Corrigindo selects de unidade no formulário de solicitações

--- app/modules/foods/resources/farmer/_initialization.js
+++ app/modules/foods/resources/farmer/_initialization.js
@@ -109,18 +109,22 @@ $(document).on("focusout", "#farmerCpf", function () {
 });
 
 $(document).on("change", "#foodSelect", function () {
+    debugger;
     let foodId = this.value.split(',')[0];
     let measurementUnit = this.value.split(',')[1];
     let measurementUnitSelect = $('#measurementUnit');
     measurementUnitSelect.empty();
     switch (measurementUnit) {
+        case "Kg":
+            measurementUnitSelect.append($('<option value="g" selected>g</option><option value="Kg">Kg</option>'));
+            break;
         case "g":
             measurementUnitSelect.append($('<option value="g" selected>g</option><option value="Kg">Kg</option>'));
             break;
-        case "u":
+        case "Und":
             measurementUnitSelect.append($('<option value="unidade" selected>Unidade</option><option value="g">g</option><option value="Kg">Kg</option>'));
             break;
-        case "l":
+        case "L":
             measurementUnitSelect.append($('<option value="l" selected>L</option>'));
             break;
     }
