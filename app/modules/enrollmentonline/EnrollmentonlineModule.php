03a6e4654911061262632dd5bfe3d2a63712eea5
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-04-28 15:26:45 -0300

    feat/create a basic configs in new module

---
+++ app/modules/enrollmentonline/EnrollmentonlineModule.php
@@ -0,0 +1,31 @@
+<?php
+
+class EnrollmentonlineModule extends CWebModule
+{
+
+	public $baseScriptUrl;
+	public $baseUrl;
+
+	public function init()
+	{
+		$this->baseUrl = Yii::app()->createUrl("enrollmentonline");
+		$this->baseScriptUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.modules.enrollmentonline.resources'));
+		$this->setImport(array(
+			'enrollmentonline.models.*',
+			'enrollmentonline.components.*',
+		));
+	}
+
+	public function beforeControllerAction($controller, $action)
+	{
+		$controller->layout = 'webroot.themes.default.views.layouts.fullmenu';
+		if(parent::beforeControllerAction($controller, $action))
+		{
+			// this method is called before any module controller action is performed
+			// you may place customized code here
+			return true;
+		}
+		else
+			return false;
+	}
+}
