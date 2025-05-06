03e0aaf3a0fb3205d9e205ea1e0e73318e26d11f
GabrielChagas14 <gmelo123455@gmail.com>
2025-05-06 09:15:37 -0300

    disabled actionDelete

--- app/modules/enrollmentonline/controllers/EnrollmentonlinestudentidentificationController.php
+++ app/modules/enrollmentonline/controllers/EnrollmentonlinestudentidentificationController.php
@@ -177,11 +177,11 @@ public function beforeAction($action)
 	 */
 	public function actionDelete($id)
 	{
-		$this->loadModel($id)->delete();
+		// $this->loadModel($id)->delete();
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+		// if(!isset($_GET['ajax']))
+		// 	$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
 	}
 
 	/**
