e2da50d4bae43d18cdf9a144a5b815900bda494b
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-05-16 13:59:13 -0300

    feat: added correct search sql query

--- app/modules/enrollmentonline/controllers/EnrollmentonlinestudentidentificationController.php
+++ app/modules/enrollmentonline/controllers/EnrollmentonlinestudentidentificationController.php
@@ -8,31 +8,31 @@ use Sentry\Event;
 Yii::import('application.modules.enrollmentonline.repository.*');
 class EnrollmentOnlineStudentIdentificationController extends Controller
 {
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
             array(
                 'allow',  // allow all users to perform 'index', 'view', and 'create' actions
                 'actions' => array('index', 'view', 'create', 'getCities', 'getSchools'),
@@ -48,16 +48,16 @@ class EnrollmentOnlineStudentIdentificationController extends Controller
                 'actions' => array('admin', 'delete'),
                 'users' => array('admin'),
             ),
-           /*  array(
-                'deny',  // deny all users
-                'users' => array('*'),
-            ), */
+            /*  array(
+                 'deny',  // deny all users
+                 'users' => array('*'),
+             ), */
         );
-	}
+    }
 
     public function init()
-{
-   if (!Yii::app()->user->isGuest) {
+    {
+        if (!Yii::app()->user->isGuest) {
 
             $authTimeout = Yii::app()->user->getState("authTimeout", SESSION_MAX_LIFETIME);
             Yii::app()->user->authTimeout = $authTimeout;
@@ -68,11 +68,11 @@ class EnrollmentOnlineStudentIdentificationController extends Controller
                 'role' => Yii::app()->authManager->getRoles(Yii::app()->user->loginInfos->id)
             ]);
         }
-}
+    }
 
-public function beforeAction($action)
+    public function beforeAction($action)
     {
-        $publicActions = ['getschools','getcities'];
+        $publicActions = ['getschools', 'getcities'];
 
         if (Yii::app()->user->isGuest && Yii::app()->request->isAjaxRequest && !in_array($action->id, $publicActions)) {
             // Se a sessão expirou e é uma requisição AJAX
@@ -105,142 +105,154 @@ public function beforeAction($action)
         }
         return false;
     }
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id)
+    {
+        $this->render('view', array(
+            'model' => $this->loadModel($id),
+        ));
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate()
+    {
         $this->layout = "webroot.app.modules.enrollmentonline.layouts.enrollmentonline";
-		$model=new EnrollmentOnlineStudentIdentification;
+        $model = new EnrollmentOnlineStudentIdentification;
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['EnrollmentOnlineStudentIdentification']))
-		{
-			$model->attributes=$_POST['EnrollmentOnlineStudentIdentification'];
+        if (isset($_POST['EnrollmentOnlineStudentIdentification'])) {
+            $model->attributes = $_POST['EnrollmentOnlineStudentIdentification'];
             $repository = new EnrollmentonlinestudentidentificationRepository($model);
             $user = $repository->savePreEnrollment();
-            Yii::app()->user->setFlash('success', Yii::t('default', 'Pre-matricula realizada om sucesso! Agora voê pode acompnhar o andamento no com seu login '. $user->username.''));
+            Yii::app()->user->setFlash('success', Yii::t('default', 'Pre-matricula realizada om sucesso! Agora voê pode acompnhar o andamento no com seu login ' . $user->username . ''));
 
-		}
+        }
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
     public function actionStudentStatus()
-	{
+    {
         $this->render('studentstatus');
     }
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->loadModel($id);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['EnrollmentOnlineStudentIdentification']))
-		{
-			$model->attributes=$_POST['EnrollmentOnlineStudentIdentification'];
-			if($model->save()) {
-				$this->redirect(array('view','id'=>$model->id));
+        if (isset($_POST['EnrollmentOnlineStudentIdentification'])) {
+            $model->attributes = $_POST['EnrollmentOnlineStudentIdentification'];
+            if ($model->save()) {
+                $this->redirect(array('view', 'id' => $model->id));
             }
-		}
-
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
-
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		// $this->loadModel($id)->delete();
-
-		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-		// if(!isset($_GET['ajax']))
-		// 	$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	}
-
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('EnrollmentOnlineStudentIdentification');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
-
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new EnrollmentOnlineStudentIdentification('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['EnrollmentOnlineStudentIdentification'])) {
-			$model->attributes=$_GET['EnrollmentOnlineStudentIdentification'];
         }
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return EnrollmentOnlineStudentIdentification the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadModel($id)
-	{
-		$model=EnrollmentOnlineStudentIdentification::model()->findByPk($id);
-		if($model===null)
-        {
-                throw new CHttpException(404,'The requested page does not exist.');
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id)
+    {
+        // $this->loadModel($id)->delete();
+
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+        // if(!isset($_GET['ajax']))
+        // 	$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex()
+    {
+        $schoolInepId = Yii::app()->user->school;
+        $dataProvider = new CActiveDataProvider('EnrollmentOnlineStudentIdentification', array(
+            'criteria' => array(
+                'alias' => 'eosi',
+                'with' => array(
+                    'enrollmentOnlineEnrollmentSolicitations' => array(
+                        'alias' => 'eoes',
+                        'joinType' => 'INNER JOIN',
+                    ),
+                ),
+                'condition' => 'eoes.school_inep_id_fk = :schoolInepId',
+                'params' => array(':schoolInepId' => $schoolInepId),
+            ),
+        ));
+
+
+
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin()
+    {
+        $model = new EnrollmentOnlineStudentIdentification('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['EnrollmentOnlineStudentIdentification'])) {
+            $model->attributes = $_GET['EnrollmentOnlineStudentIdentification'];
         }
-		return $model;
-	}
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param EnrollmentOnlineStudentIdentification $model the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='enrollment-online-student-identification-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return EnrollmentOnlineStudentIdentification the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id)
+    {
+        $model = EnrollmentOnlineStudentIdentification::model()->findByPk($id);
+        if ($model === null) {
+            throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param EnrollmentOnlineStudentIdentification $model the model to be validated
+     */
+    protected function performAjaxValidation($model)
+    {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'enrollment-online-student-identification-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
 
     public function actionGetCities()
     {
