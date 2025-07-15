bce89a525d711fec9696f73b5bac45b007630ed6
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-07-15 17:28:40 -0300

    refactor: improve formatting and spacing in test.php configuration file

--- app/config/test.php
+++ app/config/test.php
@@ -1,12 +1,12 @@
 <?php
 
 // require(dirname(__FILE__).'/main.php');
-defined("DBNAME") or define("DBNAME","demo.tag.ong.br");
+defined("DBNAME") or define("DBNAME", "demo.tag.ong.br");
 $HOST = "localhost:3306";
 $USER = 'root';
 $SECRET = 'root';
 
-define ("DBCONFIG", serialize (array(
+define("DBCONFIG", serialize(array(
     'connectionString' => "mysql:host=$HOST;dbname=demo.tag.ong.br",
     'emulatePrepare' => true,
     'username' => $USER,
@@ -16,18 +16,14 @@ define ("DBCONFIG", serialize (array(
 
 
 return CMap::mergeArray(
-	require(dirname(__FILE__).'/main.php'),
+    require(dirname(__FILE__) . '/main.php'),
 
-	array(
-		'components'=>array(
-			'fixture'=>array(
-				'class'=>'system.test.CDbFixtureManager',
-			),
-			/* uncomment the following to provide test database connection
-			// 'db'=>array(
-			// 	'connectionString'=>'DSN for test database',
-			// ),
-			// */
-		),
-	)
+    array(
+        'components' => array(
+            'fixture' => array(
+                'class' => 'system.test.CDbFixtureManager',
+            ),
+
+        ),
+    )
 );
