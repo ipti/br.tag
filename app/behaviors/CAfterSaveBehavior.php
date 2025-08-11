3d30cc6a1b4e7a417e66739ddf0e748a701e6d41
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-08-11 18:19:09 -0300

    fix: remove comment

--- app/behaviors/CAfterSaveBehavior.php
+++ app/behaviors/CAfterSaveBehavior.php
@@ -4,10 +4,7 @@ class CAfterSaveBehavior extends CActiveRecordBehavior
 {
     public $schoolInepId = '';
 
-    public function afterSave($event)
-    {
-        // $id = $this->getOwner()->id;
-        // $newId = $this->schoolInepId.";" . $id;
-        // Yii::app()->db->createCommand('UPDATE `course_plan` SET `fkid` = "'.$newId.'" WHERE `id` = '.$id.';')->query();
+    public function afterSave($event){
+    //added logic here
     }
 }
