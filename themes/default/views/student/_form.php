d36c00ab8708abb9f8f011c02f34e4a87a4cb9fd
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-09-11 15:20:23 -0300

    fix: changed to main version form view in student

--- themes/default/views/student/_form.php
+++ themes/default/views/student/_form.php
@@ -167,7 +167,7 @@ $form = $this->beginWidget(
                   array(
                     'size' => 60,
                     'maxlength' => 100,
-                    'class' => 't-field-text__input',
+                    'class' => 't-field-text__input js-trim-name',
                     'placeholder' => 'Digite o Nome de Apresentação'
                   )
                 ); ?>
@@ -196,7 +196,7 @@ $form = $this->beginWidget(
                   array(
                     'size' => 60,
                     'maxlength' => 100,
-                    'class' => 't-field-text__input',
+                    'class' => 't-field-text__input js-trim-name',
                     'placeholder' => 'Digite o Nome Civil'
                   )
                 ); ?>
