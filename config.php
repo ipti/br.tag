08739a029af13ff372bbbe132d7f61ac9496bc94
Igor Gonçalves <igor_goncalves@live.com>
2025-09-17 19:34:45 -0300

    fix: corrigin cacteres e ligando trace de erros

--- config.php
+++ config.php
@@ -8,16 +8,24 @@ define("TAG_VERSION", '3.2.4');
 
 define("YII_VERSION", Yii::getVersion());
 define("BOARD_MSG", '<div class="alert alert-success">Novas atualizações no TAG. Confira clicando <a class="changelog-link" href="?r=admin/changelog">aqui</a>.</div>');
-if (YII_DEBUG) {
-    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
-    error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
-} else {
-    ini_set('display_errors', '0');
-    error_reporting(0);
-    define("YII_ENBLE_ERROR_HANDLER", false);
-    define("YII_ENBLE_EXCEPTION_HANDLER", false);
-}
+
+// if (YII_DEBUG) {
+//     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+//     error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
+// } else {
+//     ini_set('display_errors', '0');
+//     error_reporting(0);
+//     define("YII_ENBLE_ERROR_HANDLER", false);
+//     define("YII_ENBLE_EXCEPTION_HANDLER", false);
+// }
+
 date_default_timezone_set('America/Sao_Paulo');
 ini_set('always_populate_raw_post_data', '-1');
 setlocale(LC_ALL, 'portuguese', 'pt_BR.UTF-8', 'pt_BR.UTF8', 'pt_br.UTF8', 'ptb_BRA.UTF8', "ptb", 'ptb.UTF8');
 ini_set('session.gc_maxlifetime', SESSION_MAX_LIFETIME);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+    define("YII_ENBLE_ERROR_HANDLER", true);
+    define("YII_ENBLE_EXCEPTION_HANDLER", true);
