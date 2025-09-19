758b5d91d2558e0d4dcadee70a3eaa149c629c11
Igor Gonçalves <igor_goncalves@live.com>
2025-09-19 17:13:48 -0300

    fix: var config

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
 
 // Normaliza YII_DEBUG como boolean
-$debugEnv = getenv('YII_DEBUG') ?? '';
+$debugEnv = (bool) getenv('YII_DEBUG') ?? '';
 $debug = filter_var($debugEnv, FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
 defined('YII_DEBUG') or define('YII_DEBUG', $debug ?? false);
 
 // Sessão (1h por padrão)
 defined('SESSION_MAX_LIFETIME') or define('SESSION_MAX_LIFETIME', 3600);
+
+define("TAG_VERSION", '3.5.0');
 
 define("YII_VERSION", Yii::getVersion());
 define("BOARD_MSG", '<div class="alert alert-success">Novas atualizações no TAG. Confira clicando <a class="changelog-link" href="?r=admin/changelog">aqui</a>.</div>');
@@ -16,8 +18,8 @@ if (YII_DEBUG) {
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    define("YII_ENABLE_ERROR_HANDLER", true);
-    define("YII_ENABLE_EXCEPTION_HANDLER", true);
+    defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', true);
+    defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER', true);
     error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
 
 } else {
@@ -29,8 +31,8 @@ if (YII_DEBUG) {
     // ini_set('error_log', '/var/log/php/php-errors.log');
 
     error_reporting(0);
-    define("YII_ENABLE_ERROR_HANDLER", false);
-    define("YII_ENABLE_EXCEPTION_HANDLER", false);
+    defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', false);
+    defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER', false);
 }
 
 date_default_timezone_set('America/Sao_Paulo');
