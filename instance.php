fb17c5fd77a714707c27e184a5acdfba3854e2d1
emanuellcmr <gmelo123455@gmail.com>
2025-11-26 14:10:38 -0300

    resolve merge issues

--- instance.php
+++ instance.php
@@ -17,7 +17,7 @@ $domain = array_shift($host_array);
 $newdb = $domain . '.tag.ong.br';
 
 if ($domain == "localhost") {
-    $newdb = 'rosariodocatete.tag.ong.br';
+    $newdb = 'tome-acu.tag.ong.br';
 }
 
 $_GLOBALGROUP = 0;
