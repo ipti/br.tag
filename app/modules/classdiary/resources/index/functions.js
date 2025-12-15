23f4820917b94a1a93801676c814cd2b18701650
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-12-15 17:19:31 -0300

    fix: improve formatting and consistency in classroom card rendering

--- app/modules/classdiary/resources/index/functions.js
+++ app/modules/classdiary/resources/index/functions.js
@@ -1,39 +1,45 @@
- function renderClasroomsCards(discipline){
+function renderClasroomsCards(discipline) {
+    debugger;
     $.ajax({
         url: `${window.location.host}?r=classdiary/default/getclassrooms`,
         type: "POST",
         data: {
-            discipline: discipline
-        }
+            discipline: discipline,
+        },
     }).success(function (response) {
         const result = JSON.parse(response);
         const classrooms = $(".js-add-classrooms-cards");
-         var cardsClassrooms = result["classrooms"].reduce((acc, element) =>
-            acc += `
+        var cardsClassrooms = result["classrooms"].reduce(
+            (acc, element) =>
+                (acc += `
             <div class="column clearfix no-grow">
-                <a href="${window.location.host}?r=classdiary/default/classDays&classroom_fk=${element["id"]}&stage_fk=${element["stage_fk"]}&discipline_fk=${element["edcenso_discipline_fk"]}&discipline_name=${element["discipline_name"]}&classroom_name=${element["name"]}" class="t-cards">
+                <a href="${window.location.host}?r=classdiary/default/classDays&classroomFk=${element["id"]}&stageFk=${element["stage_fk"]}&disciplineFk=${element["edcenso_discipline_fk"]}&disciplineName=${element["discipline_name"]}&classroomName=${element["name"]}" class="t-cards">
                     <div class="t-cards-content">
                         <div class="t-tag-primary">${element["discipline_name"]}</div>
                         <div class="t-cards-title">${element["name"]}</div>
                         <div class="t-cards-text clear-margin--left">${element["stage_name"]}</div>
                     </div>
                 </a>
-            </div>`, "");
-        cardsClassrooms += result["minorSchoolingClassroom"].reduce((acc, element) =>
-        acc += `
+            </div>`),
+            ""
+        );
+        cardsClassrooms += result["minorSchoolingClassroom"].reduce(
+            (acc, element) =>
+                (acc += `
         <div class="column clearfix no-grow">
-            <a href="${window.location.host}?r=classdiary/default/classDays&classroom_fk=${element["id"]}&stage_fk=${element["stage_fk"]}&discipline_fk=''&discipline_name=&classroom_name=${element["name"]}" class="t-cards">
+            <a href="${window.location.host}?r=classdiary/default/classDays&classroomFk=${element["id"]}&stageFk=${element["stage_fk"]}&disciplineFk=''&disciplineName=&classroomName=${element["name"]}" class="t-cards">
                 <div class="t-cards-content">
                     <div class="t-cards-title">${element["name"]}</div>
                     <div class="t-cards-text clear-margin--left">${element["stage_name"]}</div>
                 </div>
             </a>
-        </div>`, "");
+        </div>`),
+            ""
+        );
         classrooms.append(cardsClassrooms);
+    });
+}
 
-    })
- }
-
-$(".js-select-disciplines").on("change", function() {
+$(".js-select-disciplines").on("change", function () {
     renderClasroomsCards(this.value);
 });
