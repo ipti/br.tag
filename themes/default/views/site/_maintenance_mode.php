baafe90551a12e54c167622ab9700335d3fdb2a6
Igor Gonçalves <igor_goncalves@live.com>
2025-12-30 12:27:54 -0300

    feat: Adicionando modo de manutenção

---
+++ themes/default/views/site/_maintenance_mode.php
@@ -0,0 +1,26 @@
+<?php
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+?>
+
+<body class="login" style="overflow-x: hidden;">
+    <div class="colorful-bar">
+        <span id="span-color-blue"></span>
+        <span id="span-color-red"></span>
+        <span id="span-color-green"></span>
+        <span id="span-color-yellow"></span>
+    </div>
+    <!-- Wrapper -->
+    <div id="login">
+        <!-- <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/tag-title.png" style="padding: 20px 20px;position: absolute;top: 0;right: 0;" /> -->
+        <img alt="logo negativa" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/fadedlogo.svg" class="fadedlogo" />
+        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/tag-title.png" style="padding: 20px 20px;position: absolute;top: 0;left: 0;" />
+        <div class="login-form">
+            <h1>Caro usuário, o TAG se encontra em manutenção</h1>
+            <h4 style="text-align:center">Estamos melhorando nossos servidores para melhor atende-los</h4>
+            <h2 style="text-align:center">Retorno estimado dia 31/12 2:30 AM, Feliz ano novo!</h2>
+        </div>
+
+    </div>
+</body>
