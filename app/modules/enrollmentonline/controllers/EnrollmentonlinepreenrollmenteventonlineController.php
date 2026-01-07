45d718e11d8210fdd08a7c32b4dc627e85fe4450
emanuellcmr <gmelo123455@gmail.com>
2026-01-07 20:34:38 -0300

    github issues

--- app/modules/enrollmentonline/controllers/EnrollmentonlinepreenrollmenteventonlineController.php
+++ app/modules/enrollmentonline/controllers/EnrollmentonlinepreenrollmenteventonlineController.php
@@ -279,10 +279,6 @@ class EnrollmentonlinepreenrollmenteventonlineController extends Controller
                 'Ocorreu um erro ao deletar o registro.'
             );
         }
-
-        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-        if (!isset($_GET['ajax']))
-            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
     }
 
     /**
