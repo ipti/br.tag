ad90858d5596abecdf73baa53873024a03bab9bd
emanuellcmr <gmelo123455@gmail.com>
2026-01-08 11:14:47 -0300

    fix/student-list-view

--- app/modules/enrollmentonline/controllers/EnrollmentonlinestudentidentificationController.php
+++ app/modules/enrollmentonline/controllers/EnrollmentonlinestudentidentificationController.php
@@ -6,7 +6,7 @@ use Sentry\State\Hub;
 use Sentry\Event;
 
 Yii::import('application.modules.enrollmentonline.repository.*');
-class EnrollmentOnlineStudentIdentificationController extends Controller
+class EnrollmentonlinestudentidentificationController extends Controller
 {
     /**
      * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -142,7 +142,7 @@ class EnrollmentOnlineStudentIdentificationController extends Controller
         $model = new EnrollmentOnlineStudentIdentification();
         $repository = new EnrollmentonlinestudentidentificationRepository($model);
         $studentList = $repository->getStudentList();
-        $this->render('studentList', [
+        $this->render('studentlist', [
             'studentList' => $studentList,
         ]);
     }
