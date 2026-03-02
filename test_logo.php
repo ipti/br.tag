691901316de1ba1f7c3760fcecd32f0e2e399e41
Igor Gonçalves <igor_goncalves@live.com>
2026-03-02 16:58:26 -0300

    chore(release): version 3.11.0 and simplified changelog

--- test_logo.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once('app/yiic.php');
-$content = Yii::app()->db->createCommand('SELECT logo_file_content FROM school_identification WHERE logo_file_name IS NOT NULL LIMIT 1')->queryScalar();
-var_dump(is_resource($content) ? 'stream' : substr($content, 0, 20));
