39ee9c83e37ae3868ae2e805446e26b87223ff29
Igor Gonçalves <igor_goncalves@live.com>
2026-06-15 17:28:01 -0300

    feat: implement CensoController with school import and validation logic, Classroom model, and registration support.

--- app/migrations/3.13.14/v3.13.14_classroom_organization_form.sql
+++ app/migrations/3.13.14/v3.13.14_classroom_organization_form.sql
@@ -12,3 +12,10 @@ ALTER TABLE classroom
     AFTER `is_alternance`;
 
 COMMIT;
+
+
+--  Auto-generated SQL script #202606151723
+UPDATE edcenso_alias ea
+	SET ea.attr='organization_form',
+    ea.default='1'
+	WHERE ea.register=20 AND ea.corder=28 AND ea.attr IS NULL AND ea.cdesc='Forma de organizacao da turma' AND ea.`default` IS NULL AND ea.stable IS NULL AND ea.`year`=2026;
